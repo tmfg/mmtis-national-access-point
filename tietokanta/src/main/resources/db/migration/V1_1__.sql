a725e413099d48427178d5d82bf2f4389107926e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-20 10:26:53 +0300

    Alustavaa tietomallia

--- tietokanta/src/main/resources/db/migration/V1_1__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1__.sql
@@ -1,6 +1,32 @@
--- Ensimmäinen placeholder tietokantamuutos testejä varten, älä aja tuotantoon
+-- Liikkumispalveluiden olennaiset tiedot
 
-CREATE TABLE foo (
-  id serial primary key,
-  bar text
+
+CREATE TYPE osoite AS (
+  katuosoite VARCHAR(128),
+  postinumero CHAR(5),
+  postitoimipaikka VARCHAR(64)
 );
+
+CREATE TABLE palveluntuottaja (
+  id SERIAL PRIMARY KEY,
+  nimi VARCHAR(200) NOT NULL, -- PRH ei kerro maksimipituutta
+  ytunnus CHAR(9) NOT NULL,
+  kotisivu VARCHAR(200),
+  kayntiosoite osoite,
+  postiosoite osoite
+);
+
+
+CREATE TYPE liikkumispalvelutyyppi AS ENUM (
+  'satama','kuljetus','vuokraus','pysakointi','valityspalvelu');
+
+CREATE TABLE liikkumispalvelu (
+  id SERIAL PRIMARY KEY,
+  "palveluntuottaja-id" INTEGER REFERENCES palveluntuottaja (id) NOT NULL,
+  tyyppi liikkumispalvelutyyppi NOT NULL
+);
+
+
+
+-- Aikatauluja varten
+CREATE TYPE viikonpaiva AS ENUM ('MA','TI','KE','TO','PE','LA','SU');
