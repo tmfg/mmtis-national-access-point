9f980147c6bdc91d9c73b14605e8117f42f83618
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-20 19:27:35 +0300

    Yksinkertainen testi validille datalle

---
+++ ote/test/clj/ote/domain/liikkumispalvelu_test.clj
@@ -0,0 +1,27 @@
+(ns ote.domain.liikkumispalvelu-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure.spec.alpha :as s]
+            [ote.domain.liikkumispalvelu :as lp]
+            [ote.domain.yleiset :as yleiset])
+  (:import (java.time LocalTime)))
+
+(def kallioparkki
+  {::lp/id 1
+   ::lp/palveluntuottaja-id 1
+   ::lp/pysakointi {::lp/pysakointitiedot
+                    {::lp/pysakointialueet
+                     [{::lp/alue {::lp/sijainti "FIXME: kallioparkin polygon"
+                                  ::lp/alueen-kuvaus "Oulun keskustan alla"}
+                       ::lp/aukioloajat [{::lp/viikonpaivat [:MA :TI :KE :TO :PE]
+                                          ::lp/avaamisaika (LocalTime/of 7 0)
+                                          ::lp/sulkemisaika (LocalTime/of 23 59)}]
+                       ::lp/maksutavat [:kateinen :mobiilimaksu]
+                       ::lp/esteettomyys [:inva-wc :induktiosilmukka]
+                       ::lp/esteettomyyskuvaus "Löytyy inva wc ja induktiosilmukka kuulolaitteille"
+                       ::lp/latauspisteet "onhan noita pari normaalia töpseliä"
+                       ::lp/varauspalvelu {::lp/url "http://example.com/varaa-pysakointi"}
+                       ::lp/liikennevalineet [:henkiloauto]}]}}})
+
+(deftest pysakointidata-validi
+  (is (= nil
+         (s/explain-data ::lp/liikkumispalvelu kallioparkki))))
