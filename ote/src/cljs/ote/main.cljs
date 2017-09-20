a6df3167604ad61e9695dd69e7e15762f13178b6
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-20 11:38:27 +0300

    Siirrä päänäkymä views alle
    ote.main nimiavaruuteen jää vain sovelluksen alustus ja päänäkymän käynnistys

--- ote/src/cljs/ote/main.cljs
+++ ote/src/cljs/ote/main.cljs
@@ -7,49 +7,18 @@
             [cljs-react-material-ui.icons :as ic]
             [tuck.core :as tuck]
             [ote.app.tila :as tila]
-            [ote.views.olennaiset-tiedot :as ot]
-            [ote.views.vuokraus :as vuokraus]
-            [ote.views.alueet :as pysakointialueet]
-            [ote.views.liikennevalineet :as liikennevalineet]
-            [ote.views.valituspalvelut :as valityspalvelut]
-            [ote.lokalisaatio :as lokalisaatio]
-            #_[ote.views.kuljetus :as kuljetus]
-            [ote.views.kuljetus :as kuljetus]
-            ))
+            [ote.views.main :as main]
+            [ote.lokalisaatio :as lokalisaatio]))
 
 
-(defn ote-sovellus
-  "OTE-sovelluksen käyttöliittymän pääkomponentti"
-  [e! app]
-  [ui/mui-theme-provider
-   {:mui-theme (get-mui-theme {:palette {:text-color (color :green600)}})}
-   [:div.ote-sovellus.container
-    [ui/app-bar {:title "OTE"}]
-    [ui/tabs
-     [ui/tab {:label "Olennaiset tiedot" :value "a"}
-      [ui/paper {:class "paper-siirto"}
-        [ot/olennaiset-tiedot e! (:muokattava-palvelu app)]]]
-     [ui/tab {:label "Pysäköintialueet" :value "b"}
-      [ui/paper {:class "paper-siirto"}
-        [pysakointialueet/pysakointialueet e! (:muokattava-palvelu app)]]]
-     [ui/tab {:label "Liikennevälineet" :value "c"}
-      [ui/paper {:class "paper-siirto"}
-       [liikennevalineet/liikennevalineet e! (:muokattava-palvelu app)]]]
-     [ui/tab {:label "Välityspalvelut" :value "d"}
-      [ui/paper {:class "paper-siirto"}
-       [valityspalvelut/valityspalvelu e! (:muokattava-palvelu app)]]]
-     [ui/tab {:label "Reitit" :value "d"}
-      [ui/paper {:class "paper-siirto"}
-       [kuljetus/reitti e! (:muokattava-palvelu app)]]]
-     ]
-    ]])
+
 
 (defn ^:export main []
   (lokalisaatio/lataa-kieli!
    :fi
    (fn [kieli _]
      (reset! lokalisaatio/valittu-kieli kieli)
-     (r/render-component [tuck/tuck tila/app ote-sovellus]
+     (r/render-component [tuck/tuck tila/app main/ote-sovellus]
                          (.getElementById js/document "oteapp")))))
 
 (defn ^:export reload-hook []
---
+++ ote/src/cljs/ote/views/main.cljs
@@ -0,0 +1,39 @@
+(ns ote.views.main
+  "OTE-sovelluksen päänäkymä"
+  (:require [cljs-react-material-ui.core :refer [get-mui-theme color]]
+            [cljs-react-material-ui.reagent :as ui]
+            [cljs-react-material-ui.icons :as ic]
+            [ote.views.olennaiset-tiedot :as ot]
+            [ote.views.vuokraus :as vuokraus]
+            [ote.views.alueet :as pysakointialueet]
+            [ote.views.liikennevalineet :as liikennevalineet]
+            [ote.views.valituspalvelut :as valityspalvelut]
+            [ote.lokalisaatio :as lokalisaatio]
+            [ote.views.kuljetus :as kuljetus]))
+
+
+(defn ote-sovellus
+  "OTE-sovelluksen käyttöliittymän pääkomponentti"
+  [e! app]
+  [ui/mui-theme-provider
+   {:mui-theme (get-mui-theme {:palette {:text-color (color :green600)}})}
+   [:div.ote-sovellus.container
+    [ui/app-bar {:title "OTE"}]
+    [ui/tabs
+     [ui/tab {:label "Olennaiset tiedot" :value "a"}
+      [ui/paper {:class "paper-siirto"}
+        [ot/olennaiset-tiedot e! (:muokattava-palvelu app)]]]
+     [ui/tab {:label "Pysäköintialueet" :value "b"}
+      [ui/paper {:class "paper-siirto"}
+        [pysakointialueet/pysakointialueet e! (:muokattava-palvelu app)]]]
+     [ui/tab {:label "Liikennevälineet" :value "c"}
+      [ui/paper {:class "paper-siirto"}
+       [liikennevalineet/liikennevalineet e! (:muokattava-palvelu app)]]]
+     [ui/tab {:label "Välityspalvelut" :value "d"}
+      [ui/paper {:class "paper-siirto"}
+       [valityspalvelut/valityspalvelu e! (:muokattava-palvelu app)]]]
+     [ui/tab {:label "Reitit" :value "d"}
+      [ui/paper {:class "paper-siirto"}
+       [kuljetus/reitti e! (:muokattava-palvelu app)]]]
+     ]
+    ]])
