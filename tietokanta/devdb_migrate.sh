82c241d461886477d2983fe177e79d9634416073
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-20 10:58:26 +0300

    Lisää testidata

--- tietokanta/devdb_migrate.sh
+++ tietokanta/devdb_migrate.sh
@@ -14,6 +14,4 @@ done
 echo "Yhteys saatu!"
 mvn flyway:migrate
 cd -
-
-#ei ole vielä mitään testidataa
-#docker run -v $DIR:/tietokanta -it --link napotedb:postgres --rm postgres sh /tietokanta/devdb_testidata.sh
+docker run -v $DIR:/tietokanta -it --link napotedb:postgres --rm postgres sh /tietokanta/devdb_testidata.sh
