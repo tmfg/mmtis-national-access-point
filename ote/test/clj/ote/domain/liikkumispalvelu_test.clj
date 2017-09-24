8b022e3f0a19f85659235c755ede50dde799787c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-24 17:46:35 +0300

    Remove extraneous map level

--- ote/test/clj/ote/domain/liikkumispalvelu_test.clj
+++ ote/test/clj/ote/domain/liikkumispalvelu_test.clj
@@ -7,19 +7,18 @@
 (def kallioparkki
   {::lp/tyyppi :pysakointi
    ::lp/palveluntuottaja-id 1
-   ::lp/pysakointi {::lp/pysakointitiedot
-                    {::lp/pysakointialueet
-                     [{::lp/alue {;;::lp/sijainti "FIXME: kallioparkin polygon"
-                                  ::lp/alueen-kuvaus "Oulun keskustan alla"}
-                       ::lp/aukioloajat [{::lp/viikonpaivat [:MA :TI :KE :TO :PE]
-                                          ::lp/avaamisaika (LocalTime/of 7 0)
-                                          ::lp/sulkemisaika (LocalTime/of 23 59)}]
-                       ::lp/maksutavat [:kateinen :mobiilimaksu]
-                       ::lp/esteettomyys [:inva-wc :induktiosilmukka]
-                       ::lp/esteettomyyskuvaus "Löytyy inva wc ja induktiosilmukka kuulolaitteille"
-                       ::lp/latauspisteet "onhan noita pari normaalia töpseliä"
-                       ::lp/varauspalvelu {::lp/url "http://example.com/varaa-pysakointi"}
-                       ::lp/liikennevalineet [:henkiloauto]}]}}})
+   ::lp/pysakointi {::lp/pysakointialueet
+                    [{::lp/alue {;;::lp/sijainti "FIXME: kallioparkin polygon"
+                                 ::lp/alueen-kuvaus "Oulun keskustan alla"}
+                      ::lp/aukioloajat [{::lp/viikonpaivat [:MA :TI :KE :TO :PE]
+                                         ::lp/avaamisaika (LocalTime/of 7 0)
+                                         ::lp/sulkemisaika (LocalTime/of 23 59)}]
+                      ::lp/maksutavat [:kateinen :mobiilimaksu]
+                      ::lp/esteettomyys [:inva-wc :induktiosilmukka]
+                      ::lp/esteettomyyskuvaus "Löytyy inva wc ja induktiosilmukka kuulolaitteille"
+                      ::lp/latauspisteet "onhan noita pari normaalia töpseliä"
+                      ::lp/varauspalvelu {::lp/url "http://example.com/varaa-pysakointi"}
+                      ::lp/liikennevalineet [:henkiloauto]}]}})
 
 (deftest pysakointidata-validi
   (is (= nil
