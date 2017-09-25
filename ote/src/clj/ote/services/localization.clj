4db3875e79e150656e27d3f05c9d7573ecb92240
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-25 18:15:25 +0300

    Fix old ns reference

--- ote/src/clj/ote/services/localization.clj
+++ ote/src/clj/ote/services/localization.clj
@@ -1,7 +1,7 @@
 (ns ote.services.localization
   "Services for fetching localization messages."
   (:require [com.stuartsierra.component :as component]
-            [ote.komponentit.http :as http]
+            [ote.components.http :as http]
             [ote.localization :as localization]
             [compojure.core :refer [routes GET]]))
 
