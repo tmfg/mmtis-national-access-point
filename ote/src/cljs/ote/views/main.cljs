874cee08647071581935c0279d974e0c615567a6
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-29 09:49:18 +0300

    Change place-search to own view ns

--- ote/src/cljs/ote/views/main.cljs
+++ ote/src/cljs/ote/views/main.cljs
@@ -10,13 +10,8 @@
             [ote.views.valituspalvelut :as valityspalvelut]
             [ote.localization :as localization]
             [ote.views.kuljetus :as kuljetus]
-
-            [reagent.core :as r]
-            [ote.app.place-search :as ps]
-            [ote.ui.form-fields :as form-fields]
-            [ote.ui.napit :as napit]
-            [ote.ui.debug :as debug]
-            [ote.ui.leaflet :as leaflet]))
+            [ote.views.place-search :as place-search]
+            ))
 
 
 (defn ote-application
@@ -29,24 +24,7 @@
     [ui/app-bar {:title "OTE"}]
 
 
-    [leaflet/Map {:center #js [65 25]
-                  :zoom 10}
-     [leaflet/TileLayer {:url "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                         :attribution "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"}]
-
-     (for [{:keys [namefin geometry]} (get-in app [:place-search :results :finnish-municipalities])]
-       ^{:key namefin}
-       [leaflet/FeatureGroup
-        [leaflet/geometry {:color "red"} geometry]
-        [leaflet/Popup [:div namefin]]])]
-
-    [form-fields/field {:type :string :label "Paikan nimi"
-                        :update! #(e! (ps/->SetPlaceName %))}
-     (get-in app [:place-search :name])]
-
-    [napit/tallenna {:on-click #(e! (ps/->SearchPlaces))} "Hae paikkoja"]
-
-    [debug/debug (:place-search app)]
+    [place-search/place-search e! (:place-search app)]
 
 
     #_[ui/tabs
