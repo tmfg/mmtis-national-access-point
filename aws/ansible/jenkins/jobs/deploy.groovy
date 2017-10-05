b1d255b49de51e56cc4c18758c1ab88ed3549f18
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-10-05 13:45:24 +0300

    Working artifact copy

--- aws/ansible/jenkins/jobs/deploy.groovy
+++ aws/ansible/jenkins/jobs/deploy.groovy
@@ -2,28 +2,29 @@
 def ote_job = jenkins.model.Jenkins.getInstance().getItem('OTE build from master')
 def ote_build = ote_job.getLastSuccessfulBuild()
 def ote_artifact_path = new java.io.File(ote_build.getArtifactManager().root().toURI())
-def ote_build_artifact = ote_artifact_path.getAbsolutePath() + 'target/ote-0.1-SNAPSHOT-standalone.jar'
+def ote_build_artifact = ote_artifact_path.getAbsolutePath() + '/ote/target/ote-0.1-SNAPSHOT-standalone.jar'
 
 job('Deploy OTE') {
-  parameters {
-    choiceParam('ENV', ['staging','production']);
-  }
-  scm {
-    git('https://github.com/finnishtransportagency/mmtis-national-access-point.git')
-  }
-  steps {
-    // PENDING: run database migration for as a step
-    ansiblePlaybookBuilder {
-      playbook('ote.yml')
-      inventory {
-        inventoryPath { path('aws/ansible/environments/${ENV}/inventory') }
-      }
-      extraVars {
-        extraVar {
-          key('ote_build_artifact')
-          value(ote_build_artifact)
-        }
-      }
+    parameters {
+        choiceParam('ENV', ['staging','production']);
     }
-  }
+    scm {
+        git('https://github.com/finnishtransportagency/mmtis-national-access-point.git')
+    }
+    steps {
+        // PENDING: run database migration for as a step
+        ansiblePlaybookBuilder {
+            additionalParameters('--vault-password-file=~/.vault_pass.txt')
+            playbook('aws/ansible/ote.yml')
+            inventory {
+                inventoryPath { path('aws/ansible/environments/${ENV}/inventory') }
+            }
+            extraVars {
+                extraVar {
+                    key('ote_build_artifact')
+                    value(ote_build_artifact)
+                }
+            }
+        }
+    }
 }
