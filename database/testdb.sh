1c6e0aeab5a05b955a70fd2acfa9a316de7cd563
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-10-10 17:43:00 +0300

    Fix command invocation

--- database/testdb.sh
+++ database/testdb.sh
@@ -8,4 +8,4 @@ P="psql -h localhost -U napote "
 $P -c "DROP DATABASE IF EXISTS napotetest_template;"
 $P -c "CREATE DATABASE napotetest_template;"
 
-$P -f nap/ckan-initial-db.sql
+$P -f nap/ckan-initial-db.sql napotetest_template
