5ade0e84db3383dcb47297c0056cc1de3a5a0506
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-10-12 14:40:57 +0300

    Add custom dataset fields

--- nap/docker/ckan/ckan-plugins/ckanext-napote_theme/ckanext/napote_theme/plugin.py
+++ nap/docker/ckan/ckan-plugins/ckanext-napote_theme/ckanext/napote_theme/plugin.py
@@ -5,11 +5,12 @@ import ckan.plugins.toolkit as toolkit
 from ckan.lib.plugins import DefaultTranslation
 
 
-class NapoteThemePlugin(plugins.SingletonPlugin):
+class NapoteThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     # http://docs.ckan.org/en/latest/extensions/translating-extensions.html
     # Enable after translations have been generated
     #plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm)
 
     def update_config(self, config):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
@@ -27,3 +28,31 @@ class NapoteThemePlugin(plugins.SingletonPlugin):
 
         # Public directory for static images
         toolkit.add_public_directory(config, 'public')
+
+    def nap_package_schema(self):
+        # Take default schema
+        schema = super(NapoteThemePlugin, self).create_package_schema()
+        # add custom fields
+        schema.update({
+            'transport_service_type': [tk.get_validator('ignore_missing'),
+                                       tk.get_converter('convert_to_extras')],
+            'operation_area': [tk.get_validator('ignore_missing'),
+                               tk.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def create_package_schema(self):
+        return self.nap_package_schema()
+
+    def update_package_schema(self):
+        return self.nap_package_schema()
+
+    def show_package_schema(self):
+        schema = super(NapoteThemePlugin, self).show_package_schema()
+        schema.update({
+            'transport_service_type': [tk.get_converter('convert_from_extras'),
+                                       tk.get_validator('ignore_missing')],
+            'operation_area': [tk.get_converter('convert_from_extras'),
+                               tk.get_converter('ignore_missing')]
+        })
+        return schema
