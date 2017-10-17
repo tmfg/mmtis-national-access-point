66126859e342d1a455fa98dca7f58f63b751a14e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-10-17 11:51:28 +0300

    Configure logrotator, only keep 3 days of builds

--- aws/ansible/jenkins/jobs/build.groovy
+++ aws/ansible/jenkins/jobs/build.groovy
@@ -1,41 +1,45 @@
 job('OTE build from master') {
-  scm {
+    logRotator {
+        daysToKeep(3)
+    }
+
+    scm {
         git('https://github.com/finnishtransportagency/mmtis-national-access-point.git', '*/master')
-  }
-  triggers {
-    scm('H/15 * * * *')
-  }
-  steps {
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
 
-    shell('sh database/testdb.sh')
+        shell('sh database/testdb.sh')
 
-    maven {
-      goals('flyway:migrate')
-      rootPOM('database/pom.xml')
-      mavenInstallation('Maven 3.5.0')
-      property('databaseUrl', 'jdbc:postgresql://localhost/napotetest_template')
-      property('databaseUser', 'napotetest')
-    }
+        maven {
+            goals('flyway:migrate')
+            rootPOM('database/pom.xml')
+            mavenInstallation('Maven 3.5.0')
+            property('databaseUrl', 'jdbc:postgresql://localhost/napotetest_template')
+            property('databaseUser', 'napotetest')
+        }
 
-    leiningenBuilder {
-      subdirPath('ote')
-      task('production')
+        leiningenBuilder {
+            subdirPath('ote')
+            task('production')
+        }
     }
-  }
-  publishers {
-    archiveArtifacts {
-      pattern('ote/target/*-standalone.jar')
-      onlyIfSuccessful()
+    publishers {
+        archiveArtifacts {
+            pattern('ote/target/*-standalone.jar')
+            onlyIfSuccessful()
+        }
+        slackNotifier {
+            notifyAborted(false)
+            notifyBackToNormal(true)
+            notifyFailure(true)
+            notifyNotBuilt(false)
+            notifyRegression(true)
+            notifyRepeatedFailure(false)
+            notifySuccess(true)
+            notifyUnstable(true)
+        }
     }
-    slackNotifier {
-      notifyAborted(false)
-      notifyBackToNormal(true)
-      notifyFailure(true)
-      notifyNotBuilt(false)
-      notifyRegression(true)
-      notifyRepeatedFailure(false)
-      notifySuccess(true)
-      notifyUnstable(true)
-    }
-  }
 }
