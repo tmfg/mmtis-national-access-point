f4b163a4608af3e7c66d0daa9db8a419b8337bf0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-10-19 15:19:21 +0300

    Fix devdb clean and test data

--- database/devdb_clean_docker.sh
+++ database/devdb_clean_docker.sh
@@ -6,8 +6,11 @@ set -e
 
 cd /database
 
+psql -h napotedb -U napote postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='napote' AND pid <> pg_backend_pid();"
+psql -h napotedb -U napote postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='napotetest' AND pid <> pg_backend_pid();"
+psql -h napotedb -U napote postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='napotetest_template' AND pid <> pg_backend_pid();"
 
-psql -h napotedb -U napote postgres -c "DROP DATABASE napote;"
+psql -h napotedb -U napote postgres -c "DROP DATABASE IF EXISTS napote;"
 psql -h napotedb -U napote postgres -c "CREATE DATABASE napote OWNER napote;"
 psql -h napotedb -U postgres napote -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO napotetest;"
 psql -h napotedb -U postgres napote -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO napote;"
--- database/testdata-ote.sql
+++ database/testdata-ote.sql
@@ -1,6 +1,10 @@
 INSERT INTO "transport-operator" (name, "business-id", homepage, "visiting-address")
 VALUES ('Ajopalvelu Testinen Oy', '1234567-8', 'http://www.example.com', ROW('Testikatu 1','90666','Testi')::address);
 
-INSERT INTO "transport-service"("transport-operator-id","type","terminal","passenger-transportation","rental","parking","brokerage","created","created-by","modified","modified-by","published?")
+INSERT INTO "transport-service"
+       ("transport-operator-id","type","terminal","passenger-transportation", "rental","parking",
+       "brokerage","created","created-by","modified","modified-by","published?",
+       "contact-address", "contact-phone", "homepage")
 VALUES
-(1,E'passenger-transportation',NULL,E'({},"(www.example.com/url,{})","(,{})",{},"{""(starting,5.9,trip,EUR)"",""(\\\\""basic fare\\\\"",4.9,km,EUR)""}",{child-seat},{},"{wheelchair,walkingstick}","{""(FI,\\\\""esteetöntä on\\\\"")"",""(SV,\\\\""ja, det är\\\\"")"",""(EN,\\\\""very accessible\\\\"")""}","(""Street 1"",90100,Oulu)",123456,,test@example.com,www.solita.fi)',NULL,NULL,NULL,NULL,NULL,NULL,NULL,FALSE);
+(1,E'passenger-transportation',NULL,E'({},"(www.example.com/url,{})","(,{})",{},"{""(starting,5.9,trip,EUR)"",""(\\\\""basic fare\\\\"",4.9,km,EUR)""}",{child-seat},{},"{wheelchair,walkingstick}","{""(FI,\\\\""esteetöntä on\\\\"")"",""(SV,\\\\""ja, det är\\\\"")"",""(EN,\\\\""very accessible\\\\"")""}")',NULL,NULL,NULL,NULL,NULL,NULL,NULL,FALSE,
+'(Street 1,90100,Oulu)','123456','www.solita.fi');
