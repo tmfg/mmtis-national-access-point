f30328bd581279c94c776265d381af787f0a43cc
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-10-19 15:19:32 +0300

    Add CKAN links

---
+++ database/src/main/resources/db/migration/V1_8__ckan_dataset_links.sql
@@ -0,0 +1,21 @@
+-- Add foreign key references to CKAN dataset (package) and resource
+
+ALTER TABLE "transport-service"
+  ADD COLUMN "ckan-dataset-id" TEXT REFERENCES package (id),
+  ADD COLUMN "ckan-resource-id" TEXT REFERENCES resource (id),
+  ADD COLUMN "ckan-external-resource-id" TEXT REFERENCES resource (id);
+
+COMMENT ON COLUMN "transport-service"."ckan-dataset-id" IS
+E'When publishing a service to CKAN, this id links to the dataset (package) in CKAN.
+NULL for unpublished services.';
+
+COMMENT ON COLUMN "transport-service"."ckan-resource-id" IS
+E'When publishing a service to CKAN, this id links to the resource that points to the
+OTE published GeoJSON export URL.
+NULL for unpublished services.';
+
+COMMENT ON COLUMN "transport-service"."ckan-external-resource-id" IS
+E'When publishing a service that has an external API URL to CKAN, this id links to the
+resource that points to the external URL. A service may have both the OTE resource and
+an external one.
+NULL FOR unpublished services.';
