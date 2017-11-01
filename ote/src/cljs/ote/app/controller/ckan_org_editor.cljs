dc86337661922518537f4a792125c99b20988ca1
Toni Dahl <toni.dahl@solita.fi>
2017-11-01 11:47:19 +0200

    Move common functionality from ckan view controllers to common controller.

--- ote/src/cljs/ote/app/controller/ckan_org_editor.cljs
+++ ote/src/cljs/ote/app/controller/ckan_org_editor.cljs
@@ -3,31 +3,16 @@
   (:require [tuck.core :as tuck]
             [ote.communication :as comm]
             [taoensso.timbre :as log]
-            [ote.app.routes :as routes]))
+            [ote.app.routes :as routes]
+            [ote.app.controller.transport-operator :as to]))
 
 
 (defrecord StartEditor [])
-(defrecord GetTransportOperatorData [ckan-group-id])
-(defrecord TransportOperatorDataResponse [response])
 
 (extend-protocol tuck/Event
 
   StartEditor
   (process-event [_ app]
     (let [ckan-group-id (.getAttribute (.getElementById js/document "nap_viewer") "data-group-id")]
-      (tuck/action!
-        (fn [e!]
-          (e! (->GetTransportOperatorData ckan-group-id))))))
-
-  GetTransportOperatorData
-  (process-event [_ app]
-    (comm/post! "transport-operator/data" {} {:on-success (tuck/send-async! ->TransportOperatorDataResponse)})
-    app)
-
-  TransportOperatorDataResponse
-  (process-event [{response :response} app]
-    ;(.log js/console " Transport operator data response" (clj->js response) (clj->js (get response :transport-operator)))
-    (assoc app
-      :transport-operator (get response :transport-operator)
-      :transport-services (get response :transport-service-vector )
-      :user (get response :user ))))
+      (to/get-transport-operator-data))
+    app))
--- ote/src/cljs/ote/app/controller/ckan_org_viewer.cljs
+++ ote/src/cljs/ote/app/controller/ckan_org_viewer.cljs
@@ -3,29 +3,16 @@
   (:require [tuck.core :as tuck]
             [ote.communication :as comm]
             [taoensso.timbre :as log]
-            [ote.app.routes :as routes]))
+            [ote.app.routes :as routes]
+            [ote.app.controller.transport-operator :as to]))
 
 
 (defrecord StartViewer [])
-(defrecord GetTransportOperatorData [ckan-group-id])
-(defrecord TransportOperatorResponse [response])
 
 (extend-protocol tuck/Event
 
   StartViewer
   (process-event [_ app]
     (let [ckan-group-id (.getAttribute (.getElementById js/document "nap_viewer") "data-group-id")]
-      (tuck/action!
-        (fn [e!]
-          (e! (->GetTransportOperatorData ckan-group-id))))))
-
-  GetTransportOperatorData
-  (process-event [{id :ckan-group-id} app]
-    (comm/get! (str "transport-operator/" id) {:on-success (tuck/send-async! ->TransportOperatorResponse)})
-    app)
-
-  TransportOperatorResponse
-  (process-event [{response :response} app]
-    ;(.log js/console " Transport operator response" (clj->js response) (clj->js (get response :transport-operator)))
-    (assoc app
-      :transport-operator response)))
+      (to/get-transport-operator-by-ckan-group-id ckan-group-id))
+    app))
