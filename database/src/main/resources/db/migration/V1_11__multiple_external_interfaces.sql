a5fddec21608ff6ab1f8f04d065e40ad93a23044
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-02 16:58:10 +0200

    Remove old external resource id field

--- database/src/main/resources/db/migration/V1_11__multiple_external_interfaces.sql
+++ database/src/main/resources/db/migration/V1_11__multiple_external_interfaces.sql
@@ -8,3 +8,6 @@ CREATE TABLE "external-interface-description" (
 );
 COMMENT ON TABLE "external-interface-description" IS
 E'Store links to other machine readable interfaces that describe the transport service. These are URLs to APIs or descriptors that are not created within NAP';
+
+-- Remove old external resource id (as we can now have multiple external resources)
+ALTER TABLE "transport-service" DROP COLUMN "ckan-external-resource-id";
