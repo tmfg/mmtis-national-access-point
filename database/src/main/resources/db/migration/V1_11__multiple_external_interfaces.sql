8fd9000afba631de04cb4e79c9764962adc56a28
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-02 13:36:11 +0200

    Add table for external interfaces

---
+++ database/src/main/resources/db/migration/V1_11__multiple_external_interfaces.sql
@@ -0,0 +1,10 @@
+-- Allow multiple external interfaces to be defined
+
+CREATE TABLE "external-interface-description" (
+  "transport-service-id" INTEGER REFERENCES "transport-service" (id),
+  "external-interface" service_link,
+  "format" TEXT,
+  "ckan-resource-id" TEXT REFERENCES "resource" (id)
+);
+COMMENT ON TABLE "external-interface-description" IS
+E'Store links to other machine readable interfaces that describe the transport service. These are URLs to APIs or descriptors that are not created within NAP';
