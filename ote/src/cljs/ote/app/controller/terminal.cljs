a8e3c859e5b5b55c829bf3b355a90cf7134cfa76
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-03 15:35:53 +0200

    Remove unused events

--- ote/src/cljs/ote/app/controller/terminal.cljs
+++ ote/src/cljs/ote/app/controller/terminal.cljs
@@ -9,31 +9,9 @@
             [ote.app.controller.transport-service :as transport-service]))
 
 (defrecord EditTerminalState [data])
-(defrecord SaveTerminalToDb [])
-(defrecord HandleTerminalResponse [service])
 
 (extend-protocol t/Event
 
   EditTerminalState
   (process-event [{data :data} app]
-    (update-in app [:transport-service ::t-service/terminal] merge data))
-
-
-  SaveTerminalToDb
-  (process-event [_ {service :transport-service :as app}]
-    (let [service-data
-          (-> service
-              (assoc ::t-service/type :terminal
-                ::t-service/transport-operator-id (get-in app [:transport-operator ::t-operator/id]))
-              (update ::t-service/terminal form/without-form-metadata)
-              (transport-service/move-service-level-keys-from-form ::t-service/terminal))]
-      (comm/post! "terminal-information"
-                  service-data
-                  {:on-success (t/send-async! ->HandleTerminalResponse)})
-      app))
-
-  HandleTerminalResponse
-  (process-event [{service :service} app]
-    (assoc app
-      :transport-service service
-      :page :own-services)))
+    (update-in app [:transport-service ::t-service/terminal] merge data)))
