ec1f941ce4cdb4f4a33b2027705a4b0923ee79be
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-07 09:20:53 +0200

    Add service hour exceptions

--- database/testdata-ote.sql
+++ database/testdata-ote.sql
@@ -7,5 +7,5 @@ INSERT INTO "transport-service"
        "brokerage","created","created-by","modified","modified-by","published?",
        "contact-address", "contact-phone", "homepage")
 VALUES
-(1,E'passenger-transportation',NULL,E'({},"(www.example.com/url,{})","(,{})",{},"{""(starting,5.9,trip,EUR)"",""(\\\\""basic fare\\\\"",4.9,km,EUR)""}",{child-seat},{},"{wheelchair,walkingstick}","{""(FI,\\\\""esteetöntä on\\\\"")"",""(SV,\\\\""ja, det är\\\\"")"",""(EN,\\\\""very accessible\\\\"")""}")',NULL,NULL,NULL,NULL,NULL,NULL,NULL,FALSE,
+(1,E'passenger-transportation',NULL,E'({},"(www.example.com/url,{})","(,{})",{},"{""(starting,5.9,trip,EUR)"",""(\\\\""basic fare\\\\"",4.9,km,EUR)""}",{child-seat},{},"{wheelchair,walkingstick}","{""(FI,\\\\""esteetöntä on\\\\"")"",""(SV,\\\\""ja, det är\\\\"")"",""(EN,\\\\""very accessible\\\\"")""}",{})',NULL,NULL,NULL,NULL,NULL,NULL,NULL,FALSE,
 '(Street 1,90100,Oulu)','123456','www.solita.fi');
