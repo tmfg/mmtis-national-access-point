bef89e1e0f75dc43bba41e3cc1e7159699ae20bf
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-08 18:20:30 +0200

    Create operation area facet in database
    Create a new "operation-area-facet" table which has the operation area
    definitions indexed. The table is automatically updated by a deferred
    constraint trigger so the backend doesn't need to do anything to keep
    the facet index updated.

---
+++ database/src/main/resources/db/migration/V1_14__service_search_view.sql
@@ -0,0 +1,38 @@
+CREATE TABLE "operation-area-facet" (
+ "transport-service-id" INTEGER REFERENCES "transport-service" (id) ON DELETE CASCADE,
+ "operation-area" TEXT
+);
+
+CREATE INDEX operation_area_id_idx ON "operation-area-facet" ("transport-service-id");
+CREATE INDEX operation_area_text_idx ON "operation-area-facet" ("operation-area");
+
+CREATE FUNCTION transport_service_operation_area_array () RETURNS TRIGGER AS $$
+BEGIN
+
+  -- Delete all previous entries for this service
+  DELETE
+    FROM "operation-area-facet"
+   WHERE "transport-service-id" = NEW.id;
+
+  IF NEW."published?" = TRUE THEN
+    -- Insert new values (for published only)
+    INSERT
+      INTO "operation-area-facet"
+           ("transport-service-id", "operation-area")
+    SELECT NEW.id, oad.text
+      FROM operation_area oa
+      JOIN LATERAL unnest(oa.description) AS oad ON TRUE
+     WHERE oa."transport-service-id" = NEW.id;
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE CONSTRAINT TRIGGER tg_transport_service_operation_area_array
+AFTER INSERT OR UPDATE
+ON "transport-service"
+DEFERRABLE INITIALLY DEFERRED
+FOR EACH ROW
+EXECUTE PROCEDURE transport_service_operation_area_array();
