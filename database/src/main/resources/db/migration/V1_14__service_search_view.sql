88232590fc18d41ce8195fdceec4d702899b816c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-09 14:28:03 +0200

    Create view to fetch transport service search results

--- database/src/main/resources/db/migration/V1_14__service_search_view.sql
+++ database/src/main/resources/db/migration/V1_14__service_search_view.sql
@@ -42,3 +42,25 @@ EXECUTE PROCEDURE transport_service_operation_area_array();
 CREATE INDEX transport_service_subtype_idx
     ON "transport-service" ("sub-type")
  WHERE "published?"=TRUE;
+
+
+CREATE TYPE external_interface_search_result AS (
+  "external-interface" service_link,
+  format TEXT,
+  "ckan-resource-id" TEXT
+);
+
+
+-- Create view for search results
+CREATE VIEW transport_service_search_result AS
+SELECT t.*,
+       (SELECT op.name
+          FROM "transport-operator" op
+         WHERE op.id = t."transport-operator-id") as "operator-name",
+       (SELECT array_agg(oaf."operation-area")
+          FROM "operation-area-facet" oaf
+         WHERE oaf."transport-service-id" = t.id) AS "operation-area-description",
+       (SELECT array_agg(ROW(ei."external-interface", ei.format, ei."ckan-resource-id")::external_interface_search_result)
+          FROM "external-interface-description" ei
+         WHERE ei."transport-service-id" = t.id)::external_interface_search_result[] AS "external-interface-links"
+  FROM "transport-service" t;
