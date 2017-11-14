72779e57be83c27e2c1a5bf5447ff721c4754dbe
Markus Sakkinen <markus.sakkinen@solita.fi>
2017-11-14 14:42:57 +0200

    Rental view basic fields and groups.

--- ote/src/cljs/ote/views/rental.cljs
+++ ote/src/cljs/ote/views/rental.cljs
@@ -9,39 +9,42 @@
             [ote.ui.buttons :as buttons]
             [ote.app.controller.transport-service :as ts]
             [ote.app.controller.rental :as rental]
-            [ote.db.transport-service :as transport-service]
+            [ote.db.transport-service :as t-service]
             [ote.db.common :as common]
             [ote.localization :refer [tr tr-key]]
             [ote.views.place-search :as place-search]
-            [tuck.core :as tuck]))
+            [tuck.core :as tuck]
+            [ote.views.transport-service-common :as ts-common]))
 
 (defn rental-form-options [e!]
-  {:name->label (tr-key [:field-labels :terminal])
+  {:name->label (tr-key [:field-labels :rentals] [:field-labels :transport-service-common])
    :update!     #(e! (rental/->EditRentalState %))
    :name        #(tr [:olennaiset-tiedot :otsikot %])
    :footer-fn   (fn [data]
                   [buttons/save {:on-click #(e! (rental/->SaveRentalToDb))
-                                   :disabled (form/disable-save? data)}
+                                 :disabled (form/disable-save? data)}
                    (tr [:buttons :save])])})
 
+(defn name-group [e!]
+  (form/group
+   {:label (tr [:rentals-page :header-service-info])
+    :columns 3
+    :layout :row}
+
+   {:name      ::t-service/name
+    :type      :string
+    :required? false}))
+
 (defn rental [e! status]
-  (.log js/console " Dippadaa rental ")
-  [:div.row
-   [:div {:class "col-lg-12"}
-    [:div
-     [:h3 "Täydennä vuokraukseen liittyvät tiedot."]]
-    [form/form (rental-form-options e!)
+  (reagent/with-let [options (rental-form-options e!)
+                     groups [(name-group e!)
+                             (ts-common/contact-info-group)
+                             (ts-common/place-search-group e! ::t-service/terminal)
+                             (ts-common/external-interfaces)]]
+    [:div.row
+     [:div {:class "col-lg-12"}
+      [:div
+       [:h3 (tr [:rentals-page :header-rental-service])]]
+      [form/form options groups (get status ::t-service/rental)]]]))
 
-     [
-      (form/group
-        {:label   "Kelpoisuus"
-         :columns 1}
-        {:name        ::transport-service/:eligibility-requirements
-         :type        :localized-text
-         }
-        )
-      ]
 
-     (get status ::transport-service/rental)]
-
-    ]])
