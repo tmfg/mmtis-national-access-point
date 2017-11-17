14d544da2746742e8e72b2a0f5c4e4962ed42767
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-17 17:21:54 +0200

    Insert initial ckan db data

--- database/circleci_setup.sh
+++ database/circleci_setup.sh
@@ -4,9 +4,10 @@
 
 sudo apt-get install postgresql-client maven
 
-mvn flyway:migrate
-
 P="psql -h localhost -p 5432 -U postgres -c"
+$P ../nap/ckan-initial-db.sql
+
+mvn flyway:migrate
 
 $P "CREATE DATABASE napotetest_template WITH TEMPLATE napote OWNER napotetest;"
 $P "CREATE DATABASE napotetest WITH TEMPLATE napotetest_template OWNER napotetest;"
