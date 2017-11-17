1c62f48cb34de924023af348af21db312cd8c63d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-17 17:19:00 +0200

    No need to create users/dbs, they are in the docker image

--- database/circleci_setup.sh
+++ database/circleci_setup.sh
@@ -4,23 +4,9 @@
 
 sudo apt-get install postgresql-client maven
 
-P="psql -h localhost -p 5432 -U postgres -c"
-
-$P "CREATE USER napotetest WITH CREATEDB;"
-$P "ALTER USER napotetest WITH SUPERUSER;"
-$P "CREATE USER napote;"
-$P "ALTER USER napote WITH SUPERUSER;"
-$P "CREATE USER ckan WITH CREATEDB;"
-$P "ALTER USER ckan WITH SUPERUSER;"
-$P "CREATE DATABASE napote OWNER napote;"
-$P "CREATE DATABASE temp OWNER napotetest;"
-$P "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO napotetest;"
-$P "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO napote;"
-$P "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ckan;"
-$P "CREATE EXTENSION postgis" napote
-$P "CREATE EXTENSION postgis_topology" napote
-
 mvn flyway:migrate
+
+P="psql -h localhost -p 5432 -U postgres -c"
 
 $P "CREATE DATABASE napotetest_template WITH TEMPLATE napote OWNER napotetest;"
 $P "CREATE DATABASE napotetest WITH TEMPLATE napotetest_template OWNER napotetest;"
