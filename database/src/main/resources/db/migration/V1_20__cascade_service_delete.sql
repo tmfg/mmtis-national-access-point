21572a4522aa100cd2e58899bc03d05f7bff51e1
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-20 12:46:30 +0200

    Cascade to OTE data when CKAN package/resource is deleted

--- database/src/main/resources/db/migration/V1_20__cascade_service_delete.sql
+++ database/src/main/resources/db/migration/V1_20__cascade_service_delete.sql
@@ -11,3 +11,24 @@ ALTER TABLE "external-interface-description"
       FOREIGN KEY ("transport-service-id")
       REFERENCES "transport-service" (id)
       ON DELETE CASCADE;
+
+-- When CKAN dataset is deleted, remove OTE information as well
+ALTER TABLE "transport-service"
+ DROP CONSTRAINT "transport-service_ckan-dataset-id_fkey",
+  ADD CONSTRAINT "transport-service_ckan-dataset-id_fkey"
+      FOREIGN KEY ("ckan-dataset-id")
+      REFERENCES package (id)
+      ON DELETE CASCADE;
+
+ALTER TABLE "external-interface-description"
+ DROP CONSTRAINT "external-interface-description_ckan-resource-id_fkey",
+  ADD CONSTRAINT "external-interface-description_ckan-resource-id_fkey"   FOREIGN KEY ("ckan-resource-id")
+      REFERENCES resource (id)
+      ON DELETE CASCADE;
+
+ALTER TABLE "transport-service"
+ DROP CONSTRAINT "transport-service_ckan-resource-id_fkey",
+  ADD CONSTRAINT "transport-service_ckan-resource-id_fkey"
+      FOREIGN KEY ("ckan-resource-id")
+      REFERENCES  resource (id)
+      ON DELETE CASCADE;
