bbced33d2f93dd298e087c6bc1b859ec1b54e49f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-11-24 18:39:45 +0200

    Upgrade specql

--- ote/project.clj
+++ ote/project.clj
@@ -14,7 +14,7 @@
                  [com.zaxxer/HikariCP "2.6.1"]
                  [org.clojure/java.jdbc "0.7.1"]
                  [webjure/jeesql "0.4.7"]
-                 [specql "0.7.0-alpha8"]
+                 [specql "0.7.0-alpha10"]
 
                  ;; http-kit HTTP server (and client)
                  [http-kit "2.2.0"]
