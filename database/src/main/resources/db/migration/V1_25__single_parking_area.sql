24a690036ed35ca10cd86fa3bf7530a48e23a696
Toni Dahl <toni.dahl@solita.fi>
2017-11-27 15:02:40 +0200

    Refactor parking_provider_information migration.

--- database/src/main/resources/db/migration/V1_25__single_parking_area.sql
+++ database/src/main/resources/db/migration/V1_25__single_parking_area.sql
@@ -1,27 +1,28 @@
-DROP TYPE parking_provider_information CASCADE;
-
-
 CREATE TYPE PARKING_CAPACITY AS
 (
   "parking-facility" PARKING_FACILITY,
   "capacity" INTEGER
 );
 
-ALTER TYPE parking_area
-  RENAME TO parking_provider_information;
+-- Recreate type, add missing parking provider attributes that are mentioned in the geojson example.
+ALTER TYPE PARKING_PROVIDER_INFORMATION
+ DROP ATTRIBUTE "parking-areas",
+  ADD ATTRIBUTE "office-hours" service_hours[],
+  ADD ATTRIBUTE "service-hours" service_hours[],
+  ADD ATTRIBUTE "payment-methods" payment_method[],
+  ADD ATTRIBUTE "information-service-accessibility" accessibility_info_facility[],
+  ADD ATTRIBUTE accessibility accessibility_facility[],
+  ADD ATTRIBUTE mobility mobility_facility[],
+  ADD ATTRIBUTE "accessibility-description" localized_text[], -- Free text of accessibility
+  ADD ATTRIBUTE "charging-points" localized_text[], -- Free text of possible charging points
+  ADD ATTRIBUTE "additional-service-links" service_link[],-- Support multiple additional services instead of just one.
+  ADD ATTRIBUTE "parking-capacities" parking_capacity[],
+  ADD ATTRIBUTE "booking-service" service_link,
+  ADD ATTRIBUTE "price-classes" price_class[],
+  ADD ATTRIBUTE "real-time-information" service_link,
+  ADD ATTRIBUTE "maximum-stay" INTERVAL,
+  ADD ATTRIBUTE "office-hours-exceptions" service_exception[],
+  ADD ATTRIBUTE "service-exceptions" service_exception[];
 
--- Add missing parking provider attributes.
-ALTER TYPE parking_provider_information
-  ADD ATTRIBUTE "parking-capacities" PARKING_CAPACITY [],
-  ADD ATTRIBUTE "booking-service" SERVICE_LINK,
-  ADD ATTRIBUTE "price-classes" PRICE_CLASS [],
-  ADD ATTRIBUTE "real-time-information" SERVICE_LINK,
-  ADD ATTRIBUTE "maximum-stay" INTERVAL;
-
--- Support multiple additional services instead of just one.
-ALTER TYPE parking_provider_information
- DROP ATTRIBUTE "additional-service",
-  ADD ATTRIBUTE "additional-services" SERVICE_LINK [];
-
-ALTER TABLE "transport-service"
-  ADD COLUMN parking parking_provider_information;
+-- Drop now obsolete type.
+DROP TYPE parking_area;
