2f85dd0347592543f3393bfa6775de62d5779cf4
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-12-01 16:27:21 +0200

    Have theme container provide common parts: flash-message and debug state

--- ote/src/cljs/ote/views/ckan_org_editor.cljs
+++ ote/src/cljs/ote/views/ckan_org_editor.cljs
@@ -13,14 +13,12 @@
             [ote.localization :refer [tr tr-or]]
             [ote.views.transport-operator :as to-view]))
 
-(defn editor [e! {ckan-organization-id :ckan-organization-id :as state}]
+(defn editor [e! _]
   ;; init
   (e! (org-edit/->StartEditor))
   (fn [e! state]
+    [theme state
+     [:div.ote-sovellus
       (if (get-in state [:transport-operator :loading?])
-      [:div.loading [:img {:src "/base/images/loading-spinner.gif"}]]
-
-      [theme
-       [:div.ote-sovellus
-        [:div
-         [to-view/operator e! state]]]])))
+        [:div.loading [:img {:src "/base/images/loading-spinner.gif"}]]
+        [to-view/operator e! state])]]))
