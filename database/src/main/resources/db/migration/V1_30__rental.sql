ee1e2afa83bc4eacdd15de683270170c7c99a988
Markus Sakkinen <markus.sakkinen@solita.fi>
2017-12-04 16:05:17 +0200

    migration split

--- database/src/main/resources/db/migration/V1_30__rental.sql
+++ database/src/main/resources/db/migration/V1_30__rental.sql
@@ -2,9 +2,6 @@ ALTER TYPE rental_provider_information
   ADD ATTRIBUTE "luggage-restrictions" localized_text[],
   ADD ATTRIBUTE "payment-methods" payment_method[];
 
-ALTER TYPE vehicle_accessibility
-  ADD VALUE 'disability-adapted-vehicle'; 
-
 ALTER TYPE rental_provider_information
   ADD ATTRIBUTE "guaranteed-vehicle-accessibility" vehicle_accessibility[],
   ADD ATTRIBUTE "limited-vehicle-accessibility" vehicle_accessibility[],
---
+++ database/src/main/resources/db/migration/V1_31__rental_disability_adapted_vehicle.sql
@@ -0,0 +1,2 @@
+ALTER TYPE vehicle_accessibility
+  ADD VALUE 'disability-adapted-vehicle'; 
