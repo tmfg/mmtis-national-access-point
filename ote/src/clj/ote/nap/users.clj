81292ed8ffd1162e61a7ff44b3b599e6917d5820
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-12-04 15:50:57 +0200

    Use find-user directly

--- ote/src/clj/ote/nap/users.clj
+++ ote/src/clj/ote/nap/users.clj
@@ -27,13 +27,11 @@
   "Ring middleware to fetch user info based on :user-id (username)."
   [db handler]
   ;; Cache fetched user info for 15 minutes
-  (let [cached-user-info (atom (cache/ttl-cache-factory {} :ttl (* 15 60 1000)))
-        find-user (partial find-user db)]
-    (fn [{username :user-id :as req}]
-      (if-let [user (when username
-                      (find-user username))]
-        (do (log/debug "User found: " (pr-str user))
-            (handler (assoc req :user user)))
-        (do (log/info "No user info, return 401")
-            {:status 401
-             :body "Unknown user"})))))
+  (fn [{username :user-id :as req}]
+    (if-let [user (when username
+                    (find-user db username))]
+      (do (log/debug "User found: " (pr-str user))
+          (handler (assoc req :user user)))
+      (do (log/info "No user info, return 401")
+          {:status 401
+           :body "Unknown user"}))))
