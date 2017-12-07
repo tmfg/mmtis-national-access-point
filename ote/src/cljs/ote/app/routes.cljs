3e9d3f77fb9e27545100e2a6b68eb821f60145f5
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-12-07 08:39:24 +0200

    Fix circular reference

--- ote/src/cljs/ote/app/routes.cljs
+++ ote/src/cljs/ote/app/routes.cljs
@@ -1,8 +1,7 @@
 (ns ote.app.routes
   "Routes for the frontend app."
   (:require [bide.core :as r]
-            [ote.app.state :as state]
-            [ote.app.controller.front-page :as fp-controller]))
+            [ote.app.state :as state]))
 
 (def ote-router
   (r/router
@@ -19,7 +18,8 @@
     ["/edit-service/:id" :edit-service]
     ["/services" :services]]))
 
-(defn- on-navigate [name params query]
+
+(defn- on-navigate [go-to-url-event name params query]
   (swap! state/app
          (fn [{:keys [before-unload-message navigation-prompt-open? url] :as app}]
            (if (and before-unload-message (not navigation-prompt-open?))
@@ -31,15 +31,16 @@
                ;; if the user confirms navigation.
                (assoc app
                       :navigation-prompt-open? true
-                      :navigation-confirm (fp-controller/->GoToUrl new-url)))
+                      :navigation-confirm (go-to-url-event new-url)))
 
              (merge app {:page name
                          :params params
                          :query query
                          :url js/window.location.href})))))
-(defn start! []
+
+(defn start! [go-to-url-event]
   (r/start! ote-router {:default :front-page
-                        :on-navigate on-navigate}))
+                        :on-navigate (partial on-navigate go-to-url-event)}))
 
 (defn navigate!
   "Navigate to given page with optional route and query parameters.
