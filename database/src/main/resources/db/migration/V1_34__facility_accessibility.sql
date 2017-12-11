22d3cb628015989e27ac004a86dfc2e144a263ec
Toni Dahl <toni.dahl@solita.fi>
2017-12-11 16:39:53 +0200

    Bring accessibility_info_facility enum back.

--- database/src/main/resources/db/migration/V1_34__facility_accessibility.sql
+++ database/src/main/resources/db/migration/V1_34__facility_accessibility.sql
@@ -1,6 +1,4 @@
--- Merge mobility_facility and accessibility_info_facility enums into accessibility_facility
-
--- Add facility related values from MOBILITY_FACILITY enum
+-- Merge mobility_facility enum into accessibility_facility
 
 -- This might be useless, because we can use the 'lift', 'ramp' etc. values to define if the facility has step free access.
 ALTER TYPE accessibility_facility ADD VALUE 'step-free-access';
@@ -8,10 +6,3 @@ ALTER TYPE accessibility_facility ADD VALUE 'suitable-for-wheelchairs';-- Added 
 -- NOTE: Fixes a typo that was in the original enum (tactile-patform-edges)
 ALTER TYPE accessibility_facility ADD VALUE 'tactile-platform-edges';
 ALTER TYPE accessibility_facility ADD VALUE 'tactile-guiding-strips';
-
-  -- Add values from ACCESSIBILITY_INFO_FACILITY
-ALTER TYPE accessibility_facility ADD VALUE 'audio-for-hearing-impaired';
-ALTER TYPE accessibility_facility ADD VALUE 'audio-information';
-ALTER TYPE accessibility_facility ADD VALUE 'visual-displays';
-ALTER TYPE accessibility_facility ADD VALUE 'displays-for-visually-impaired';
-ALTER TYPE accessibility_facility ADD VALUE 'large-print-timetables';
--- database/src/main/resources/db/migration/V1_35__drop_obsolete_accessibility_types.sql
+++
@@ -1,3 +0,0 @@
--- Drop obsolete types. These were merged into accessibility_facility.
-DROP TYPE mobility_facility CASCADE;
-DROP TYPE accessibility_info_facility CASCADE;
