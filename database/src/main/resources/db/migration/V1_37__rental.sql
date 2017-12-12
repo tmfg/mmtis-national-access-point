dfb9cd58384850d83d0270bd034f0f12bf7fb229
Markus Sakkinen <markus.sakkinen@solita.fi>
2017-12-12 11:53:22 +0200

    Update migration file number

---
+++ database/src/main/resources/db/migration/V1_37__rental.sql
@@ -0,0 +1,25 @@
+-- New type for rental additional services
+CREATE TYPE rental_additional_service AS (
+  "additional-service-type" additional_services,
+  "additional-service-price" price_class
+);
+
+ALTER TYPE rental_provider_information
+ DROP ATTRIBUTE "additional-services";
+
+ALTER TYPE rental_provider_information
+  ADD ATTRIBUTE "rental-additional-services" rental_additional_service[];
+
+-- New type for vehicle 
+CREATE TYPE rental_vehicle AS (
+  "vehicle-type" VARCHAR(128),
+  "license-required" VARCHAR(20),
+  "minimum-age" INTEGER,
+  "vehicle-prices" price_class[]
+);
+
+ALTER TYPE rental_provider_information
+  ADD ATTRIBUTE "rental-vehicles" rental_vehicle[];
+
+ALTER TYPE rental_provider_information
+  ADD ATTRIBUTE "usage-area" VARCHAR(500);
