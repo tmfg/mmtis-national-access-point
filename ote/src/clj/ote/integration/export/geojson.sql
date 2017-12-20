52e627a55b893d0deb53e6907d0b5b27ed635671
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-12-20 13:19:28 +0200

    Set SRID to WGS84 explicitly

--- ote/src/clj/ote/integration/export/geojson.sql
+++ ote/src/clj/ote/integration/export/geojson.sql
@@ -1,6 +1,6 @@
 -- name: fetch-operation-area-for-service
 -- single?: true
 -- Fetch the operation area for a transportation service as a GeoJSON document
-SELECT ST_AsGeoJSON(ST_Collect(location))
+SELECT ST_AsGeoJSON(ST_Collect(ST_SetSRID(location,4326)))
   FROM operation_area
  WHERE "transport-service-id" = :transport-service-id;
