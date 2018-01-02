a7b5af2d43a3f1c31f0903dcc57cc94689fe2a83
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-01-02 12:58:57 +0200

    Make external interface description, url and format required

--- ote/src/cljs/ote/views/transport_service_common.cljs
+++ ote/src/cljs/ote/views/transport_service_common.cljs
@@ -72,11 +72,14 @@
     :prepare-for-save values/without-empty-rows
     :table-fields [{:name ::t-service/external-service-description :type :localized-text :width "21%"
                     :read (comp ::t-service/description ::t-service/external-interface)
-                    :write #(assoc-in %1 [::t-service/external-interface ::t-service/description] %2)}
+                    :write #(assoc-in %1 [::t-service/external-interface ::t-service/description] %2)
+                    :required? true}
                    {:name ::t-service/external-service-url :type :string :width "21%"
                     :read (comp ::t-service/url ::t-service/external-interface)
-                    :write #(assoc-in %1 [::t-service/external-interface ::t-service/url] %2)}
-                   {:name ::t-service/format :type :string :width "16%"}
+                    :write #(assoc-in %1 [::t-service/external-interface ::t-service/url] %2)
+                    :required? true}
+                   {:name ::t-service/format :type :string :width "16%"
+                    :required? true}
                    {:name ::t-service/license :type :string :width "21%"}
                    {:name ::t-service/license-url :type :string :width "21%"}]
     :delete? true
