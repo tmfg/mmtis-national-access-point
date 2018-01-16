670a6cd3eb9eaf9202db58f4ffd6ffd6a3e17471
Toni Dahl <toni.dahl@solita.fi>
2018-01-16 08:46:08 +0200

    Insert municipalities etc. in circleci_setup

--- database/circleci_setup.sh
+++ database/circleci_setup.sh
@@ -13,5 +13,29 @@ mvn flyway:migrate
 $P -f testdata-ckan.sql napote
 $P -f testdata-ote.sql napote
 
+echo "Insert Finnish municipalities"
+$P -c "TRUNCATE finnish_municipalities;" || true
+$P -c "\COPY finnish_municipalities FROM /static-data/finnish_municipalities.csv CSV HEADER;" || true
+
+echo "Insert Finnish postal codes"
+$P -c "TRUNCATE finnish_postal_codes;" || true
+$P -c "\COPY finnish_postal_codes FROM /static-data/finnish_postal_codes.csv CSV HEADER;" || true
+
+echo "Insert Finnish regions"
+$P -c "TRUNCATE finnish_regions;" || true
+$P -c "\COPY finnish_regions FROM /static-data/maakunnat.csv CSV HEADER;" || true
+
+echo "Insert countries"
+$P -c "TRUNCATE country;" || true
+$P -c "\COPY country FROM /static-data/countries.csv CSV HEADER;" || true
+
+
+echo "Clean up and free connections"
+$P -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'napote' AND pid <> pg_backend_pid();"
+
+echo "Drop napotetest databases if they exists"
+$P -c "DROP DATABASE IF EXISTS napotetest_template;"
+$P -c "DROP DATABASE IF EXISTS napotetest;"
+
 $P -c "CREATE DATABASE napotetest_template WITH TEMPLATE napote OWNER napotetest;"
 $P -c "CREATE DATABASE napotetest WITH TEMPLATE napotetest_template OWNER napotetest;"
