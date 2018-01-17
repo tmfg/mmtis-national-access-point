7e4ba214259d05d4760df42bd8cd641323907187
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-01-17 12:37:26 +0200

    Add SQL query for listing users

--- ote/src/clj/ote/nap/users.sql
+++ ote/src/clj/ote/nap/users.sql
@@ -4,6 +4,7 @@
            u.fullname as user_name,
            u.apikey as user_apikey,
            u.email as user_email,
+           u.sysadmin as "user_admin?",
            g.id as group_id,
            g.name as group_name,
            g.title as group_title
@@ -11,3 +12,19 @@
  LEFT JOIN "member" m ON (m.table_name='user' AND m.state='active' AND m.table_id=u.id)
  LEFT JOIN "group" g ON g.id = m.group_id
      WHERE u.name = :username;
+
+-- name: list-users
+SELECT u.id as id,
+       u.name as username,
+       u.fullname as name,
+       u.email as email,
+       u.sysadmin as "admin?",
+       (SELECT string_agg(g.title, ', ')
+          FROM "member" m
+               JOIN "group" g ON g.id = m.group_id
+         WHERE m.table_name='user' AND
+               m.state='active' AND
+               m.table_id=u.id) as groups
+  FROM "user" u
+ WHERE (:email::VARCHAR IS NOT NULL AND u.email LIKE :email)
+    OR (:name::VARCHAR IS NOT NULL AND u.fullname LIKE :name);
