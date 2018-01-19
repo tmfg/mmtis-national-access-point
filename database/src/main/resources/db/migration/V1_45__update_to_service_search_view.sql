3e314d270fbb2aa1d75b8b13a7e240bda736a43a
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-01-19 13:23:05 +0200

    Removed drop trigger, drop function rows and only add create or replace function

--- database/src/main/resources/db/migration/V1_45__update_to_service_search_view.sql
+++ database/src/main/resources/db/migration/V1_45__update_to_service_search_view.sql
@@ -3,13 +3,6 @@
 -- delete all from operation-area-facet table
 DELETE FROM "operation-area-facet";
 
--- DROP trigger
-DROP TRIGGER IF EXISTS tg_transport_service_operation_area_array ON "transport-service";
-
--- DROP function
-DROP FUNCTION IF EXISTS transport_service_operation_area_array();
-
-
 -- insert initial data to facet - only this time add only finnish_muncipalities
 INSERT
   INTO "operation-area-facet"
@@ -22,7 +15,7 @@ SELECT t.id, oad.text
    AND oad.text IN (SELECT f.namefin FROM "finnish_municipalities" f);
 
 -- Create trigger function to update facet when service changes
-CREATE FUNCTION transport_service_operation_area_array () RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION transport_service_operation_area_array () RETURNS TRIGGER AS $$
 BEGIN
 
   -- Delete all previous entries for this service
@@ -45,11 +38,3 @@ BEGIN
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
--- Create trigger to update operation area at the end of transaction
-CREATE CONSTRAINT TRIGGER tg_transport_service_operation_area_array
-AFTER INSERT OR UPDATE
-ON "transport-service"
-DEFERRABLE INITIALLY DEFERRED
-FOR EACH ROW
-EXECUTE PROCEDURE transport_service_operation_area_array();
