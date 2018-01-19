0ef7c099797f6107db523098d90f12957d040cb9
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-01-19 15:46:05 +0200

    Add tool to generate changelog html

---
+++ tools/changelog/project.clj
@@ -0,0 +1,6 @@
+(defproject ote-changelog "0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [http-kit "2.2.0"]
+                 [cheshire "5.8.0"]
+                 [com.atlassian.commonmark/commonmark "0.11.0"]]
+  :main ote.tools.changelog)
