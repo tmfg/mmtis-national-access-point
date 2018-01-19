7b592f9807081fa7111c7001d80c63d5cd2fa984
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-01-19 14:45:37 +0200

    Copy backup files to S3

--- aws/ansible/jenkins/jobs/backup.groovy
+++ aws/ansible/jenkins/jobs/backup.groovy
@@ -16,5 +16,12 @@ job('Backup FINAP database') {
         }
 
         shell('PGPASSWORD=${vault_db_flyway_password} pg_dump -h ${vault_db_host} -U ${vault_db_flyway_user} --schema=public -Fc napote > napote-${ENV}.dump')
+
+        shell('mv napote-${ENV}.dump napote-${ENV}-`date +%Y-%m-%d`.dump')
+
+        shell('AWS_ACCESS_KEY_ID=${vault_backup_aws_key_id} AWS_SECRET_ACCESS_KEY=${vault_backup_aws_secret} aws cp napote-*.dump s3://finap-backup/')
+
+        shell('rm *.dump')
+
     }
 }
