1fc63a688d12c40be3ba509fd1f48c44805cc4c7
Toni Dahl <toni.dahl@solita.fi>
2018-02-03 17:44:34 +0200

    Add some localization unit tests.

--- ote/test/clj/ote/localization_test.clj
+++ ote/test/clj/ote/localization_test.clj
@@ -1,7 +1,8 @@
 (ns ote.localization-test
-  (:require  [clojure.test :as t :refer [deftest is]]
-             [ote.localization :as localization]
-             [clojure.set :as set]))
+  (:require [clojure.test :as t :refer [deftest is]]
+            [ote.localization :as localization]
+            [clojure.set :as set]
+            [clojure.java.io :as io]))
 
 (def languages ["fi" "sv" "en"])
 
@@ -13,6 +14,37 @@
                        (deep-key-paths (conj prefix-path key) val)
                        [(conj prefix-path key)])))
              #{} m))
+
+(defn- load-test-edn []
+  (-> (slurp (str "test/resources/lang.edn"))
+      read-string))
+
+
+(deftest handle-unsupported-tr-operation
+  (let [translations (load-test-edn)]
+    (is (= (#'localization/message (get-in translations [:unsupported-op]) {})
+           (str "{{unknown translation operation " :no-op "}}")))))
+
+(deftest concatenate-tr-vec
+  (let [translations (load-test-edn)]
+    (is (= (#'localization/message (get-in translations [:vec]) {})
+           "This is a vector"))))
+
+(deftest handle-tr-plurals
+  (let [translations (load-test-edn)]
+    (is (= (#'localization/message (get-in translations [:plural]) {:count 0})
+           "Got no results"))
+    (is (= (#'localization/message (get-in translations [:plural]) {:count 1})
+           "Got one result"))
+    (is (= (#'localization/message (get-in translations [:plural]) {:count 2})
+           "Got 2 results"))))
+
+;; TODO: Needs cljs testing support for more thorough testing of this feature.
+(deftest tr-markdown-cljs-only
+  (let [translations (load-test-edn)]
+    (is (thrown-with-msg?
+          clojure.lang.ExceptionInfo #"Markdown formatted translations not supported."
+          (#'localization/message (get-in translations [:markdown]) {})))))
 
 (deftest all-languages-have-same-keys
   (let [langs (atom {})]
