88f1c7ebb5e2cc7ac8c1ca088d344dca2b005b76
Toni Dahl <toni.dahl@solita.fi>
2018-02-06 13:25:24 +0200

    Use arrow functions in cy smoke tests.

--- cypress/integration/smoke/ote_spec.js
+++ cypress/integration/smoke/ote_spec.js
@@ -1,29 +1,29 @@
 // TODO: Figure out really good smoke tests. These are just copypasted from our more detailed spec files.
 
-import {randomName} from '../../support/util';
+import { randomName } from '../../support/util';
 
-describe('Own services basic tests', function () {
+describe('Own services basic tests', () => {
     // Login in only once before tests run
-    before(function () {
+    before(() => {
         cy.login();
     });
 
-    beforeEach(function () {
+    beforeEach(() => {
         // Session cookies will not be cleared before the NEXT test starts
         cy.preserveSessionOnce();
 
         cy.visit('/ote/#/own-services');
     });
 
-    it('should render the own services page', function () {
+    it('should render the own services page', () => {
         cy.contains('Lisää uusi palvelu');
     });
 
-    it('should redirect to add service view', function () {
+    it('should redirect to add service view', () => {
         cy.contains('Lisää uusi palvelu').click();
     });
 
-    it('should redirect to add new service provider view', function () {
+    it('should redirect to add new service provider view', () => {
         // Get the service selector with partial id (i.e. id-attribute contains the desired "Valitsepalveluntuottaja"-substring).
         cy.get('[id*="Valitsepalveluntuottaja"]')
             .click();
@@ -33,28 +33,24 @@ describe('Own services basic tests', function () {
 });
 
 
-describe('Add a new service', function () {
-    before(function () {
+describe('Add a new service', () => {
+    before(() => {
         cy.login();
     });
 
-    beforeEach(function () {
+    beforeEach(() => {
         // Session cookies will not be cleared before the NEXT test starts
         cy.preserveSessionOnce();
     });
 
-    describe('Taxi service', function () {
-        before(function () {
-            cy.wrap(randomName('test-service-')).as('serviceName')
-        });
+    describe('Taxi service', () => {
+        const serviceName = randomName('test-service-');
 
-        beforeEach(function () {
+        beforeEach(() => {
             cy.fixture('services/taxi.json').as('service');
         });
 
-        it('should should add a new service', function () {
-            const service = this.service;
-
+        it('should should add a new service', () => {
             cy.visit('/ote/#/new-service');
 
             cy.get('[id*="Valitseliikkumispalveluntyyppi"]')
@@ -68,21 +64,26 @@ describe('Add a new service', function () {
             cy.contains('Jatka').click();
 
             // Fill mandatory fields
-            cy.get('input[id*="name--Palvelunnimi"]').type(this.serviceName);
-            cy.get('input[name="street"]').type(service.contact.street);
-            cy.get('input[name="postal_code"]').type(service.contact.postal_code);
-            cy.get('input[name="post_office"]').type(service.contact.post_office);
-            cy.get('input[id*="-Haepaikkanimell"]').as('areaInput');
 
-            cy.wrap(service.areas).each(area => {
-                cy.get('@areaInput').type(area);
+            cy.get('input[id*="name--Palvelunnimi"]').type(serviceName);
 
-                return cy.wait('@placeCompletion')
-                    .then(() => {
-                        cy.contains(area).click();
+            cy.get('@service').then(service => {
+                cy.get('input[name="street"]').type(service.contact.street);
+                cy.get('input[name="postal_code"]').type(service.contact.postal_code);
+                cy.get('input[name="post_office"]').type(service.contact.post_office);
 
-                        return cy.wait(2000);
-                    });
+                cy.get('input[id*="-Haepaikkanimell"]').as('areaInput');
+
+                cy.wrap(service.areas).each(area => {
+                    cy.get('@areaInput').type(area);
+
+                    return cy.wait('@placeCompletion')
+                        .then(() => {
+                            cy.contains(area).click();
+
+                            return cy.wait(2000);
+                        });
+                });
             });
 
             cy.contains('.form-field', 'Olen lukenut tämän osion')
@@ -93,13 +94,13 @@ describe('Add a new service', function () {
             cy.contains('Tallenna ja julkaise').click();
         });
 
-        it('should delete the test service', function () {
+        it('should delete the test service', () => {
             cy.server();
-            cy.route('POST','/ote/transport-service/delete').as('deleteService');
+            cy.route('POST', '/ote/transport-service/delete').as('deleteService');
 
             cy.visit('/ote/#/own-services');
 
-            cy.contains('tr', this.serviceName)
+            cy.contains('tr', serviceName)
                 .within($tr => {
                     cy.get('a').last().click();
                 });
@@ -112,12 +113,12 @@ describe('Add a new service', function () {
 
 });
 
-describe('Add new service provider', function () {
-    before(function () {
+describe('Add new service provider', () => {
+    before(() => {
         cy.login();
     });
 
-    beforeEach(function () {
+    beforeEach(() => {
         // Session cookies will not be cleared before the NEXT test starts
         cy.preserveSessionOnce();
 
