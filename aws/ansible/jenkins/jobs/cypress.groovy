b81d597f014fa22b455198f90ebdc7f775ce2c66
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-02-07 11:15:00 +0200

    Add Cypress e2e test job

---
+++ aws/ansible/jenkins/jobs/cypress.groovy
@@ -0,0 +1,29 @@
+job('Cypress e2e tests') {
+    logRotator {
+        daysToKeep(3)
+    }
+
+    scm {
+        git('https://github.com/finnishtransportagency/mmtis-national-access-point.git', '*/master')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+
+    wrappers {
+        nodejs('nodejs-8.x-cypress')
+        toolenv('nodejs-8.x-cypress')
+    }
+
+    steps {
+        shell('ansible-vault view --vault-password-file=~/.vault_pass.txt aws/ansible/environments/staging/group_vars/all/vault > build.properties')
+
+        envInjectBuilder {
+            propertiesContent('')
+            propertiesFilePath('build.properties')
+        }
+
+        shell('npm i cypress@1.x && $(npm bin)/cypress verify')
+        shell('CYPRESS_RECORD_KEY=${vault_cypress_record_key} $(npm bin)/cypress run --browser chrome --record --spec cypress/integration/smoke/ote_spec.js')
+    }
+}
