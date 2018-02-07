c49d44e038c8a6171a97d7818d0a81f22d5c5d94
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-02-07 14:24:41 +0200

    Remove wait (the .result-title assert should be enough)

--- cypress/integration/service-search_spec.js
+++ cypress/integration/service-search_spec.js
@@ -2,13 +2,7 @@
 
 describe('Service search page basic tests', function () {
     it('should open the search page and load default results', function () {
-        cy.server();
-        cy.route('/ote/service-search').as('serviceSearch');
-
         cy.visit('/ote/#/services');
-
-        cy.wait('@serviceSearch');
-
         // Expect to have some default results
         cy.get('.service-search').find('.result-title').should('have.length.above', 0)
     });
