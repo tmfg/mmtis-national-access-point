c8f98e03198f093310d3842d74488e6dc6b0ab29
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-02-08 15:21:50 +0200

    Save external companies to service-company table at the same time when service is saved. Refactor check-csv return object Add business-id validator Change header validator.

--- ote/src/clj/ote/services/external.clj
+++ ote/src/clj/ote/services/external.clj
@@ -2,13 +2,26 @@
   "CKAN-integration tasks. Functions to invoke CKAN API calls from OTE."
   (:require [com.stuartsierra.component :as component]
             [ote.components.http :as http]
+            [ote.db.transport-service :as t-service]
+            [ote.db.modification :as modification]
             [org.httpkit.client :as httpkit]
             [compojure.core :refer [routes GET POST DELETE]]
             [taoensso.timbre :as log]
             [ote.authorization :as authorization]
             [clojure.string :as str]
             [clojure.data.csv :as csv]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [specql.core :as specql]))
+
+(defn valid-csv-header?
+  "Ensure that there is at least 2 elements in header"
+  [header]
+  (>= (count header) 2))
+
+(defn valid-business-id? [value]
+  (let [pattern  #"\d{7}-\d"]
+  (if  (re-matches pattern value)
+    true false)))
 
 (defn ensure-url
   "Add http:// to the beginning of the given url if it doesn't exist."
@@ -20,25 +33,46 @@
 (defn parse-response->csv
   "Convert given vector to map where map key is given in the first line of csv file."
   [csv-data]
-  (map zipmap
-       (->> (first csv-data) ;; First row is the header
-            (map keyword) ;; Drop if you want string keys instead
-            repeat)
-       (rest csv-data)))
+  (let [headers (first csv-data)
+        valid-header? (valid-csv-header? headers)
+        parsed-data (when valid-header?
+                      (map (fn [[business-id name]]
+                             {::t-service/business-id business-id
+                              ::t-service/name name})
+                            (rest csv-data)))
+        validated-data (filter #(valid-business-id? (::t-service/business-id %)) parsed-data)
+        filtered-data  (map #(dissoc % :remove) validated-data )]
+    {:result filtered-data
+     :failed-count (- (count parsed-data) (count filtered-data))}))
 
-(defn- check-csv
+(defn save-companies
+  "Save business-ids, company names to db"
+  [db data]
+   (let [data (if (nil? (::t-service/id data))
+               ;; Add created at the first time
+               (assoc data ::t-service/created (java.sql.Timestamp. (System/currentTimeMillis)))
+               ;; And modified for the rest
+               (assoc data ::t-service/modified (java.sql.Timestamp. (System/currentTimeMillis))))]
+    (specql/upsert! db ::t-service/service-company data)))
+
+(defn check-csv
   "Fetch csv file from given url, parse it and return status map that contains information about the count of companies
   in the file."
-  [url-data]
+  [db url-data]
   (let [url (ensure-url (get url-data :url))
+        transport-service-id (get url-data :ts-id)
         response @(httpkit/get url {:as :text
                                     :timeout 30000})]
     (if (= 200 (:status response))
       (try
         (let [data (when (= 200 (:status response))
-                               (csv/read-csv (:body response)))]
+                               (csv/read-csv (:body response)))
+              parsed-data (parse-response->csv data)]
+          ;; response to client application
             {:status :success
-             :count (count (parse-response->csv data))})
+             :count (count (:result parsed-data))
+             :failed-count (:failed-count parsed-data)
+             :companies (:result parsed-data)})
         (catch Exception e
           (log/info "CSV parsing failed: " e)
           {:status :failed
@@ -62,7 +96,7 @@
   (routes
    (POST  "/check-company-csv" {url-data :body}
           (http/transit-response
-           (check-csv
+           (check-csv db
             (http/transit-request url-data ))))
    (POST  "/check-external-api" {url-data :body}
           (http/transit-response
--- ote/src/clj/ote/services/transport.clj
+++ ote/src/clj/ote/services/transport.clj
@@ -247,17 +247,41 @@
                        (filter (comp (complement from-ui) ::t-service/id) from-db))]
     to-delete))
 
+(defn- delete-external-companies
+  "User might remove url from service, so we delete all service-companies from db"
+  [db transport-service]
+    (specql/delete! db ::t-service/service-company {::t-service/transport-service-id (::t-service/id transport-service)}))
+
+(defn- save-external-companies
+  "Service can contain an url that contains company nmes and business-id. Sevice can also contain an imported csv file
+  with company names and business-ids."
+  [db transport-service]
+  (let [current-data (first (fetch db ::t-service/service-company (specql/columns ::t-service/service-company)
+                            {::t-service/transport-service-id (::t-service/id transport-service)}))
+        companies (into [] (:companies (ote.services.external/check-csv db {:url (get transport-service ::t-service/companies-csv-url)
+                                                                            :ts-id (get transport-service ::t-service/id)})))
+        new-data (if (empty? current-data)
+                      {::t-service/companies            companies
+                       ::t-service/transport-service-id (get transport-service ::t-service/id)
+                       ::t-service/source               "URL"}
+                      (assoc current-data ::t-service/companies companies))]
+
+    (ote.services.external/save-companies db new-data)
+  ))
+
 (defn- save-transport-service
   "UPSERT! given data to database. And convert possible float point values to bigdecimal"
   [nap-config db user {places ::t-service/operation-area
                        external-interfaces ::t-service/external-interfaces
+                       service-company ::t-service/service-company
                        :as data}]
   ;(println "DATA: " (pr-str data))
   (let [service-info (-> data
                          (modification/with-modification-fields ::t-service/id user)
                          (dissoc ::t-service/operation-area)
                          floats-to-bigdec
-                         (dissoc ::t-service/external-interfaces))
+                         (dissoc ::t-service/external-interfaces)
+                         (dissoc ::t-service/service-company))
 
         resources-from-db (publish/fetch-transport-service-external-interfaces db (::t-service/id data))
         removed-resources (removable-resources resources-from-db external-interfaces)
@@ -272,6 +296,13 @@
 
             ;; Save operation areas
             (places/save-transport-service-operation-area! db transport-service-id places)
+
+            ;; Save companies
+            (if (::t-service/companies-csv-url transport-service)
+              ;; Update companies
+              (save-external-companies db transport-service)
+              ;; If url is empty, delete remaining data
+              (delete-external-companies db transport-service))
 
             transport-service))]
 
