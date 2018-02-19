88b8adbcc996dd39e9cd1256e8edda128dbd5cd0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-02-19 09:56:09 +0200

    Add table and function to store daily company count stats

---
+++ database/src/main/resources/db/migration/V1_60__company_stats.sql
@@ -0,0 +1,20 @@
+-- Add table for daily stats about company count
+
+CREATE TABLE company_stats (
+  "date" DATE PRIMARY KEY,
+  "count" INTEGER
+);
+
+CREATE OR REPLACE FUNCTION store_daily_company_stats () RETURNS VOID AS $$
+INSERT INTO company_stats ("date", "count")
+VALUES ((SELECT current_date - 1),
+        (SELECT SUM(COALESCE(
+                (SELECT array_length(companies,1)
+                   FROM service_company sc
+                  WHERE sc."transport-service-id" = ts.id),
+                array_length(companies,1),
+                0))
+          FROM "transport-service" ts
+         WHERE ts."published?" = TRUE))
+ON CONFLICT DO NOTHING;
+$$ LANGUAGE SQL;
