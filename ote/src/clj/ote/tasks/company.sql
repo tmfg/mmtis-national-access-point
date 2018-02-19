e62f445588dc3627b862dd250c0c3719ac530c16
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-02-19 12:12:02 +0200

    Update CSV files periodically throughout the day

---
+++ ote/src/clj/ote/tasks/company.sql
@@ -0,0 +1,20 @@
+-- name: store-daily-company-stats
+SELECT store_daily_company_stats();
+
+-- name: select-company-csv-for-update
+SELECT "transport-service-id" FROM service_company
+ WHERE updated < (current_timestamp - '1 day'::interval)
+   AND source = 'URL'
+ORDER BY updated ASC LIMIT 1
+ FOR UPDATE SKIP LOCKED;
+
+-- name: fetch-company-csv-url
+-- single?: true
+SELECT "companies-csv-url"
+  FROM "transport-service"
+ WHERE id = :transport-service-id;
+
+-- name: update-done!
+UPDATE service_company
+   SET updated = current_timestamp
+ WHERE "transport-service-id" = :transport-service-id;
