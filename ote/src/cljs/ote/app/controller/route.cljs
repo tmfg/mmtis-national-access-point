5e132ca909d8f0c0b0a1ba8d6b7edb5a782180f8
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-03-12 15:39:11 +0200

    Fix clear calendar on new datamodel

--- ote/src/cljs/ote/app/controller/route.cljs
+++ ote/src/cljs/ote/app/controller/route.cljs
@@ -156,7 +156,7 @@
 
   ClearServiceCalendar
   (process-event [_ app]
-    (assoc-in app [:route :service-calendar] {}))
+    (assoc-in app [:route ::transit/service-calendars 0] {}))
 
   GoToStep
   (process-event [{step :step} app]
