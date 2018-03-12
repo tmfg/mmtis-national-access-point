a00cd22aec5b1d2b5e97d824f3009d5f1d6ffbb7
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-03-12 11:06:11 +0200

    Use namespaced keywords for fields

--- ote/src/cljs/ote/views/route/basic_info.cljs
+++ ote/src/cljs/ote/views/route/basic_info.cljs
@@ -2,13 +2,16 @@
   "Route wizard: basic info form"
   (:require [ote.ui.form :as form]
             [ote.app.controller.route :as rc]
-            [ote.db.transport-operator :as t-operator]))
+            [ote.db.transport-operator :as t-operator]
+            [ote.db.transit :as transit]))
 
 (defn basic-info [e! app]
   ;; Initially select the first operator
   (e! (rc/->EditRoute
-       {:transport-operator (:transport-operator
-                             (first (:transport-operators-with-services app)))}))
+       {::transit/transport-operator-id
+        (::t-operator/id
+         (:transport-operator
+          (first (:transport-operators-with-services app))))}))
   (fn [e! {route :route :as app}]
     [:div.route-basic-info
      [form/form {:update! #(e! (rc/->EditRoute %))}
@@ -16,11 +19,12 @@
         {:label "Reitin nimi ja palveluntuottaja"
          :columns 2
          :layout :row}
-        {:name :name
+        {:name ::transit/name
          :type :string
          :label "Reitin nimi"
          :required? true}
-        {:name :transport-operator
+        {:name ::transit/transport-operator-id
+         :option-value ::t-operator/id
          :type :selection
          :label "Palveluntuottaja"
          :options (map :transport-operator
