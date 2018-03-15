342110bc1092d565b63298fa6ff5344f9cbfbc94
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-03-15 11:05:24 +0200

    Make exception icon smaller

--- ote/src/cljs/ote/style/route.cljs
+++ ote/src/cljs/ote/style/route.cljs
@@ -4,6 +4,6 @@
     [stylefy.core :as stylefy :refer [use-style use-sub-style]]))
 
 (def stepper {::stylefy/mode {:hover {:text-decoration "underline"}}})
-(def exception-icon-size {:width 24 :height 24})
-(def exception-icon (merge {:color "#000"} exception-icon-size))
+(def exception-icon-size {:width 18 :height 18})
+(def exception-icon (merge {:color "#CCC"} exception-icon-size))
 (def selected-exception-icon (merge {:color "#2D75B4"} exception-icon-size))
