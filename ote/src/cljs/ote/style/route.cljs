e24984a166f95e18acda282fdeb26fee011d4fb5
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-03-15 07:54:50 +0200

    Add initial colors and sizes for pickup and dropoff exception icons

--- ote/src/cljs/ote/style/route.cljs
+++ ote/src/cljs/ote/style/route.cljs
@@ -4,3 +4,6 @@
     [stylefy.core :as stylefy :refer [use-style use-sub-style]]))
 
 (def stepper {::stylefy/mode {:hover {:text-decoration "underline"}}})
+(def exception-icon-size {:width 24 :height 24})
+(def exception-icon (merge {:color "#000"} exception-icon-size))
+(def selected-exception-icon (merge {:color "#2D75B4"} exception-icon-size))
