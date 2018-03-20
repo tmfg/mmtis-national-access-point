a7fb029790a876d62c90a8e41542a09db7faf0d7
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-03-20 12:36:30 +0200

    Store user generated ports with OTE+sequence number code

--- ote/src/clj/ote/services/routes.clj
+++ ote/src/clj/ote/services/routes.clj
@@ -1,6 +1,7 @@
 (ns ote.services.routes
   "Routes api."
   (:require [com.stuartsierra.component :as component]
+            [ote.util.fn :refer [flip]]
             [ote.components.http :as http]
             [ote.services.transport :as transport]
             [specql.core :refer [fetch update! insert! upsert! delete!] :as specql]
@@ -14,8 +15,12 @@
             [taoensso.timbre :as log]
             [specql.op :as op]
             [ote.authorization :as authorization]
-            [cheshire.core :as cheshire])
+            [cheshire.core :as cheshire]
+            [ote.db.tx :as tx]
+            [jeesql.core :refer [defqueries]])
   (:import (org.postgis PGgeometry Point Geometry)))
+
+(defqueries "ote/services/routes.sql")
 
 (def route-list-columns  #{::transit/id
                            ::transit/transport-operator-id
@@ -43,25 +48,52 @@
       .toInstant
       java.util.Date/from))
 
-(defn- stop-location-geometry [{[lat lng] ::transit/location :as stop}]
+(defn- point-geometry [geom]
+  (if (instance? PGgeometry geom)
+    geom
+    (let [[lat lng] geom]
+      (PGgeometry. (Point. lat lng)))))
+
+(defn- stop-location-geometry [{loc ::transit/location :as stop}]
   (assoc stop
-         ::transit/location (PGgeometry. (Point. lat lng))))
+         ::transit/location (point-geometry loc)))
 
 (defn- service-calendar-dates [{::transit/keys [service-removed-dates service-added-dates] :as cal}]
   (assoc cal
          ::transit/service-removed-dates (map service-date->inst service-removed-dates)
          ::transit/service-added-dates (map service-date->inst service-added-dates)))
 
+(defn- next-stop-code [db]
+  (str "OTE" (next-stop-sequence-number db)))
+
+(defn save-custom-stops [route db]
+  (let [custom-stops
+        (into {}
+              (map (fn [{:keys [id geojson]}]
+                     [id (specql/insert!
+                          db ::transit/finnish-ports
+                          {::transit/code (next-stop-code db)
+                           ::transit/name (get-in geojson ["properties" "name"])
+                           ::transit/location (point-geometry (get-in geojson ["geometry" "coordinates"]))})]))
+              (:custom-stops route))]
+    (-> route
+        (dissoc :custom-stops)
+        (update ::transit/stops (flip mapv)
+                (fn [{::transit/keys [code] :as stop}]
+                  (or (custom-stops code) stop))))))
+
 (defn save-route [nap-config db user route]
   (authorization/with-transport-operator-check
     db user (::transit/transport-operator-id route)
     (fn []
-      (let [r (-> route
-                  (modification/with-modification-fields ::transit/id user)
-                  (update ::transit/stops #(mapv stop-location-geometry %))
-                  (update ::transit/service-calendars #(mapv service-calendar-dates %)))]
-        (log/debug "Save route: " r)
-        (upsert! db ::transit/route r)))))
+      (tx/with-transaction db
+        (let [r (-> route
+                    (save-custom-stops db)
+                    (modification/with-modification-fields ::transit/id user)
+                    (update ::transit/stops #(mapv stop-location-geometry %))
+                    (update ::transit/service-calendars #(mapv service-calendar-dates %)))]
+          (log/debug "Save route: " r)
+          (upsert! db ::transit/route r))))))
 
 (defn- routes-auth
   "Routes that require authentication"
