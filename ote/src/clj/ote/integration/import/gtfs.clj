32f9a80237283b4196bf4c5e05ea5f2f0046c69b
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-03-26 11:08:48 +0300

    Remove debug code

--- ote/src/clj/ote/integration/import/gtfs.clj
+++ ote/src/clj/ote/integration/import/gtfs.clj
@@ -16,9 +16,7 @@
   (http/transit-response
    (into {}
          (keep (fn [{:keys [name data]}]
-                 (println "name:" name)
                  (when-let [gtfs-file-type (gtfs-spec/name->keyword name)]
-                   (println "name: " name ", file type: " gtfs-file-type)
                    [gtfs-file-type (gtfs-parse/parse-gtfs-file gtfs-file-type data)])))
          (load-zip-from-url url))))
 
@@ -29,8 +27,7 @@
            (http/publish! http {:authenticated? false}
                           (routes
                            (GET "/import/gtfs" {params :query-params}
-                                (println "PARAMS: " params)
-                                (#'load-gtfs (get params "url")))))))
+                                (load-gtfs (get params "url")))))))
   (stop [{stop ::stop :as this}]
     (stop)
     (dissoc this ::stop)))
