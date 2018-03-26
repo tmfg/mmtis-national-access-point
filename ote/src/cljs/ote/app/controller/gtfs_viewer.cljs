e13fba24a1fd98772c31c9bd2558940b4b807a5f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-03-26 10:59:45 +0300

    Use new define-event for all events

--- ote/src/cljs/ote/app/controller/gtfs_viewer.cljs
+++ ote/src/cljs/ote/app/controller/gtfs_viewer.cljs
@@ -6,14 +6,27 @@
             [ote.gtfs.query :as gq]
             [clojure.string :as str]))
 
-(defrecord StartViewer [])
-(defrecord LoadGTFSResponse [response])
-(defrecord LoadGTFSFailure [response])
+(declare ->LoadGTFSResponse ->LoadGTFSFailure)
+
+(define-event StartViewer []
+  {}
+  (comm/get! (str "import/gtfs?url=" (js/encodeURIComponent (get-in app [:query :url])))
+    {:on-success (tuck/send-async! ->LoadGTFSResponse)
+     :on-failure (tuck/send-async! ->LoadGTFSFailure)})
+  app)
+
+(define-event LoadGTFSResponse [response]
+  {:path [:gtfs-viewer]}
+  response)
+
+(define-event LoadGTFSFailure [response]
+  {}
+  (.log js/console "Load GTFS failed: " (pr-str response))
+  (assoc app :flash-message-error "GTFS tiedoston lataus epäonnistui"))
 
 (define-event SelectRoute [route]
   {:path [:gtfs-viewer]
    :app gtfs}
-  (println "ROUTE" (pr-str route))
   (let [route-id (:gtfs/route-id route)
         trips (gq/route-trips gtfs route-id)
         stop-sequences (gq/stop-sequences-for-trips gtfs trips)]
@@ -32,28 +45,3 @@
 
 (defmethod routes/on-navigate-event :view-gtfs [_]
   (->StartViewer))
-
-(extend-protocol tuck/Event
-  StartViewer
-  (process-event [_ app]
-    (println "STARTING GTFS VIEW")
-    (comm/get! (str "import/gtfs?url=" (js/encodeURIComponent (get-in app [:query :url])))
-               {:on-success (tuck/send-async! ->LoadGTFSResponse)
-                :on-failure (tuck/send-async! ->LoadGTFSFailure)})
-    app)
-
-  LoadGTFSResponse
-  (process-event [{response :response} app]
-    #_(.log js/console "GOT RESPONSE:" (pr-str response))
-    (assoc app :gtfs-viewer response))
-
-  LoadGTFSFailure
-  (process-event [{response :response} app]
-    (.log js/console "Load GTFS failed: " (pr-str response))
-    app))
-
-(define-event FetchStopTimesForStop [stop-id]
-  {:path [:gtfs-viewer]
-   :app gtfs}
-  (assoc-in gtfs [:selected-route :stop-times-for-stop stop-id]
-            "FOFOFOFOFOF"))
