1440255b563144aa31f7a57285ada75ca88baaae
Toni Dahl <toni.dahl@solita.fi>
2018-04-11 16:38:53 +0300

    Fix cypress route tests.

--- cypress/integration/route_spec.js
+++ cypress/integration/route_spec.js
@@ -1,7 +1,6 @@
 describe('Sea route tests', function () {
-
-    before (function () {
-	cy.login();
+    before(function () {
+        cy.login();
     });
 
     beforeEach(function () {
@@ -10,84 +9,86 @@ describe('Sea route tests', function () {
     });
 
 
-    it ('Create new route', function () {
-	cy.visit ('/ote/#/routes/');
+    it('Create new route', function () {
+        cy.visit('/ote/#/routes/');
 
-	cy.get('tr').should('have.length', 1);
+        cy.get('tr').should('have.length', 1);
 
-	cy.contains('Lisää uusi merireitti').click();
+        cy.contains('Lisää uusi merireitti').click();
     });
 
-    it ('Add basic information', function () {
-	cy.get('[id*="Reitinnimi"]').type('Test route').should('have.value', 'Test route');
-	cy.get('[id*="Palveluntuottaja"]').click();
-	cy.contains('Testinen').click();
-	cy.get('[id*="departure-point-name"]').type('Oulu').should('have.value', 'Oulu');
-	cy.get('[id*="destination-point-name"]').type('Vaasa').should('have.value', 'Vaasa');
-	cy.get('[id*="Voimassaalkaen"]').click();
-	cy.get(':nth-child(3) > :nth-child(4) > span').click();
-	cy.get('[id*="Voimassaasti"]').click();
-	cy.get(':nth-child(5) > :nth-child(4) > span').click();
+    it('Add basic information', function () {
+        cy.get('[id*="Reitinnimi"]').type('Test route').should('have.value', 'Test route');
+        cy.get('[id*="Palveluntuottaja"]').click();
+
+        cy.contains('Testinen').click();
+        cy.get('[id*="departure-point-name"]').type('Oulu').should('have.value', 'Oulu');
+        cy.get('[id*="destination-point-name"]').type('Vaasa').should('have.value', 'Vaasa');
+
+        cy.get('[id*="Voimassaalkaen"]').click();
+        cy.document().its('body').type('{enter}');
+        cy.get('[id*="Voimassaasti"]').click();
+        cy.document().its('body').type('{downarrow}{downarrow}{enter}');
     });
 
-    it ('Add stops', function () {
-	cy.get('img[title="OULUN SATAMA: Vihreäsaari"]').click();
-	cy.get('img[title="RAAHE: Rautaruukki"]').click();
-	cy.get('img[title="KALAJOKI: Itäkenttä"]').click();
-	cy.get('img[title="VAASA: Lassenlaituri"]').click();
+    it('Add stops', function () {
+        cy.get('img[title="OULUN SATAMA: Vihreäsaari"]').click();
+        cy.get('img[title="RAAHE: Rautaruukki"]').click();
+        cy.get('img[title="KALAJOKI: Itäkenttä"]').click();
+        cy.get('img[title="VAASA: Lassenlaituri"]').click();
     });
 
-    it ('Add calendar', function () {
-	cy.get('#add-route-button').should('be.disabled');
+    it('Add calendar', function () {
+        cy.get('#add-route-button').should('be.disabled');
 
-	cy.get('#button_0').first().click();
+        cy.get('#button_0').first().click();
 
-	cy.get('[id*="date-picker-from-date"]').click();
-	cy.get(':nth-child(2) > :nth-child(4) > span').click();
+        cy.get('[id*="date-picker-from-date"]').click();
+        cy.document().its('body').type('{enter}');
 
-	cy.get('[id*="date-picker-to-date"]').click();
-	cy.get(':nth-child(4) > :nth-child(4) > span').click();
+        cy.get('[id*="date-picker-to-date"]').click();
+        cy.document().its('body').type('{downarrow}{enter}');
 
-	cy.get('#row_0 > :nth-child(3)').click();
-	cy.get('#row_0 > :nth-child(4)').click();
-	cy.get('#row_0 > :nth-child(5)').click();
-	cy.get('#row_0 > :nth-child(6)').click();
-	cy.get('#row_0 > :nth-child(7)').click();
+        cy.get('#row_0 > :nth-child(3)').click();
+        cy.get('#row_0 > :nth-child(4)').click();
+        cy.get('#row_0 > :nth-child(5)').click();
+        cy.get('#row_0 > :nth-child(6)').click();
+        cy.get('#row_0 > :nth-child(7)').click();
 
-	cy.get('#new-calendar-period-button').click()
-	    .should('is.disabled');
+        cy.get('#new-calendar-period-button').click()
+            .should('is.disabled');
 
-	cy.contains('button', 'Sulje').click();
+        cy.contains('button', 'Sulje').click();
     });
 
-    it ('Add route times', function () {
-	let hour = 1;
-	cy.get('.route-times')
-	    .within($route => {
-		cy.get('input[id*="hours-tt-nul"]').each(el => {
-		    cy.wrap(el).type(hour++);
-		});
-	    });
+    it('Add route times', function () {
+        let hour = 1;
 
-	cy.get('.route-times')
-	    .within($route => {
-		cy.get('input[id*="minutes-mm-nul"]').each(el => {
-		    cy.wrap(el).type('00');
-		});
-	    });
+        cy.get('.route-times')
+            .within($route => {
+                cy.get('input[id*="hours-tt-nul"]').each(el => {
+                    cy.wrap(el).type(hour++);
+                });
+            });
 
-	cy.get('#add-route-button').should('be.enabled');
+        cy.get('.route-times')
+            .within($route => {
+                cy.get('input[id*="minutes-mm-nul"]').each(el => {
+                    cy.wrap(el).type('00');
+                });
+            });
+
+        cy.get('#add-route-button').should('be.enabled');
     });
 
-    it ('Save template', function () {
-	cy.contains('Tallenna luonnoksena').click();
-	cy.get('tr').should('have.length', 2);
+    it('Save template', function () {
+        cy.contains('Tallenna luonnoksena').click();
+        cy.get('tr').should('have.length', 2);
     });
 
-    it ('Delete template', function () {
-	cy.get('[id*="delete-route"]').click();
-	cy.contains('button', 'Poista').click();
-	cy.get('tr').should('have.length', 1);
+    it('Delete template', function () {
+        cy.get('[id*="delete-route"]').click();
+        cy.contains('button', 'Poista').click();
+        cy.get('tr').should('have.length', 1);
     });
-
 });
