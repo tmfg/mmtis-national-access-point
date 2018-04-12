6eda8bc00f030ea6ebbfb7d76db99125c2ae86a3
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-04-12 15:50:31 +0300

    Refactor list-header

--- ote/src/cljs/ote/ui/list_header.cljs
+++ ote/src/cljs/ote/ui/list_header.cljs
@@ -1,19 +1,10 @@
-(ns ote.ui.list-header
+(ns ote.ui.list_header
   "Header component for OTE list pages"
   (:require [cljs-react-material-ui.reagent :as ui]
             [ote.ui.common :as common]))
 
-
-
-
 (defn header [title add-button operator-selection]
-
-  [:div.row
-   [:div.col-xs-12.col-sm-6.col-md-9
-    [:h1 title]]]
-  [:div.row operator-selection]
-  [:div.row
-   [:div.col-xs-12.col-sm-6.col-md-3
-    add-button]]
-
-  )
+  [:div
+   [:div.row [:h1 title]]
+   [:div.row operator-selection]
+   [:div.row add-button]])
