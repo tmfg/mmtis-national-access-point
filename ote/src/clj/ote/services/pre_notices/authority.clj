6fec850af932a2ba2298785744d6ba1f1198ea93
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-04-12 19:11:53 +0300

    Add functions to fetch notice and add comments

--- ote/src/clj/ote/services/pre_notices/authority.clj
+++ ote/src/clj/ote/services/pre_notices/authority.clj
@@ -9,16 +9,33 @@
             [ote.db.transport-operator :as t-operator]))
 
 
-(defn list-published-notices [db]
+(defn list-published-notices [db user]
+  (def last-user user) ;;FIXME: for repl debugging
   (specql/fetch db ::transit/pre-notice
-                (conj (specql/columns ::transit/pre-notice)
-                      [::t-operator/transport-operator #{::t-operator/name}])
+                #{::transit/id
+                  ::modification/created
+                  ::transit/route-description
+                  ::transit/pre-notice-type
+                  [::t-operator/transport-operator #{::t-operator/name}]}
                 {}
-                {:specql.core/order-by ::modification/created}))
+                {:specql.core/order-by ::modification/created
+                 :specql.core/order-direction :desc}))
+
+(defn fetch-notice [db id]
+  (first (specql/fetch db ::transit/pre-notice
+                       (specql/columns ::transit/pre-notice)
+                       {::transit/id id})))
+
+(defn add-comment [db user {:keys [id comment]}]
+  (specql/insert! db ::transit/pre-notice-comment
+                  (modification/with-modification-fields
+                    {::transit/comment comment
+                     ::transit/pre-notice-id id}
+                    ::transit/id user)))
 
 (defn authority-pre-notices-routes [db]
   (routes
    (GET "/pre-notices/authority-list" {user :user :as req}
         (authorization/with-transit-authority-check
           db user
-          #(http/no-cache-transit-response (list-published-notices db))))))
+          #(http/no-cache-transit-response (list-published-notices db user))))))
