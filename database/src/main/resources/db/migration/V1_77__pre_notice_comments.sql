fc5bf9d3736fca81bfdfc45fa3b41c87aab44792
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-04-12 19:09:13 +0300

    Add pre_notice_comment table

---
+++ database/src/main/resources/db/migration/V1_77__pre_notice_comments.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pre_notice_comment (
+  id SERIAL PRIMARY KEY,
+  "pre-notice-id" INTEGER REFERENCES pre_notice (id),
+  "created-by" TEXT REFERENCES "user" (id),
+  created TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  "comment" TEXT
+);
