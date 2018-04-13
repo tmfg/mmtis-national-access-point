6426482cb556bfd8786b51683138e120150a2858
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-04-13 14:56:38 +0300

    Remove quotes

--- database/src/main/resources/db/migration/V1_77__pre_notice_comments.sql
+++ database/src/main/resources/db/migration/V1_77__pre_notice_comments.sql
@@ -3,5 +3,5 @@ CREATE TABLE pre_notice_comment (
   "pre-notice-id" INTEGER REFERENCES pre_notice (id),
   "created-by" TEXT REFERENCES "user" (id),
   created TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  "comment" TEXT
+  comment TEXT
 );
