743810fe2df5e87e696807d97973af3bdd764643
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-04-13 13:36:09 +0300

    Add pre notice related styles ns

---
+++ ote/src/cljs/ote/style/pre_notice.cljs
@@ -0,0 +1,6 @@
+(ns ote.style.pre-notice
+  "Styles for pre notice related things"
+  (:require [stylefy.core :as stylefy]))
+
+(def comment-container {:margin-top "1em"})
+(def comment-style {:margin-top "1em"})
