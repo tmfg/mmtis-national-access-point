aecd440f3a598dbe818ac876b90cde8fb3c903d0
Toni Dahl <toni.dahl@solita.fi>
2018-04-19 17:03:01 +0300

    First draft of pre-notices html email notification template.

--- ote/src/clj/ote/tasks/pre_notices.clj
+++ ote/src/clj/ote/tasks/pre_notices.clj
@@ -3,10 +3,12 @@
             [jeesql.core :refer [defqueries]]
             [com.stuartsierra.component :as component]
             [taoensso.timbre :as log]
-            [ote.email :refer [send-email]]
-            [ote.db.tx :as tx]
             [clj-time.core :as t]
-            [clj-time.periodic :refer [periodic-seq]])
+            [clj-time.periodic :refer [periodic-seq]]
+            [clojure.string :as str]
+            [hiccup.core :refer [html]]
+            [ote.email :refer [send-email]]
+            [ote.db.tx :as tx])
   (:import (org.joda.time DateTimeZone)))
 
 (defqueries "ote/nap/users.sql")
@@ -15,33 +17,88 @@
 (def daily-notify-time (t/from-time-zone (t/today-at 8 15)
                                          (DateTimeZone/forID "Europe/Helsinki")))
 
-(defn generate-notification [db]
+(defn PgArray->str [arr]
+  (if arr
+    (str/join ", " (.getArray arr))
+    "-"))
+
+(defn pre-notice-row [{:keys [id regions operator-name pre-notice-type route-description created modified :as pre-notice]}]
+  [:tr
+   [:td [:b id]]
+   [:td (PgArray->str regions)]
+   [:td operator-name]
+   [:td (PgArray->str pre-notice-type)]
+   [:td "Muutoksen ensimmäinen voimaantulopäivä??? (Tämä vielä epäselvä)"]
+   [:td route-description]])
+
+(defn notification-template [pre-notices]
+  [:html
+   [:head
+    [:style
+     "table { border-collapse: collapse; font-size: 10px; }
+      table,td,tr,th { border: 1px solid black; }
+      td,tr,th { padding: 5px; }"]]
+   [:body
+    [:p [:b "Herätesähköposti 1krt/vrk –  60pv muutosilmoitukset"]]
+    [:br]
+    [:p [:b "Lähettäjä: NAP"]]
+    [:p [:b "OTSIKKO: Uudet 60 päivän muutosilmoitukset NAP:ssa xx.xx.xxxx"]]
+    [:p [:b "SISÄLTÖ:"]]
+    [:p [:b "Uudet 60 päivän muutosilmoitukset NAP:ssa"]]
+    [:ul
+     [:li
+      "Muutosilmoitukset on listattu voimaantulopäivämäärän mukaisesti."]
+     [:li
+      "Pääset tarkastelemaan muutosilmoitusta NAP:ssa klikkaamalla reitin nimeä taulukossa."]]
+    [:table
+     [:tbody
+      [:tr
+       [:th [:b "#"]]
+       [:th [:b "Alue"]]
+       [:th [:b "Palveluntuottajan nimi"]]
+       [:th [:b "Muutoksen tyyppi"]]
+       [:th [:b "Muutoksen ensimmäinen voimaantulopäivä"]]
+       [:th [:b "Reitin nimi"]]]
+      (doall
+        (for [n pre-notices]
+          (pre-notice-row n)))]]
+    [:br]
+    [:p "Tämän viestin lähetti NAP."]
+    [:p "Ongelmia? Ota yhteys NAP-Helpdeskiin," [:br]
+     [:a
+      {:href "mailto:joukkoliikenne@liikennevirasto.fi"}
+      "joukkoliikenne@liikennevirasto.fi"]
+     [:span " tai 0295 34 3434 (arkisin 10-16)"]]]])
+
+
+(defn notificiation-html [db]
   (try
     (when-let [notices (fetch-pre-notices-by-interval db {:interval "1 day"})]
-      (println notices))
+      (html (notification-template notices)))
     (catch Exception e
-      (log/warn "Error while fetching pre-notices:" e))))
+      (log/warn "Error while generating notification html:" e))))
 
 (defn send-notification! [db]
   (tx/with-transaction
     db
     (let [users (list-users db {:transit-authority? true :email nil :name nil})
-          notification (generate-notification db)]
-      (println users)
-      #_(try
-          (send-email {:to "test.testerson@notanymail.com"
-                       :from "nope@localhost"
-                       :subject "testing"
-                       :body "Testing testing"})
-          (catch Exception e
-            (log/warn "Error while sending a notification" e))))))
+          emails (mapv #(:email %) users)
+          notification (notificiation-html db)]
+      (println emails)
+      (try
+        #_(send-email {:bcc "nomailatall@nope.com"
+                     :from "nope@localhost"
+                     :subject "testing"
+                     :body [{:type "text/html" :content notification}]})
+        (catch Exception e
+          (log/warn "Error while sending a notification" e))))))
 
 
 (defrecord PreNoticesTasks [at]
   component/Lifecycle
   (start [{db :db :as this}]
     (assoc this
-      ::stop-tasks [(chime-at (drop 1 (periodic-seq at (t/seconds 10)))
+      ::stop-tasks [(chime-at (drop 1 (periodic-seq at (t/seconds 40)))
                               (fn [_]
                                 (#'send-notification! db)))]))
   (stop [{stop-tasks ::stop-tasks :as this}]
--- ote/src/clj/ote/tasks/pre_notices.sql
+++ ote/src/clj/ote/tasks/pre_notices.sql
@@ -1,5 +1,11 @@
 -- name: fetch-pre-notices-by-interval
-SELECT regions, "pre-notice-type", "route-description", created, modified, "transport-operator-id"
-  FROM "pre_notice"
+SELECT id, "pre-notice-type", "route-description", created, modified,
+  (SELECT array_agg(fr.nimi)
+    FROM "finnish_regions" fr
+   WHERE n.regions IS NOT NULL AND fr.numero = ANY(n.regions)) as "regions",
+  (SELECT op.name
+     FROM "transport-operator" op
+    WHERE op.id = n."transport-operator-id") as "operator-name"
+  FROM "pre_notice" n
  WHERE "pre-notice-state" = 'sent' AND
        (created > (current_timestamp - :interval::interval) OR modified > (current_timestamp - :interval::interval));
