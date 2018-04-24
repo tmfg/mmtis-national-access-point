dcc734f30153ecc8dfcaf960273f5e0bcdedcdd4
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-04-24 16:03:11 +0300

    Simplify validator

--- nap/docker/ckan/ckan-plugins/ckanext-napote_theme/ckanext/napote_theme/validators.py
+++ nap/docker/ckan/ckan-plugins/ckanext-napote_theme/ckanext/napote_theme/validators.py
@@ -12,13 +12,8 @@ Invalid = df.Invalid
 
 def email_uniq_validator(value, context):
     model = context['model']
-
     username = context['user']
-
-    try:
-        user_obj =  context['user_obj']
-    except KeyError:
-        user_obj = None
+    user_obj = context.get('user_obj')
 
     if user_obj == None or (username == user_obj.name and user_obj.email != value):
         # Check if editing self and email has changed or registering new user
