5242b8875660089c3f61254bb55ad42fe6357206
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-04-28 11:03:47 +0300

    Improve documentation

--- ote/src/clj/ote/db/lock.clj
+++ ote/src/clj/ote/db/lock.clj
@@ -5,7 +5,15 @@
 (defqueries "ote/db/lock.sql")
 
 (defmacro try-with-lock
-  "Run body with lock, if it is not already locked."
+  "Run body with lock, if it is not already locked.
+  If lock is already locked, body will not run and nil is returned.
+
+  The lock-name is a short human readable name for the lock.
+
+  Timelimit is the amount of seconds after which the lock is considered stale.
+  If a lock is in a locked state but it was locked more than timelimit seconds ago,
+  it will be acquired. This prevents a deadlock where one process crashes while
+  holding the lock."
   [db lock-name timelimit & body]
   `(let [db# ~db
          lock-name# ~lock-name
