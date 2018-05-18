b2f5236f5b23ce8276505ec6be29decf51a9b771
Toni Dahl <toni.dahl@solita.fi>
2018-05-18 14:56:17 +0300

    Add radiobuttons for choosing between :same and :diff transit day highlights.

--- ote/src/cljs/ote/app/controller/transit_visualization.cljs
+++ ote/src/cljs/ote/app/controller/transit_visualization.cljs
@@ -25,7 +25,8 @@
                       []
                       (vec
                        (range (reduce min (map (comp time/year :date) dates))
-                              (inc (reduce max (map (comp time/year :date) dates)))))))
+                              (inc (reduce max (map (comp time/year :date) dates))))))
+             :highlight {:mode :same})
       (dissoc :loading?)))
 
 (define-event LoadOperatorDates [operator-id]
@@ -34,12 +35,18 @@
              {:on-success (tuck/send-async! ->LoadOperatorDatesResponse)})
   true)
 
+(define-event SetHighlightMode [mode]
+  {:path [:transit-visualization :highlight]}
+  (-> app
+      (assoc :mode mode)))
+
 (defmethod routes/on-navigate-event :transit-visualization [_]
   (->LoadOperatorDates 1))
 
 (define-event HighlightHash [hash day]
   {:path [:transit-visualization :highlight]}
-  {:hash hash :day day})
+  (-> app
+      (merge {:hash hash :day day})))
 
 (define-event RoutesForDatesResponse [date1 date2 routes]
   {:path [:transit-visualization :compare]}
@@ -64,6 +71,7 @@
       (do
         (comm/get! (str "transit-visualization/routes-for-dates")
                    {:params (select-keys app [:date1 :date2])
-                    :on-success (tuck/send-async! ->RoutesForDatesResponse date1 date2)})
+                    ;:on-success (tuck/send-async! ->RoutesForDatesResponse date1 date2)
+                    })
         (assoc app :loading? true))
       app)))
--- ote/src/cljs/ote/views/transit_visualization.cljs
+++ ote/src/cljs/ote/views/transit_visualization.cljs
@@ -6,29 +6,46 @@
             [ote.app.controller.transit-visualization :as tv]
             [taoensso.timbre :as log]
             [ote.time :as time]
+            [cljs-react-material-ui.reagent :as ui]
             [ote.ui.table :as table]))
 
-(defn day-style [hash->color date->hash highlight day selected?]
+(defn highlight-style [hash->color date->hash day highlight]
   (let [d (time/format-date day)
         d-hash (date->hash d)
         hash-color (hash->color (date->hash d))
         hover-day (:day highlight)
-        hover-hash (:hash highlight)]
+        hover-hash (:hash highlight)
+        mode (or (:mode highlight) :same)]
+
+    (when hash-color
+      (case mode
+        :same (if (and (= d-hash hover-hash) (not (= d (time/format-date hover-day))))
+                {:box-shadow "inset 0 0 0 2px black, inset 0 0 0 100px rgba(255,255,255,.5)"}
+                (if (= d (time/format-date hover-day))
+                  {:box-shadow "inset 0 0 0 2px black, inset 0 0 0 100px rgba(255,255,255,.75)"}
+                  {:box-shadow "inset 0 0 0 2px transparent, inset 0 0 0 100px rgba(0,0,0,.25)"}))
+        :diff (if (and (not (= d-hash hover-hash))
+                       (= (time/day-of-week day) (time/day-of-week hover-day)))
+                {:box-shadow "inset 0 0 0 2px crimson, inset 0 0 0 3px black, inset 0 0 0 100px rgba(255,255,255,.65)"}
+                (if (and (= d-hash hover-hash) (= d (time/format-date hover-day)))
+                  {:box-shadow "inset 0 0 0 2px black,
+                                inset 0 0 0 4px transparent,
+                                inset 0 0 0 100px rgba(255,255,255,.75)"}
+                  {:box-shadow "inset 0 0 0 2px transparent,
+                                inset 0 0 0 4px transparent,
+                                inset 0 0 0 100px rgba(0,0,0,.25)"}))))))
+
+(defn day-style [hash->color date->hash highlight day selected?]
+  (let [d (time/format-date day)
+        hash-color (hash->color (date->hash d))]
 
     (merge
      {:background-color hash-color
       :color "rgb (0, 255, 255)"
       :transition "box-shadow 0.25s"
       :box-shadow "inset 0 0 0 2px transparent, inset 0 0 0 100px transparent"}
-     (when hover-hash
-       (if (and (not (= d-hash hover-hash))
-                (= (time/day-of-week day) (time/day-of-week hover-day))
-                hash-color)
-         {;:background (str "repeating-linear-gradient(-45deg," hash-color "," hash-color ",6px, black 3px, black 7px)")
-          :box-shadow "inset 0 0 0 2px black, inset 0 0 0 100px rgba(255,255,255,.5)"}
-         (if (and (= d-hash hover-hash) (= d (time/format-date hover-day)))
-           {:box-shadow "inset 0 0 0 1px red, inset 0 0 0 100px rgba(255,255,255,.5)"}
-           {:box-shadow "inset 0 0 0 2px transparent, inset 0 0 0 100px rgba(0,0,0,.25)"}))))))
+     (when (:hash highlight)
+       (highlight-style hash->color date->hash day highlight)))))
 
 (defn hover-day [e! date->hash day]
   (e! (tv/->HighlightHash (date->hash (time/format-date day)) day)))
@@ -54,6 +71,24 @@
    "tähän palvelun valinta"
    (when (and (not loading?) hash->color)
      [:div.transit-visualization
+      [:div
+       [ui/radio-button-group {:name "select-highlight-mode"
+                               :on-change #(e! (tv/->SetHighlightMode (keyword %2)))
+                               :value-selected (:mode highlight)
+                               :style {:display "flex" :justify-content "flex-start" :flex-direction "row wrap"}}
+        [ui/radio-button {:label "Näytä samanlaiset"
+                          :value :same
+                          :style {:white-space "nowrap"
+                                  :width "auto"
+                                  :margin-right "20px"
+                                  :font-size "12px"
+                                  :font-weight "bold"}}]
+        [ui/radio-button {:label "Näytä poikkeukset"
+                          :value :diff
+                          :style {:white-space "nowrap"
+                                  :width "auto"
+                                  :font-size "12px"
+                                  :font-weight "bold"}}]]]
       [service-calendar/service-calendar {:selected-date? (constantly false)
                                           :on-select (r/partial select-day e!)
                                           :on-hover (r/partial hover-day e! date->hash)
