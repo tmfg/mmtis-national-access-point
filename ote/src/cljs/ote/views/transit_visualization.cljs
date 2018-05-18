d9960009eb4a26c4aeea32824d4ecf8d72f5cf3d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-05-18 09:18:05 +0300

    Minor code cleanup

--- ote/src/cljs/ote/views/transit_visualization.cljs
+++ ote/src/cljs/ote/views/transit_visualization.cljs
@@ -7,7 +7,24 @@
             [taoensso.timbre :as log]
             [ote.time :as time]))
 
+(defn day-style [hash->color date->hash highlight day selected?]
+  (let [d (time/format-date day)
+        hash-color (hash->color (date->hash d))]
+    (merge
+     {:background-color hash-color
+      :color "rgb (0, 255, 255)"
+      :transition "box-shadow 0.25s"
+      :box-shadow "inset 0 0 0 2px transparent, inset 0 0 0 100px transparent"}
+     (if (and highlight (= (date->hash d) highlight))
+       {;:background (str "repeating-linear-gradient(-45deg," hash-color "," hash-color ",6px, black 3px, black 7px)")
+        :box-shadow "inset 0 0 0 2px black, inset 0 0 0 100px rgba(255,255,255,.5)"}
+       (when highlight
+         {:box-shadow "inset 0 0 0 2px transparent, inset 0 0 0 100px rgba(0,0,0,.25)"})))))
 
+(defn hover-day [e! date->hash day]
+  (e! (tv/->HighlightHash (date->hash (time/format-date day)))))
+
+(defn select-day [e! day])
 
 (defn transit-visualization [e! {:keys [transit-visualization] :as app}]
   (let [hash->color (:hash->color transit-visualization)
@@ -16,25 +33,10 @@
      (when (and (not (:loading transit-visualization))
                 (:hash->color transit-visualization))
        [service-calendar/service-calendar {:selected-date? (constantly false)
-                                           :on-select (r/partial e! :D)
-                                           :on-hover #(e! (tv/->HighlightHash (date->hash (time/format-date %))))
-                                           :day-style (fn [day selected?]
-                                                        (let [d (time/format-date day)
-                                                              hash-color (hash->color (date->hash d))
-                                                              highlight (:highlight transit-visualization)]
-                                                          (merge
-                                                            {:background-color hash-color
-                                                             :color "rgb (0, 255, 255)"
-                                                             :transition "box-shadow 0.25s"
-                                                             :box-shadow "inset 0 0 0 2px transparent,
-                                                                          inset 0 0 0 100px transparent"}
-                                                            (if (and highlight (= (date->hash d) highlight))
-                                                              {;:background (str "repeating-linear-gradient(-45deg," hash-color "," hash-color ",6px, black 3px, black 7px)")
-                                                               :box-shadow "inset 0 0 0 2px black,
-                                                                            inset 0 0 0 100px rgba(255,255,255,.5)"}
-                                                              (when highlight
-                                                                {:box-shadow "inset 0 0 0 2px transparent,
-                                                                              inset 0 0 0 100px rgba(0,0,0,.25)"})))))
+                                           :on-select (r/partial select-day e!)
+                                           :on-hover (r/partial hover-day e! date->hash)
+                                           :day-style (r/partial day-style hash->color date->hash
+                                                                 (:highlight transit-visualization))
                                            :years [2017 2018]}])
 
      ]))
