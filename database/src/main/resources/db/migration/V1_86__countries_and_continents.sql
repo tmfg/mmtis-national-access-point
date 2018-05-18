7bc9458139a690d7f604a43843380d1fcf74feec
Markus Sakkinen <markus.sakkinen@solita.fi>
2018-05-18 14:55:24 +0300

    Add contents to places search.

--- database/src/main/resources/db/migration/V1_86__countries_and_continents.sql
+++ database/src/main/resources/db/migration/V1_86__countries_and_continents.sql
@@ -8,3 +8,33 @@ CREATE TABLE continent (
   nameeng TEXT,
   location geometry
 );
+
+CREATE OR REPLACE VIEW places AS
+ SELECT CONCAT('finnish-municipality-', natcode) AS id,
+        'finnish-municipality' as type,
+        namefin, nameswe, ST_FlipCoordinates(location) as location
+   FROM finnish_municipalities
+UNION ALL
+ SELECT CONCAT('finnish-postal-',posti_alue) as id,
+        'finnish-postal' as type,
+        CONCAT(posti_alue,' ',nimi) AS namefin,
+        CONCAT(posti_alue,' ',namn) AS nameswe,
+        location
+   FROM finnish_postal_codes
+UNION ALL
+ SELECT CONCAT('finnish-region-',numero) as id,
+        'finnish-region' AS type,
+        nimi AS namefin,
+        '' AS nameswe,
+        location
+   FROM finnish_regions
+UNION ALL
+ SELECT CONCAT('country-',code) as id,
+        'country' AS type,
+        namefin, nameswe, location
+   FROM country
+UNION ALL
+ SELECT CONCAT('continent-',code) as id,
+        'continent' AS type,
+        namefin, nameswe, location
+   FROM continent;
