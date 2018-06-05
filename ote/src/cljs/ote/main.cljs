0b806d93475717dbbdaa902d76fd2c95737a171d
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-06-05 08:18:53 +0300

    Remove null checks

--- ote/src/cljs/ote/main.cljs
+++ ote/src/cljs/ote/main.cljs
@@ -38,10 +38,8 @@
 
 (defn- load-embedded-user-info []
   (let [elt (.getElementById js/document "ote-user-info")
-        user-info (when elt
-                    (transit/transit->clj (.-innerText elt)))]
-    (when elt
-      (.removeChild (.-parentNode elt) elt))
+        user-info (transit/transit->clj (.-innerText elt))]
+    (.removeChild (.-parentNode elt) elt)
     user-info))
 
 (defn ^:export main []
