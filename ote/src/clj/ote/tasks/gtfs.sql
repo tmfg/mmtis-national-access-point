e1465fc62bd6452a4112c20a3e0d044a1dbd2fbc
Markus Sakkinen <markus.sakkinen@solita.fi>
2018-06-12 19:19:38 +0300

    Clean blacklisted operator from db results.

--- ote/src/clj/ote/tasks/gtfs.sql
+++ ote/src/clj/ote/tasks/gtfs.sql
@@ -1,14 +1,15 @@
 -- name: select-gtfs-urls-update
 SELECT eid.id as id, (eid."external-interface").url, eid.format[1], eid.license,
        ts."transport-operator-id" as "operator-id", ts.id as "ts-id", eid."gtfs-imported" as "last-import-date"
-  FROM "transport-service"  ts, "external-interface-description"  eid
+  FROM "external-interface-description"  eid
+  JOIN "transport-service" ts ON eid."transport-service-id" = ts.id
+  JOIN "transport-operator" top ON  top.id = ts."transport-operator-id"
  WHERE ts."published?" = TRUE
-   AND ts.id = eid."transport-service-id"
+   AND top.id NOT IN (:blacklist)
    AND ('GTFS' = ANY(eid.format) OR 'Kalkati.net' = ANY(eid.format))
    AND ( "gtfs-imported" < (current_timestamp - '1 day'::interval) OR "gtfs-imported" IS NULL)
  ORDER BY "gtfs-imported" ASC LIMIT 1
    FOR UPDATE SKIP LOCKED;
-
 
 -- name: refresh-nightly-transit-changes!
 REFRESH MATERIALIZED VIEW "nightly-transit-changes";
