0f9c3c126742f330c44160b4d4bd718bee5d41f7
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-06-21 08:54:01 +0300

    Remove second "add new service" button

--- ote/src/cljs/ote/views/front_page.cljs
+++ ote/src/cljs/ote/views/front_page.cljs
@@ -27,7 +27,7 @@
             [ote.ui.form-fields :as form-fields]
             [ote.ui.common :as ui-common]
             [ote.views.transport-operator :as t-operator-view]
-            [ote.ui.list_header :as list-header]
+            [ote.ui.list-header :as list-header]
             [clojure.string :as str]))
 
 (defn- delete-service-action [e! {::t-service/keys [id name]
@@ -125,12 +125,6 @@
    (warn-about-test-server)
    [list-header/header
     (tr [:common-texts :own-api-list])
-    [ui/raised-button {:label (tr [:buttons :add-transport-service])
-                       :on-click #(do
-                                    (.preventDefault %)
-                                    (e! (ts/->OpenTransportServiceTypePage)))
-                       :primary  true
-                       :icon (ic/content-add)}]
     [t-operator-view/transport-operator-selection e! state true]]
     [:div.row
     [:div {:class "col-xs-12 col-md-12"}
