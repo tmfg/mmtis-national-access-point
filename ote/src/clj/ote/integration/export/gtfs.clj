52873ad7abf1a2bf367b278b83748caabeccd6e6
Toni Dahl <toni.dahl@solita.fi>
2018-07-19 16:19:11 +0300

    Remove available-from and available-to restrictions from current-routes fn.

--- ote/src/clj/ote/integration/export/gtfs.clj
+++ ote/src/clj/ote/integration/export/gtfs.clj
@@ -39,11 +39,7 @@
            #{::transit/id ::transit/name ::transit/stops ::transit/trips
              ::transit/route-type ::transit/service-calendars}
            {::transit/transport-operator-id transport-operator-id
-            ::transit/published? true
-            ::transit/available-from (op/or op/null?
-                                            (op/< now))
-            ::transit/available-to (op/or op/null?
-                                          (op/> now))})))
+            ::transit/published? true})))
 
 (defn routes-gtfs-zip
   [transport-operator routes output-stream]
