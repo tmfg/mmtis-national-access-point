160e2ed07e158718314dcfbf6eebaafde87dcb8f
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-09-20 08:41:26 +0300

    Send notification emails to users separately by the regions that they have selected from email notification settings.

--- ote/src/clj/ote/tasks/pre_notices.clj
+++ ote/src/clj/ote/tasks/pre_notices.clj
@@ -6,6 +6,7 @@
             [clj-time.core :as t]
             [clj-time.format :as format]
             [clj-time.coerce :as coerce]
+            [clj-time.periodic :refer [periodic-seq]]
             [clojure.string :as str]
             [hiccup.core :refer [html]]
             [hiccup.util :refer [escape-html]]
@@ -94,47 +95,49 @@
      [:span " tai 0295 34 3434 (arkisin 10-16)"]]]])
 
 
-(defn notification-html [db]
+(defn user-notification-html
+  "Every user can have their own set of notifications. Return notification html based on regions."
+  [db regions]
   (try
-    (when-let [notices (fetch-pre-notices-by-interval db {:interval "1 day"})]
+    (when-let [notices (fetch-pre-notices-by-interval-and-regions db {:interval "1 day" :regions regions})]
       (if-not (empty? notices)
         (do
-          (log/info "Found" (count notices) "new pre-notices from 24 hours.")
+          (log/info "Found" (count notices) "new pre-notices from 24 hours for regions " regions)
           (html (notification-template notices)))
         (log/info "No new pre-notices found.")))
 
     (catch Exception e
-      (log/warn "Error while generating notification html:" e))))
+      (log/warn "Error while generating notification html for regions: " regions " ERROR: " e ))))
 
 (defn send-notification! [db {server-opts :server msg-opts :msg :as email-opts}]
   (log/info "Starting pre-notices notification task...")
 
   (lock/try-with-lock
-   db "pre-notice-email" 300
-   (localization/with-language
-     "fi"
-     (tx/with-transaction db
-       (let [users (nap-users/list-users db {:transit-authority? true :email nil :name nil :group nil})
-             emails (mapv #(:email %) users)
-             notification (notification-html db)]
+    db "pre-notice-email" 300
+    (localization/with-language
+      "fi"
+      (tx/with-transaction db
+         (let [authority-users (nap-users/list-authority-users db)] ;; Authority users
+           (try
+             (when (not (empty? authority-users))
+               (doseq [u authority-users]
+                 (let [notification (user-notification-html db (:finnish-regions u))]
+                   (if notification
+                     (do
+                       (log/info "Trying to send a pre-notice email to: " (pr-str (:email u)))
+                       (send-email
+                         server-opts
+                         {:bcc     (:email u)
+                          :from    (or (:from msg-opts) "NAP")
+                          :subject (str "Uudet 60 päivän muutosilmoitukset NAP:ssa "
+                                        (datetime-string (t/now) timezone))
+                          :body    [{:type "text/html;charset=utf-8" :content notification}]}))
+                     (log/info "Could not find notification for user with email: " (pr-str (:email u)))))))
 
-         (try
-           (when (and (not (empty? emails)) notification)
-             (doseq [emails (partition-all 50 emails)]
-               (log/info "Trying to send a pre-notice email to: " (pr-str emails))
-
-               (send-email
-                server-opts
-                {:bcc emails
-                 :from (or (:from msg-opts) "NAP")
-                 :subject (str "Uudet 60 päivän muutosilmoitukset NAP:ssa "
-                               (datetime-string (t/now) timezone))
-                 :body [{:type "text/html;charset=utf-8" :content notification}]})))
-
-           ;; Sleep for 5 seconds to ensure that no other nodes are trying to send email at the same mail.
-           (Thread/sleep 5000)
-           (catch Exception e
-             (log/warn "Error while sending a notification" e))))))))
+             ;; Sleep for 5 seconds to ensure that no other nodes are trying to send email at the same mail.
+             (Thread/sleep 5000)
+             (catch Exception e
+               (log/warn "Error while sending a notification" e))))))))
 
 
 (defrecord PreNoticesTasks [email-config]
