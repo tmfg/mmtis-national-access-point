7c1d4cfd76649337c077525e0bf2e873083e8472
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-09-21 12:14:20 +0300

    Fix pre-notice sql query

--- ote/src/clj/ote/tasks/pre_notices.sql
+++ ote/src/clj/ote/tasks/pre_notices.sql
@@ -26,5 +26,5 @@ SELECT id, "pre-notice-type", "route-description", created, modified, descriptio
   FROM "pre_notice" n
  WHERE "pre-notice-state" = 'sent'
    AND sent IS NOT NULL
-   AND ((:regions::int[])[1] IS NULL OR (:regions::int[])[1] = ANY(n.regions::integer[]))
+   AND ((:regions::int[])[1] IS NULL OR (:regions::int[]) && n.regions::integer[])
    AND (sent > (current_timestamp - :interval::interval));
