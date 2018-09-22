cdcda1b0b04bab3ade9cb2d4bfd27f4c70395197
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-09-22 10:55:35 +0300

    Componentize email sending

--- ote/src/clj/ote/email.clj
+++ ote/src/clj/ote/email.clj
@@ -4,10 +4,31 @@
             [ote.nap.cookie :as nap-cookie]
             [ote.nap.users :as nap-users]
             [taoensso.timbre :as log]
-            [ote.localization :as localization]))
+            [ote.localization :as localization]
+            [com.stuartsierra.component :as component]))
 
+(defprotocol Send
+  (send! [this message]
+    "Send a single email.
+  Message can have the following keys.
 
-(defn send-email
+  Sender and recipient:
+  :from, :to, :cc, :bcc
+
+  Message subject and content:
+  :subject, :body"))
+
+(defn- send-email
   "Send a singular email using Postal."
   [& args]
   (apply postal/send-message args))
+
+(defrecord Email [email-opts]
+  component/Lifecycle
+  (start [this] this)
+  (stop [this] this)
+
+  Send
+  (send! [{{:keys [server msg]} :email-opts} message]
+    (send-email server
+                (merge msg message))))
