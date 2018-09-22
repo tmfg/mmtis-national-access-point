34fcd57fe4741959c32b458350d2a2662c31b065
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-09-22 10:57:18 +0300

    Add test for password reset request

--- ote/test/clj/ote/services/login_test.clj
+++ ote/test/clj/ote/services/login_test.clj
@@ -1,12 +1,20 @@
 (ns ote.services.login-test
   (:require [ote.services.login :as sut]
-            [clojure.test :as t]
+            [clojure.test :as t :refer [deftest is testing]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.properties :as prop]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [ote.test :refer [system-fixture http-post]]
+            [com.stuartsierra.component :as component])
   (:import (java.util Base64)))
 
+(t/use-fixtures :each
+  (system-fixture
+   :login (component/using
+           (sut/->LoginService {:shared-secret "test"
+                                :digest-algorithm "MD5"})
+           [:http :db :email])))
 
 (defspec base64<->hex
   (prop/for-all
@@ -27,3 +35,17 @@
    (let [buddy (sut/encrypt input-password)]
      (= buddy
         (sut/passlib->buddy (sut/buddy->passlib buddy))))))
+
+(deftest password-reset-request-sends-email
+  (let [response (http-post "request-password-reset" {:email "admin@napoteadmin123.com"
+                                                      :language "en"})
+        outbox @ote.test/outbox]
+    (is (= :ok (:transit response)))
+
+    (is (= 1 (count outbox)))
+    (testing "Email message"
+      (let [{:keys [to subject body]} (first outbox)]
+        (is (= to "admin@napoteadmin123.com"))
+        (is (= subject "NAP - Reset your password"))
+        (is (str/includes? body "admin"))
+        (is (str/includes? body "#reset-password?key="))))))
