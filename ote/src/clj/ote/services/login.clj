bbdd99b892bee9a0380977b569807d6d03daf3da
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-09-22 10:57:01 +0300

    Add backend route for requesting password reset

--- ote/src/clj/ote/services/login.clj
+++ ote/src/clj/ote/services/login.clj
@@ -10,12 +10,16 @@
             [ote.nap.users :as users]
             [ote.services.transport :as transport]
             [ote.components.service :refer [define-service-component]]
-            [ote.db.tx :refer [with-transaction]]
+            [ote.db.tx :as tx :refer [with-transaction]]
             [specql.core :as specql]
             [ote.db.user :as user]
             [ote.time :as time]
             [ote.util.feature :as feature]
-            [taoensso.timbre :as log])
+            [taoensso.timbre :as log]
+            [ote.db.modification :as modification]
+            [ote.localization :as localization :refer [tr]]
+            [ote.email :as email]
+            [ote.environment :as env])
   (:import (java.util UUID Base64 Base64$Decoder)))
 
 (defqueries "ote/services/login.sql")
@@ -206,8 +210,39 @@
       ;; Failed, return errors to form
       (http/transit-response result))))
 
+(defn request-password-reset! [db {:keys [email]}]
+  (tx/with-transaction db
+    (when-let [user (first (fetch-login-info db {:email email}))]
+      (log/info "User " (:id user) " requested password reset.")
+      {:user user
+       :password-reset-request (specql/insert! db ::user/password-reset-request
+                                               {::user/reset-key (java.util.UUID/randomUUID)
+                                                ::modification/created-by (:id user)})})))
+
+(defn send-password-reset-email [email {:keys [user password-reset-request]} language]
+  (log/debug "Sending password reset email to " user)
+  (localization/with-language language
+    (email/send! email
+                 {:to (:email user)
+                  :subject (tr [:email-templates :password-reset :subject])
+                  :body (tr [:email-templates :password-reset :body]
+                            {:name (:name user)
+                             :reset-link (str (env/base-url) "#reset-password?key="
+                                              (::user/reset-key password-reset-request))})})))
+
+(defn request-password-reset [db email form-data]
+  (try
+    (when-let [reset-request (request-password-reset! db form-data)]
+      (send-password-reset-email email reset-request (:language form-data)))
+    (catch Exception e
+      (log/error e "Unable to send password reset email")))
+  ;; Always send :ok back, no matter what
+  :ok)
+
+
 (define-service-component LoginService
-  {:fields [auth-tkt-config]}
+  {:fields [auth-tkt-config]
+   :dependencies {email :email}}
 
   ^:unauthenticated
   (POST "/login" {form-data :body}
@@ -230,4 +265,8 @@
   (POST "/save-user" {form-data :body
                       user :user}
         (#'save-user db auth-tkt-config (:user user)
-                     (http/transit-request form-data))))
+                     (http/transit-request form-data)))
+
+  ^{:unauthenticated true :format :transit}
+  (POST "/request-password-reset" {form-data :body}
+        (request-password-reset db email (http/transit-request form-data))))
