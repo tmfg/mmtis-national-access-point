8dea6ad96a00298f34c1184605b4d233d429729c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-09-27 12:47:42 +0300

    Remove lambda references

--- ote/src/clj/ote/integration/import/kalkati.clj
+++ ote/src/clj/ote/integration/import/kalkati.clj
@@ -6,34 +6,11 @@
             [clj-http.client :as http-client]
             [ote.integration.import.gtfs :as gtfs-import]
             [ote.db.transport-service :as t-service]
-            [amazonica.aws.lambda :as lambda]
             [cheshire.core :as cheshire]
             [clojure.walk :as walk]
             [taoensso.timbre :as log]
             [specql.core :as specql]
             [ote.gtfs.kalkati-to-gtfs :as kalkati-to-gtfs]))
-
-
-
-
-(defn kalkati-to-gtfs
-  "Invoke an kalkati_to_gtfs Lambda function directly through AWS SDK.
-  Returns InvokeResult."
-  [kalkati-url headers]
-  (let [headers (select-keys headers ["if-modified-since"])]
-    ()
-    (decode-payload
-     (lambda/invoke :function-name "kalkati_to_gtfs"
-                    :region "eu-central-1"
-                    :invocation-type "RequestResponse"
-                    ;; We also want to support invoking lambda functions through API Gateway lambda proxy
-                    ;; so we'll have to encode the :body separately.
-                    :payload (cheshire/encode {:body (cheshire/encode kalkati-url)
-                                               :headers headers})))))
-
-
-
-
 
 (defn load-kalkati [url headers]
   (let [resp (http-client/get url {:headers headers
