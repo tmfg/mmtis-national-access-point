117a863b90ddc381fb850cbbbec144ab8262e3c0
Toni Dahl <toni.dahl@solita.fi>
2018-10-02 15:20:40 +0300

    Truncate gtfs_package before each test.

--- ote/test/clj/ote/integration/import/gtfs_test.clj
+++ ote/test/clj/ote/integration/import/gtfs_test.clj
@@ -43,10 +43,12 @@
 (defn upsert-gtfs-package! [id date]
   (set
     (sql-execute!
-      "DELETE FROM gtfs_package WHERE id =" id ";"
       "INSERT INTO gtfs_package (id, \"transport-operator-id\", \"transport-service-id\", \"external-interface-description-id\", created)
        VALUES (" id ", 1, 2, 999, '" date "'::DATE)")))
 
+(defn truncate-gtfs-package-table! []
+  (sql-execute!
+    "TRUNCATE TABLE gtfs_package RESTART IDENTITY CASCADE"))
 
 (defn fetch-latest-transit-change []
   (first (sql-query
@@ -71,22 +73,27 @@
 
 
 (deftest save-gtfs-to-db
-  (import-gtfs "test1" 1000 "2018-01-02")
+  (truncate-gtfs-package-table!)
+  (import-gtfs "test1" 1000 (time/format-date-iso-8601 (time/now)))
 
   (let [gtfs-package (fetch-gtfs-package 1000)]
+
+    (println gtfs-package)
 
     (is (= 1000 (:id gtfs-package)))
     (is (not (nil? (:envelope gtfs-package))))
     (is (= 1 (:transport-operator-id gtfs-package)))
     (is (= 2 (:transport-service-id gtfs-package)))
     (is (= 999 (:external-interface-description-id gtfs-package)))
-    (is (= (time/date-fields (time/parse-date-iso-8601 "2018-01-02"))
+    (is (= (time/date-fields-only (time/now))
            (time/date-fields-only (time/native->date-time (:created gtfs-package)))))))
 
 
 (deftest transit-changes-no-changes
+  (truncate-gtfs-package-table!)
+  (import-gtfs "test1" 1000 (time/format-date-iso-8601 (time/now)))
   ;; Add a new package for the same service. Use the same test package -> no changes should be occurring.
-  (import-gtfs "test1" 1001 "2018-01-03")
+  (import-gtfs "test1" 1001 (time/format-date-iso-8601 (time/days-from (time/now) 14)))
 
   ;; Compute transit changes
   (upsert-service-transit-change (:db *ote*) {:service-id 2})
@@ -108,8 +115,10 @@
 
 
 #_(deftest transit-changes-has-changes
+  (truncate-gtfs-package-table!)
+  (import-gtfs "test1" 1000 (time/format-date-iso-8601 (time/now)))
   ;; Add a new package for the same service. Use the same test package -> no changes should be occurring.
-  (import-gtfs "test1_changes" 1002 "2018-01-04")
+  (import-gtfs "test1_changes" 1001 (time/format-date-iso-8601 (time/days-from (time/now) 14)))
 
   ;; Compute transit changes
   (upsert-service-transit-change (:db *ote*) {:service-id 2})
@@ -119,6 +128,9 @@
                                        {:category "A"
                                         :element-type :gtfs/route-change-info}
                                        (str (:route-changes latest-change)))]
+
+    (is (= 1 latest-change))
+    (is (= 1 route-changes))
 
     (is (= 1 (:removed-routes latest-change)))
     (is (= 1 (:added-routes latest-change)))
