af3b739b58a6571645ccd575de91b3fbeb6fa77e
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-10-23 15:50:42 +0300

    Update header elements

--- ote/src/cljs/ote/ui/list_header.cljs
+++ ote/src/cljs/ote/ui/list_header.cljs
@@ -1,12 +1,13 @@
 (ns ote.ui.list-header
   "Header component for OTE list pages"
   (:require [cljs-react-material-ui.reagent :as ui]
-            [ote.ui.common :as common]))
+            [ote.ui.common :as common]
+            [ote.style.base :as style-base]))
 
-(defn header [title add-button operator-selection user-guide-button]
+(defn header [app title add-button operator-selection user-guide-button]
   [:div
    (when title
-     [:div.row [:h1 title]])
+     [:div.row (when (< (:width app) style-base/mobile-width-px) {:style {:padding-top "20px"}}) [:h1 title]])
    (when user-guide-button
      [:div.row user-guide-button])
    (when operator-selection
