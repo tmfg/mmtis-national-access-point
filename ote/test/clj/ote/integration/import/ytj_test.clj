14d7f5a04935bb49c98eeebc5197a78d06ee497f
Erno Kuusela <erno-laptop!erno>
2018-11-14 09:24:39 +0200

    change name

--- ote/test/clj/ote/integration/import/ytj_test.clj
+++ ote/test/clj/ote/integration/import/ytj_test.clj
@@ -1,6 +1,6 @@
 (ns ote.integration.import.ytj-test
   (:require  [clojure.test :as t]
-             [ote.integration.import.ytj :as sut]
+             [ote.integration.import.ytj :as ytj]
              [com.stuartsierra.component :as component]
              [ote.test :as ote-test]
              [cheshire.core :refer [encode]]
@@ -8,7 +8,7 @@
 
 (t/use-fixtures :each
   (ote-test/system-fixture
-   :operators (component/using (sut/->YTJFetch) [:db :http])))
+   :operators (component/using (ytj/->YTJFetch) [:db :http])))
 
 (t/deftest expired-removal
   (let [test-input {:foo [{:stuff 1 :endDate nil}
@@ -18,12 +18,12 @@
         reference-output {:foo [{:stuff 1, :endDate nil}
                                 {:stuff 3, :endDate "2077-12-21"}]
                           :bar {:stuff 4 :endDate "12-12-12"}}]
-    (t/is (= reference-output (sut/without-expired-items test-input)))))
+    (t/is (= reference-output (ytj/without-expired-items test-input)))))
 
 
 ;; remember to eval whole ns along with fixture
 (t/deftest request-made
-  (with-redefs [sut/http-get (fn [url opts]
+  (with-redefs [ytj/http-get (fn [url opts]
                                {:status 200
                                 :headers {"Content-Type" "application/json"}
                                 :body {:totalResults "1"
