f783bad8a968c3c7786ada191bb6307b1e35a0c5
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-11-27 15:28:25 +0200

    Add stored procedure before it is called

--- database/src/main/resources/db/migration/V1_122__add_route_hash_ids_to_all_packages.sql
+++ database/src/main/resources/db/migration/V1_122__add_route_hash_ids_to_all_packages.sql
@@ -1,3 +1,26 @@
+
+-- R file is handled after these V files. So We need to add this function twice once in here on once to R file
+CREATE OR REPLACE FUNCTION calculate_route_hash_id_using_headsign(package_id INTEGER)
+RETURNS VOID
+AS $$
+BEGIN
+
+  DELETE FROM "detection-route" WHERE "package-id" = package_id;
+
+  INSERT INTO "detection-route" ("gtfs-route-id", "package-id", "route-id", "route-short-name", "route-long-name", "route-hash-id", "trip-headsign")
+    SELECT r.id, r."package-id", r."route-id", r."route-short-name", r."route-long-name",
+           concat(trim(r."route-short-name"), '-', trim(r."route-long-name"), '-', trim(trip."trip-headsign")), trip."trip-headsign"
+     FROM "gtfs-route" r
+     JOIN "gtfs-trip" t ON (t."package-id" = r."package-id" AND r."route-id" = t."route-id")
+     JOIN LATERAL unnest(t.trips) trip ON true
+    WHERE r."package-id" = package_id
+    GROUP BY r.id, trip."trip-headsign";
+
+END
+$$ LANGUAGE plpgsql;
+
+
+
 -- Create function that generates routes to detection-route table using default route-hash-id (short-long-headsign)
 CREATE OR REPLACE FUNCTION generate_route_hash_ids_for_packages() RETURNS VOID AS $$
 DECLARE
