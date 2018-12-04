a9989c36d141f555df930575686cdd15d6bafad4
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-12-04 06:45:50 +0200

    Change queries to depend more to route-hash-id and less to route-long-name, route-short-name and trip-headsign

--- ote/src/clj/ote/services/transit_visualization.sql
+++ ote/src/clj/ote/services/transit_visualization.sql
@@ -73,8 +73,8 @@ SELECT trip."trip-id", trip."trip-headsign", stop."stop-name", stoptime."departu
  ORDER BY stoptime."stop-sequence") x
  GROUP BY x."trip-id", x."trip-headsign";
 
--- name: fetch-route-trips-by-name-and-date
--- Fetch geometries of route trips for given date by route short and long name
+-- name: fetch-route-trips-by-hash-and-date
+-- Fetch geometries of route trips for given date by route hash-id
 SELECT ST_AsGeoJSON(COALESCE(
           -- If there exists a "gtfs-shape" row for this package and trip shape id,
           -- use that to generate the detailed route line
@@ -91,14 +91,12 @@ SELECT ST_AsGeoJSON(COALESCE(
                ST_MakeLine(ST_MakePoint(stop."stop-lon", stop."stop-lat") ORDER BY stoptime."stop-sequence") as "route-line",
                string_agg(CONCAT(stop."stop-lon", ',', stop."stop-lat", ',', stop."stop-name"), '||' ORDER BY stoptime."stop-sequence") as stops,
                trip."shape-id", r."package-id"
-          FROM "gtfs-route" r
+          FROM "detection-route" r
           JOIN "gtfs-trip" t ON (r."package-id" = t."package-id" AND r."route-id" = t."route-id")
           JOIN LATERAL unnest(t.trips) trip ON TRUE
           JOIN LATERAL unnest(trip."stop-times") stoptime ON TRUE
           JOIN "gtfs-stop" stop ON (r."package-id" = stop."package-id" AND stoptime."stop-id" = stop."stop-id")
-         WHERE COALESCE(:route-short-name::VARCHAR,'') = COALESCE(r."route-short-name",'')
-           AND COALESCE(:route-long-name::VARCHAR,'') = COALESCE(r."route-long-name",'')
-           AND COALESCE(:trip-headsign::VARCHAR,'') = COALESCE(trip."trip-headsign",'')
+         WHERE COALESCE(:route-hash-id::VARCHAR,'') = COALESCE(r."route-hash-id",'')
            AND ROW(r."package-id", t."service-id")::service_ref IN
                (SELECT * FROM gtfs_services_for_date(gtfs_service_packages_for_date(:service-id::INTEGER, :date::DATE),
                           :date::DATE))
@@ -120,36 +118,8 @@ SELECT trip."package-id", (trip.trip)."trip-id",
  JOIN LATERAL unnest(rt.tripdata) trip ON TRUE
  JOIN LATERAL unnest((trip.trip)."stop-times") stoptime ON TRUE
  JOIN "gtfs-stop" s ON (s."package-id" = trip."package-id" AND s."stop-id" = stoptime."stop-id")
- WHERE COALESCE(:route-short-name::VARCHAR,'') = COALESCE(rt."route-short-name",'')
-   AND COALESCE(:route-long-name::VARCHAR,'') = COALESCE(rt."route-long-name",'')
-   AND COALESCE(:trip-headsign::VARCHAR,'') = COALESCE(rt."trip-headsign",'')
+ WHERE COALESCE(:route-hash-id::VARCHAR,'') = COALESCE(rt."route-hash-id",'')
  GROUP BY trip."package-id", (trip.trip)."trip-id";
-
--- name: fetch-date-hashes-for-route-with-route-names
--- Fetch the date/hash pairs for a given route
-WITH dates AS (
-  -- Calculate a series of dates from beginning of last year
-  -- to the end of the next year.
-  SELECT ts::date AS date
-    FROM generate_series(
-            (date_trunc('year', CURRENT_DATE) - '1 year'::interval)::date,
-            (date_trunc('year', CURRENT_DATE) + '2 years'::interval)::date,
-            '1 day'::interval) AS g(ts)
-)
-SELECT x.date::text, string_agg(x.hash,' ' ORDER BY x.package_id) as hash
-  FROM (SELECT d.date, package_id, rh.hash::text
-          FROM dates d
-          -- Join packages for each date
-          JOIN LATERAL unnest(gtfs_service_packages_for_date(:service-id::INTEGER, d.date))
-            AS ps (package_id) ON TRUE
-          -- Join all date hashes for packages
-          JOIN "gtfs-date-hash" dh ON (dh."package-id" = package_id AND dh.date = d.date)
-          -- Join unnested per route hashes
-          JOIN LATERAL unnest(dh."route-hashes") rh ON TRUE
-         WHERE COALESCE(rh."route-short-name", '') = COALESCE(:route-short-name::VARCHAR, '')
-           AND COALESCE(rh."route-long-name", '') = COALESCE(:route-long-name::VARCHAR, '')
-           AND COALESCE(rh."trip-headsign", '') = COALESCE(:trip-headsign::VARCHAR, '')) x
- GROUP BY x.date;
 
 -- name: fetch-date-hashes-for-route-with-route-hash-id
 -- Fetch the date/hash pairs for a given route using route-hash-id which isn't used for all services
