36b59f353ae87aaa84ed58b5406059a153845596
Markus Vähäkangas <Markus.Vahakangas@solita.fi>
2018-12-04 06:46:42 +0200

    Update route-trips-for-date query to work using route-hash-id

--- ote/src/clj/ote/transit_changes/detection.sql
+++ ote/src/clj/ote/transit_changes/detection.sql
@@ -30,7 +30,7 @@ SELECT * FROM gtfs_service_routes_with_daterange(:service-id::INTEGER);
 SELECT t."package-id", trip."trip-id",
        stoptime."stop-id", stoptime."departure-time", stoptime."stop-sequence",
         stop."stop-name", stop."stop-lat", stop."stop-lon"
-  FROM "gtfs-route" r
+  FROM "detection-route" r
   JOIN "gtfs-trip" t ON (t."package-id" = r."package-id" AND r."route-id" = t."route-id")
   JOIN LATERAL unnest(t.trips) trip ON true
   JOIN LATERAL unnest(trip."stop-times") as stoptime ON TRUE
@@ -39,9 +39,7 @@ SELECT t."package-id", trip."trip-id",
    AND ROW(r."package-id", t."service-id")::service_ref IN
        (SELECT * FROM gtfs_services_for_date(
         (SELECT gtfs_service_packages_for_date(:service-id::INTEGER, :date::DATE)), :date::DATE))
-   AND COALESCE(r."route-short-name",'') = COALESCE(:route-short-name::TEXT,'')
-   AND COALESCE(r."route-long-name",'') = COALESCE(:route-long-name::TEXT,'')
-   AND COALESCE(trip."trip-headsign",'') = COALESCE(:trip-headsign::TEXT,'')
+   AND COALESCE(r."route-hash-id",'') = COALESCE(:route-hash-id::TEXT,'')
  ORDER BY t."package-id", trip."trip-id", stoptime."stop-sequence";
 
 -- name: generate-date-hashes
