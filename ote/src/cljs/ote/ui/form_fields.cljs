1a2105b451bdc3d622de498da0d1d2b34c46df97
Tommi Kenakkala <tommi.kenakkala@solita.fi>
2018-12-31 11:53:53 +0200

    ui: new form field for reagent/material divider component

--- ote/src/cljs/ote/ui/form_fields.cljs
+++ ote/src/cljs/ote/ui/form_fields.cljs
@@ -1047,4 +1047,7 @@
 (defmethod field :info-toggle [{:keys [label body default-state]}]
   [info/info-toggle label body default-state])
 
+(defmethod field :divider [{:keys [_]}]
+  [ui/divider])
+
 
