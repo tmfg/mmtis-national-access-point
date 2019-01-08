72e5f0990f09071031e9191acbc36598deb2ec8f
Tommi Kenakkala <tommi.kenakkala@solita.fi>
2019-01-08 15:50:13 +0200

    style: operation warning msg control
    A themed icon and an aligned text label, which may be omitted if necessary.

---
+++ ote/src/cljs/ote/ui/warning_msg.cljs
@@ -0,0 +1,11 @@
+(ns ote.ui.warning_msg
+  (:require [ote.localization :refer [tr tr-key]]
+            [cljs-react-material-ui.icons :as ic]
+            [ote.style.base :as style-base]))
+
+(defn warning-msg [content]
+  [:div
+   {:style (merge style-base/msg-container style-base/msg-warning)}
+   [ic/alert-warning {:style style-base/msg-warning}]
+   [:div {:style style-base/msg-item-column-margin} content]])
+
