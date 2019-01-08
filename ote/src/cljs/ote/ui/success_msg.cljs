b571cbd129ca10a18a0a93cfe986d94336cd0806
Tommi Kenakkala <tommi.kenakkala@solita.fi>
2019-01-08 15:50:42 +0200

    style: operation success msg control
    A themed icon and an aligned text label, which may be omitted if necessary.

---
+++ ote/src/cljs/ote/ui/success_msg.cljs
@@ -0,0 +1,11 @@
+(ns ote.ui.success_msg
+  (:require [ote.localization :refer [tr tr-key]]
+            [cljs-react-material-ui.icons :as ic]
+            [ote.style.base :as style-base]))
+
+(defn success-msg [content]
+  [:div
+   {:style (merge style-base/msg-container style-base/msg-success)}
+   [ic/action-check-circle {:style style-base/msg-success}]
+   [:div {:style style-base/msg-item-column-margin} content]])
+
