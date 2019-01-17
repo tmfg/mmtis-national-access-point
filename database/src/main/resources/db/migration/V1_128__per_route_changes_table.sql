a2423baa035a89cb7bc140418c314886b7abc702
Erno Kuusela <erno-laptop!erno>
2019-01-17 14:41:11 +0200

    Mostly working migration
    todo recheck route-hash-id null contraint (some rows in my dev db had this as null)

--- database/src/main/resources/db/migration/V1_128__per_route_changes_table.sql
+++ database/src/main/resources/db/migration/V1_128__per_route_changes_table.sql
@@ -1,34 +1,18 @@
--- napote=# \d "gtfs-route-change-info"
---           Composite type "public.gtfs-route-change-info"
---            Column           |           Type           | Modifiers 
--- ----------------------------+--------------------------+-----------
---  route-short-name           | text                     | 
---  route-long-name            | text                     | 
---  trip-headsign              | text                     | 
---  change-type                | "gtfs-route-change-type" | 
---  added-trips                | integer                  | 
---  removed-trips              | integer                  | 
---  trip-stop-sequence-changes | int4range                | 
---  trip-stop-time-changes     | int4range                | 
---  current-week-date          | date                     | 
---  different-week-date        | date                     | 
---  change-date                | date                     | 
---  route-hash-id              | text                     | 
-
 create table "gtfs-route-change" (
  "transit-change-date"        date not null,
  "transit-service-id"         integer not null references "transport-service" (id),
- "route-short-name"           text not null, 
+ "route-short-name"           text, 
  "route-long-name"            text not null, 
- "trip-headsign"              text not null, 
+ "trip-headsign"              text, 
  "change-type"                "gtfs-route-change-type", 
- "added-trips"                integer not null, 
- "removed-trips"              integer not null, 
- "trip-stop-sequence-changes" int4range not null,
- "trip-stop-time-changes"     int4range not null,
- "current-week-date"          date not null, 
- "different-week-date"        date not null, 
- "change-date"                date not null, 
+ "added-trips"                integer default 0, 
+ "removed-trips"              integer default 0, 
+ "trip-stop-sequence-changes" int4range,
+ "trip-stop-time-changes"     int4range,
+ "current-week-date"          date, 
+ "different-week-date"        date, 
+ "change-date"                date,
+ "created-date"               date,
  "route-hash-id"              text not null,
  foreign key ("transit-change-date", "transit-service-id") references "gtfs-transit-changes" ("date", "transport-service-id"));
 
@@ -36,17 +20,27 @@ do $$
  declare r record;
  c "gtfs-route-change-info";
 begin
- for r in select date as d "transport-service-id" as tsid, "route-changes" as rc from "gtfs-transit-changes" limit 10
+ for r in select date as d, "transport-service-id" as tsid, "route-changes" as rc from "gtfs-transit-changes" -- limit 10 -- for testing
  loop
-   foreach c in array rc."route-changes" -- todo: find working syntax to do this
+   foreach c in array r.rc
      loop
-       -- todo: insert statement here
-       raise notice 'd % c hs %', date, c."trip-headsign";
+       insert into "gtfs-route-change" values (r.d, r.tsid,
+       	      c."route-short-name",
+       	      c."route-long-name",
+	      c."trip-headsign",
+	      c."change-type",
+	      c."added-trips",
+	      c."removed-trips",
+	      c."trip-stop-sequence-changes",
+	      c."trip-stop-time-changes",
+	      c."current-week-date",
+	      c."different-week-date",
+      	      c."change-date",
+	      null,
+	      c."route-hash-id");
+
+       -- raise notice 'd % c hs %', r.d, c."trip-headsign";
      end loop;
   end loop;
 end
-$$;
-
--- todo: drop array column after data is copied to new table
-
-
+$$ language plpgsql;
