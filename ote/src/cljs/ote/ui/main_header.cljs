540248c90fa9e84fbf46eb3395d12a15ab7944f6
Tommi Kenakkala <tommi.kenakkala@solita.fi>
2019-02-14 08:34:51 +0200

    views: main header :user-menu-video-tutorials-link commented out
    For now videos are not available. Shall be restored when they are.

--- ote/src/cljs/ote/ui/main_header.cljs
+++ ote/src/cljs/ote/ui/main_header.cljs
@@ -216,11 +216,13 @@
            (merge (stylefy/use-style
                     style-topnav/topnav-dropdown-link)
                   {:target "_blank"})]]
-         [:li
-          [linkify (tr [:common-texts :user-menu-video-tutorials-link]) (tr [:common-texts :user-menu-video-tutorials])
-           (merge (stylefy/use-style
-                    style-topnav/topnav-dropdown-link)
-                  {:target "_blank"})]]
+
+         ;; TODO: commented out because for now there are no valid videos. Link shall be restored and updated when videos are available.
+         ;[:li
+         ; [linkify (tr [:common-texts :user-menu-video-tutorials-link]) (tr [:common-texts :user-menu-video-tutorials])
+         ;  (merge (stylefy/use-style
+         ;           style-topnav/topnav-dropdown-link)
+         ;         {:target "_blank"})]]
 
          [:li
           [linkify "https://github.com/finnishtransportagency/mmtis-national-access-point/blob/master/docs/api/README.md"
