115e5a0ca5282af93956c53952af15ac653bbda4
Matti Uusitalo <matti.uusitalo@solita.fi>
2019-03-20 12:50:03 +0200

    Merged multiple migration files to one

--- database/src/main/resources/db/migration/V1_142__Add_Spatial_Indices.sql
+++ database/src/main/resources/db/migration/V1_142__Add_Spatial_Indices.sql
@@ -5,3 +5,58 @@ CREATE INDEX finnish_regions_gix ON finnish_regions USING GIST (location);
 CREATE INDEX country_gix ON country USING GIST (location);
 CREATE INDEX continent_gix ON continent USING GIST (location);
 CREATE INDEX finnish_postal_codes_gix ON finnish_postal_codes USING GIST (location);
+
+-- DROP views to allow updating depending columns
+DROP VIEW operation_area_geojson RESTRICT;
+DROP VIEW places RESTRICT;
+
+--- Update SRIDs to location columns to make Spatial joins more efficient
+--- Data is already in this projection
+SELECT UpdateGeometrySRID('operation_area','location',4326);
+SELECT UpdateGeometrySRID('finnish_municipalities','location',4326);
+SELECT UpdateGeometrySRID('finnish_postal_codes','location',4326);
+SELECT UpdateGeometrySRID('finnish_regions','location',4326);
+SELECT UpdateGeometrySRID('country','location',4326);
+SELECT UpdateGeometrySRID('continent','location',4326);
+
+-- Rebuild operation_area_geojson
+CREATE VIEW operation_area_geojson AS
+ SELECT oa.*, ST_AsGeoJSON(oa.location) AS "location-geojson"
+   FROM operation_area oa;
+
+--- Recreate view
+CREATE OR REPLACE VIEW places AS
+ SELECT CONCAT('finnish-municipality-', natcode) AS id,
+        'finnish-municipality' as type,
+        namefin,
+        nameswe,
+        ST_FlipCoordinates(location) as location
+   FROM finnish_municipalities
+UNION ALL
+ SELECT CONCAT('finnish-postal-',posti_alue) as id,
+        'finnish-postal' as type,
+        CONCAT(posti_alue,' ',nimi) AS namefin,
+        CONCAT(posti_alue,' ',namn) AS nameswe,
+        location
+   FROM finnish_postal_codes
+UNION ALL
+ SELECT CONCAT('finnish-region-',numero) as id,
+        'finnish-region' AS type,
+        nimi AS namefin,
+        '' AS nameswe,
+        location
+   FROM finnish_regions
+UNION ALL
+ SELECT CONCAT('country-',code) as id,
+        'country' AS type,
+        namefin,
+        nameswe,
+        location
+   FROM country
+UNION ALL
+ SELECT CONCAT('continent-',code) as id,
+        'continent' AS type,
+        namefin,
+        nameswe,
+        location
+   FROM continent;
