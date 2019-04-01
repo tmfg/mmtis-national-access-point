09b7006ba6a03777bc3d0bf14b79a374ced330a3
Joni Korpelin <joni.korpelin@solita.fi>
2019-04-01 09:26:43 +0300

    Added optional options map to link-with-icon to pass id and other options in the future.

--- ote/src/cljs/ote/ui/link_icon.cljs
+++ ote/src/cljs/ote/ui/link_icon.cljs
@@ -4,13 +4,14 @@
 
 
 (defn link-with-icon
-  [icon url link-text on-click]
+  [{:keys [id]} icon url link-text on-click]
   [:div
    [:a (merge {:href url
-               :style {:margin-right "2rem"}
-               :id "edit-transport-operator-btn"}
+               :style {:margin-right "2rem"}}
+              (when id
+                {:id id})
               (when (not-empty on-click)
-                :on-click on-click)
+                {:on-click on-click})
               (stylefy/use-style style-base/blue-link-with-icon))
     icon
     link-text]])
