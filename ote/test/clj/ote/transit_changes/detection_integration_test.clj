93178419c6c78d562e270777f3407a74c85d217c
Erno Kuusela <erno-laptop!erno>
2019-05-07 08:33:25 +0300

    fix day-of-week adjustment in integration test calendar rewriting

--- ote/test/clj/ote/transit_changes/detection_integration_test.clj
+++ ote/test/clj/ote/transit_changes/detection_integration_test.clj
@@ -19,36 +19,27 @@
     (clojure.java.io/copy (clojure.java.io/input-stream x) out)
     (.toByteArray out)))
 
+(defn round-up-to-nearest-multiple-of-7 [n]
+  (if (pos? (rem n 7))
+    (+ n (- 7 (rem n 7)))
+    n))
 
-(defn rewrite-calendar [calendar-data orig-date filter-fn]
-  ;; find out how far past orig-date is in history, and shift all calendar-data
-  ;; dates forward by that amount.
+(defn rewrite-calendar 
+  "Find out how far past orig-date is in history, and shift all calendar-data
+  dates forward by that amount."
+  [calendar-data orig-date filter-fn]
   (let [day-diff (ote.time/day-difference (ote.time/native->date-time orig-date) (clj-time.core/now))
+        day-diff-weekfixed (round-up-to-nearest-multiple-of-7 day-diff)
+        ;; _ (println "rewrite-calendar: day difference (rounded to week):" day-diff-weekfixed)
         
-        _ (println "rewrite-calendar: applying day difference:" (ote.time/native->date-time orig-date) (clj-time.core/now) "->" day-diff "- orig date was" orig-date)
-        
-        date-fwd-fn (fn [java-local-date]                     
-                      (let [joda-orig-datetime (ote.time/java-localdate->joda-date-time
-                                                java-local-date)
-                            orig-dow (clj-time.core/day-of-week joda-orig-datetime)
-                            joda-future-datetime (ote.time/days-from joda-orig-datetime day-diff)
-                            future-dow (clj-time.core/day-of-week joda-future-datetime)
-                            dow-diff (- orig-dow future-dow)
-                            ;; _ (println "dow diff" orig-dow future-dow "->" dow-diff)
-                            dow-corrected (ote.time/days-from joda-future-datetime dow-diff)
-                            ;; _ (println "fd/cfd" joda-future-datetime dow-corrected)
-                            str-future-datetime (ote.time/format-date-iso-8601 dow-corrected)
-                            java-future-datetime (java.time.LocalDate/parse str-future-datetime)]
-                        
-                        ;; (println "weekday adjusted:" (ote.time/day-of-week joda-future-datetime))
-                        java-future-datetime))
         calendar-data (if filter-fn
                         (filterv filter-fn
                                 calendar-data)
                         calendar-data)
-        rewritten-calendar-data (mapv #(update % :gtfs/date date-fwd-fn) calendar-data)]
+        rewritten-calendar-data (mapv (fn [record] (update record :gtfs/date #(.plusDays % day-diff-weekfixed))) calendar-data)
+        ]
        
-    (def *cd rewritten-calendar-data)
+    ;; (def *cd rewritten-calendar-data)
     rewritten-calendar-data))
 
 
