94c51dc9848bfc77407ff3068e687acf88c25f81
jkorpelin <joni.korpelin@solita.fi>
2019-05-16 15:25:46 +0300

    own-service: fixed bug with service errors. Duplicated the service if it had many interfaces.

--- ote/src/clj/ote/services/transport.sql
+++ ote/src/clj/ote/services/transport.sql
@@ -4,15 +4,21 @@ SELECT description FROM "group" WHERE id = :id;
 
 -- name: fetch-transport-services
 SELECT ts.id,
-    ts."transport-operator-id",
-    ts."name",
-    ts."type",
-    ts."sub-type",
-    ts."published",
-    ts."created",
-    ts."modified",
-    ts."transport-type",
-    eid."data-content" as "interface-types"
-FROM "transport-service" ts LEFT OUTER JOIN "external-interface-description" eid on (ts.id = eid."transport-service-id")
-WHERE ts."transport-operator-id" in (:operator-ids)
+       ts."transport-operator-id",
+       ts."name",
+       ts."type",
+       ts."sub-type",
+       ts."published",
+       ts."created",
+       ts."modified",
+       ts."transport-type",
+       (SELECT array_agg(dc)
+        FROM "external-interface-description" eid
+                 JOIN LATERAL unnest(eid."data-content") dc ON TRUE
+        WHERE ts.id = eid."transport-service-id") as "interface-types"
+FROM "transport-service" ts
+         LEFT JOIN "external-interface-description" eid on (ts.id = eid."transport-service-id")
+         LEFT JOIN LATERAL unnest("data-content") types ON true
+WHERE ts."transport-operator-id" in (:operator)
+GROUP BY ts.id
 ORDER BY ts."type" DESC, ts.modified DESC NULLS LAST;
