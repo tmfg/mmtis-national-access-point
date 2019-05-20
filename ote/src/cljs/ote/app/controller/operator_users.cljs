cae3cb1c247790d325fbbce65961ec45a061cbe3
jkorpelin <joni.korpelin@solita.fi>
2019-05-20 14:58:05 +0300

    User access: on-failures added to controller calls

--- ote/src/cljs/ote/app/controller/operator_users.cljs
+++ ote/src/cljs/ote/app/controller/operator_users.cljs
@@ -4,6 +4,7 @@
             [ote.communication :as comm]
             [ote.util.url :as url-util]
             [ote.app.routes :as routes]
+            [ote.app.controller.common :refer [->ServerError]]
             [ote.db.transport-operator :as t-operator]))
 
 (define-event EmailFieldOnChange [input]
@@ -21,16 +22,17 @@
   (assoc-in app [:manage-access :operator-name] (::t-operator/name result)))
 
 (defn GetOperatorUsers [app]
-  (println app)
   (comm/get!
     (str "transport-operator/" (url-util/encode-url-component (get-in app [:params :operator-id])) "/users")
-    {:on-success (tuck/send-async! ->GetUsersSuccess)})
+    {:on-success (tuck/send-async! ->GetUsersSuccess)
+     :on-failure (tuck/send-async! ->ServerError)})
   (assoc-in app [:manage-access :loaded?] false))
 
 (defn GetTransportOperator [app]
   (comm/get!
     (str "t-operator/" (url-util/encode-url-component (get-in app [:params :operator-id])))
-    {:on-success (tuck/send-async! ->GetOperatorSuccess)})
+    {:on-success (tuck/send-async! ->GetOperatorSuccess)
+     :on-failure (tuck/send-async! ->ServerError)})
   app)
 
 (define-event NewUserSuccess [result]
@@ -47,7 +49,8 @@
   (comm/post!
     (str "transport-operator/" (url-util/encode-url-component operator-id) "/users")
     {:email email}
-    {:on-success (tuck/send-async! ->NewUserSuccess)})
+    {:on-success (tuck/send-async! ->NewUserSuccess)
+     :on-failure (tuck/send-async! ->ServerError)})
   (assoc-in app [:manage-access :new-member-loading?] true))
 
 (define-event InitTransportUserView []
