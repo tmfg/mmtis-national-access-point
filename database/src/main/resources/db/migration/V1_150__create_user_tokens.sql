1eb572c3cc43edec6762847e7a81c04f93f36876
jkorpelin <joni.korpelin@solita.fi>
2019-06-11 10:28:22 +0300

    invitation: user-tokens -> user-token && new auditlog

--- database/src/main/resources/db/migration/V1_150__create_user_tokens.sql
+++ database/src/main/resources/db/migration/V1_150__create_user_tokens.sql
@@ -1,7 +1,8 @@
-CREATE TABLE "user-tokens" (
+CREATE TABLE "user-token" (
     "user-email" text NOT NULL UNIQUE,
     token text NOT NULL PRIMARY KEY,
     "operator-id" INTEGER NOT NULL,
     expiration date NOT NULL,
     "requester-id" text NOT NULL
-)
+);
+
