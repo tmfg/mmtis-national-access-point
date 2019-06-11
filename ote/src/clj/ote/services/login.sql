44e3702e8726e33c6544945ebb1bde655d250edc
jkorpelin <joni.korpelin@solita.fi>
2019-06-11 08:44:07 +0300

    invitation: Login sql also needs username/email exists checks.

--- ote/src/clj/ote/services/login.sql
+++ ote/src/clj/ote/services/login.sql
@@ -3,3 +3,10 @@ SELECT name, email, password, fullname, id
   FROM "user"
  WHERE (lower(name) = lower(:email) OR lower(email) = lower(:email)) and state = 'active';
 
+-- name: username-exists?
+-- single?: true
+SELECT EXISTS(SELECT id FROM "user" WHERE LOWER(name) = LOWER(:username));
+
+-- name: email-exists?
+-- single?: true
+SELECT EXISTS(SELECT id FROM "user" WHERE LOWER(email) = LOWER(:email));
