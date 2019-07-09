97669b82e8237c17b26f09547ebfb1fb7fdfb01d
jkorpelin <joni.korpelin@solita.fi>
2019-07-09 11:43:27 +0300

    email confirmation: add email confirmed to testdata users

--- database/testdata-ckan.sql
+++ database/testdata-ckan.sql
@@ -1,11 +1,10 @@
 -- ### Add admin user ###
-INSERT INTO "user" (id, name, openid, password, fullname, email, apikey, created, reset_key, about, activity_streams_email_notifications, sysadmin, state)
+INSERT INTO "user" (id, name, openid, password, fullname, email, apikey, created, reset_key, about, activity_streams_email_notifications, sysadmin, state, "email-confirmed?")
 VALUES ('401139db-8f3e-4371-8233-5d51d4c4c8b6', 'admin', NULL,
                                                 '$pbkdf2-sha512$25000$dq51TgmB0JpzDiGEkDKGUA$YVW2kq0qZrp7ZMge7P33JCTTaUaz9eH0eKXDNENRpOJ/ndCrpBGR94Qm3XakaGBLCA54qi1Q6wxnDgcfvgOxbA',
                                                 'Admin Adminson', 'admin@napoteadmin123.com',
                                                 'd7c6dccf-6541-4443-a9b4-7ab7c36735bc',
-                                                '2017-10-04T09:45:10.895301' :: TIMESTAMP, NULL, NULL, FALSE, TRUE,
-        'active');
+                                                '2017-10-04T09:45:10.895301' :: TIMESTAMP, NULL, NULL, FALSE, TRUE, 'active', TRUE);
 INSERT INTO "activity" (id, timestamp, user_id, object_id, revision_id, activity_type, data)
 VALUES ('2fc23860-6275-4712-a1a8-37bab21cc2b6', '2017-10-04T09:45:10.899229' :: TIMESTAMP,
         '401139db-8f3e-4371-8233-5d51d4c4c8b6', '401139db-8f3e-4371-8233-5d51d4c4c8b6', NULL, 'new user', NULL);
@@ -77,9 +76,9 @@ WHERE revision.id = '7b7f56df-1c88-48b7-b807-6bcfaedb274a';
 -- ### ... ###
 
 -- Add regular user (normaluser/password)
-INSERT INTO "public"."user"("id","name","apikey","created","about","openid","password","fullname","email","reset_key","sysadmin","activity_streams_email_notifications","state")
+INSERT INTO "public"."user"("id","name","apikey","created","about","openid","password","fullname","email","reset_key","sysadmin","activity_streams_email_notifications","state", "email-confirmed?")
 VALUES
-(E'676a2532-b106-4329-b95e-e30c8c8265d5',E'normaluser',E'8eb7bf65-2a7b-45dd-a576-be10a02c3801',E'2018-01-17 11:46:11.658956',NULL,NULL,E'$pbkdf2-sha512$25000$UApBKOXcW6uVci4FQKiVcg$B4j1WY60oVyXvJHA9YRXIN8wKl8lD.Gzn802IwOLTuCgcIqbaaEQIJcqaIWr2ROf4XBtKdCTSCyroLhIMpFC9g',E'User Userson',E'user.userson@example.com',NULL,FALSE,FALSE,E'active');
+(E'676a2532-b106-4329-b95e-e30c8c8265d5',E'normaluser',E'8eb7bf65-2a7b-45dd-a576-be10a02c3801',E'2018-01-17 11:46:11.658956',NULL,NULL,E'$pbkdf2-sha512$25000$UApBKOXcW6uVci4FQKiVcg$B4j1WY60oVyXvJHA9YRXIN8wKl8lD.Gzn802IwOLTuCgcIqbaaEQIJcqaIWr2ROf4XBtKdCTSCyroLhIMpFC9g',E'User Userson',E'user.userson@example.com',NULL,FALSE,FALSE,E'active',TRUE);
 
 INSERT INTO "public"."revision"("id","timestamp","author","message","state","approved_timestamp")
 VALUES
