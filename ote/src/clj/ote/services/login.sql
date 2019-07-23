983d0f822cd9fa0d3196ef3f7aecf7d2f51b5bf3
jkorpelin <joni.korpelin@solita.fi>
2019-07-23 13:45:23 +0300

    refactoring sql files: moved fns to more matching files

--- ote/src/clj/ote/services/login.sql
+++ ote/src/clj/ote/services/login.sql
@@ -2,11 +2,3 @@
 SELECT name, email, password, fullname, id, "email-confirmed?"
   FROM "user"
  WHERE (lower(name) = lower(:email) OR lower(email) = lower(:email)) and state = 'active';
-
--- name: username-exists?
--- single?: true
-SELECT EXISTS(SELECT id FROM "user" WHERE LOWER(name) = LOWER(:username));
-
--- name: email-exists?
--- single?: true
-SELECT EXISTS(SELECT id FROM "user" WHERE LOWER(email) = LOWER(:email));
