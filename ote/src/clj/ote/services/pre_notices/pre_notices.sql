652882739b095b2aba9f007662660551a6223acd
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-07-31 15:18:21 +0300

    Capitalize pre-notice.sql

--- ote/src/clj/ote/services/pre_notices/pre_notices.sql
+++ ote/src/clj/ote/services/pre_notices/pre_notices.sql
@@ -1,7 +1,7 @@
 -- name: fetch-authority-pre-notices
-SELECT p.id, p.sent, p.created, p.regions, p."route-description", p."pre-notice-type" as "pre-notice-type",
-       (select array_agg(ed."effective-date") from unnest(p."effective-dates") ed) as "change-dates",
-       top.name as "transport-operator"
+SELECT p.id, p.sent, p.created, p.regions, p."route-description", p."pre-notice-type" AS "pre-notice-type",
+       (SELECT array_agg(ed."effective-date") FROM unnest(p."effective-dates") ed) AS "change-dates",
+       top.name AS "transport-operator"
   FROM "pre_notice" p, "transport-operator" top
  WHERE top.id = p."transport-operator-id"
  ORDER BY p.sent DESC;
