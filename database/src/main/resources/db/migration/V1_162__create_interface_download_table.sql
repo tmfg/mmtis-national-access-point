750dde7a7a2701464024547fff7b170e25a5e954
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-08-14 08:49:34 +0300

    Drop import-error and db-error column from external-interface-description table
    Drop views that uses them and create views again with external-interface-download-status table columns

--- database/src/main/resources/db/migration/V1_162__create_interface_download_table.sql
+++ database/src/main/resources/db/migration/V1_162__create_interface_download_table.sql
@@ -5,7 +5,7 @@
 CREATE TYPE interface_download_status AS ENUM ('success','failure');
 
 -- And create the table
-CREATE TABLE "interface-download" (
+CREATE TABLE "external-interface-download-status" (
   id SERIAL PRIMARY KEY,
   "external-interface-description-id" INTEGER REFERENCES "external-interface-description" (id) NOT NULL,
   "download-error" text,
@@ -15,8 +15,8 @@ CREATE TABLE "interface-download" (
   created TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
--- Move all existing download information (errors) to new table
-INSERT INTO "interface-download" ("external-interface-description-id", "download-error", "db-error",
+-- Copy all existing download information (errors) to new table
+INSERT INTO "external-interface-download-status" ("external-interface-description-id", "download-error", "db-error",
             "download-status", "package-id", created)
 SELECT eid.id, eid."gtfs-import-error", eid."gtfs-db-error",
   CASE
@@ -27,3 +27,39 @@ SELECT eid.id, eid."gtfs-import-error", eid."gtfs-db-error",
        p.id as "package-id", p."created"
   FROM "external-interface-description" eid
        JOIN "gtfs_package" p ON p."external-interface-description-id" = eid.id;
+
+-- Need to drop dependant views before altering
+DROP VIEW transport_service_search_result;
+
+-- Create dropped views again
+CREATE VIEW transport_service_search_result AS
+SELECT t.*, op.name as "operator-name", op."business-id" as "business-id",
+       -- Changed Start
+       array_cat(
+               array_cat(
+                       (SELECT sc."companies"
+                        FROM "service_company" sc
+                        WHERE sc."transport-service-id" = t.id),
+                       t."companies"),
+               array_agg((aso.name, aso."business-id")::company)) AS "service-companies",
+       -- Changed End
+       (SELECT array_agg(oaf."operation-area")
+        FROM "operation-area-facet" oaf
+        WHERE oaf."transport-service-id" = t.id) AS "operation-area-description",
+       (SELECT array_agg(ROW(ei."external-interface", ei.format,
+           ei."ckan-resource-id",
+           ei.license, ei."data-content",
+           eids."download-error",
+           eids."db-error")::external_interface_search_result)
+        FROM "external-interface-description" ei, "external-interface-download-status" eids
+        WHERE ei."transport-service-id" = t.id)::external_interface_search_result[] AS "external-interface-links"
+FROM "transport-service" t
+         LEFT JOIN (SELECT top.name as name, top."business-id" as "business-id", a."service-id"
+                    FROM "transport-operator" top, "associated-service-operators" a WHERE a."operator-id" = top.id) as aso ON aso."service-id" = t.id
+         JOIN  "transport-operator" op ON op.id = t."transport-operator-id"
+GROUP BY t.id, op.name, op."business-id";
+
+-- Drop unused columns
+ALTER TABLE "external-interface-description"
+  DROP COLUMN "gtfs-import-error",
+  DROP COLUMN "gtfs-db-error";
