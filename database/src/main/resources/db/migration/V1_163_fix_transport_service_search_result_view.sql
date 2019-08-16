7810d8381410ee1a6aad8e35ac4b641f6b8d5927
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-08-16 15:28:15 +0300

    Fix service search view

---
+++ database/src/main/resources/db/migration/V1_163_fix_transport_service_search_result_view.sql
@@ -0,0 +1,36 @@
+-- Fix "external-interface-download-status" join to "external-interface-description"
+-- Drop view first
+DROP VIEW transport_service_search_result;
+
+-- Create view again
+CREATE VIEW transport_service_search_result AS
+SELECT t.*, op.name as "operator-name", op."business-id" as "business-id",
+       -- Changed Start
+       array_cat(
+               array_cat(
+                       (SELECT sc."companies"
+                        FROM "service_company" sc
+                        WHERE sc."transport-service-id" = t.id),
+                       t."companies"),
+               array_agg((aso.name, aso."business-id")::company)) AS "service-companies",
+       -- Changed End
+       (SELECT array_agg(oaf."operation-area")
+        FROM "operation-area-facet" oaf
+        WHERE oaf."transport-service-id" = t.id) AS "operation-area-description",
+       (SELECT array_agg(ROW(ei."external-interface", ei.format,
+           ei."ckan-resource-id",
+           ei.license, ei."data-content",
+           eids."download-error",
+           eids."db-error")::external_interface_search_result)
+
+       -- Changed section
+       FROM "external-interface-description" ei
+             JOIN "external-interface-download-status" eids ON eids."external-interface-description-id" = ei.id
+       -- Changed section ends
+
+        WHERE ei."transport-service-id" = t.id)::external_interface_search_result[] AS "external-interface-links"
+FROM "transport-service" t
+         LEFT JOIN (SELECT top.name as name, top."business-id" as "business-id", a."service-id"
+                    FROM "transport-operator" top, "associated-service-operators" a WHERE a."operator-id" = top.id) as aso ON aso."service-id" = t.id
+         JOIN  "transport-operator" op ON op.id = t."transport-operator-id"
+GROUP BY t.id, op.name, op."business-id";
