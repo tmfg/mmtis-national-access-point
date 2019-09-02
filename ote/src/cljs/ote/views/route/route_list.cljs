3e7dd6d6ea4a8c10d6dc1ada64368c76f3cf93fc
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-09-02 14:28:47 +0300

    Show help texts when user tries to add sea route without operator and service

--- ote/src/cljs/ote/views/route/route_list.cljs
+++ ote/src/cljs/ote/views/route/route_list.cljs
@@ -160,43 +160,59 @@
         schedule-services (filter
                             (fn [{::t-service/keys [sub-type] :as s}]
                               (or (= :schedule sub-type) false))
-                            operator-services)]
+                            operator-services)
+        fake-operator {::t-operator/id 987654321
+                       ::t-operator/name (tr [:common-texts :no-operators])}]
     [:div
      [page/page-controls "" (tr [:route-list-page :header-route-list])
       [:div
        [:h4 {:style {:margin "0"}}
         (tr [:field-labels :select-transport-operator])]
-       [form-fields/field
-        {:element-id "select-operator-at-searoute-frontpage"
-         :name :select-transport-operator
-         :type :selection
-         :show-option #(::t-operator/name %)
-         :update! #(e! (to/->SelectOperator %))
-         :options (mapv to/take-operator-api-keys (mapv :transport-operator operators))
-         :auto-width? true
-         :class-name "mui-select-button"}
-        (to/take-operator-api-keys operator)]]]
+       (if operator
+         [form-fields/field
+          {:element-id "select-operator-at-searoute-frontpage"
+           :name :select-transport-operator
+           :type :selection
+           :show-option #(::t-operator/name %)
+           :update! #(e! (to/->SelectOperator %))
+           :options (mapv to/take-operator-api-keys (mapv :transport-operator operators))
+           :auto-width? true
+           :class-name "mui-select-button"}
+          (to/take-operator-api-keys operator)]
+         [form-fields/field
+          {:element-id "empty-operator-select-searoute-frontpage"
+           :name :select-transport-operator
+           :type :selection
+           :show-option #(::t-operator/name %)
+           :options [fake-operator]
+           :auto-width? true
+           :class-name "mui-select-button"}
+          fake-operator])]]
 
-     [:div.container
-      [:h2 (get-in app [:transport-operator :ote.db.transport-operator/name])]
-      [:a (merge {:href (str "#/new-route/")
-                  :id "new-route-button"
-                  :on-click #(do
-                               (.preventDefault %)
-                               (e! (route-list/->CreateNewRoute)))}
-                 (stylefy/use-style style-buttons/primary-button))
-       (tr [:buttons :add-new-route])]
-      [list-routes e! public-routes draft-routes]
-      (when schedule-services
-        [link-services-to-routes e! schedule-services services-with-route (not (empty? public-routes))])
-      (if (not (empty? public-routes))
-        (let [loc (.-location js/document)
-              url (str (.-protocol loc) "//" (.-host loc) (.-pathname loc)
-                       "export/gtfs/" (::t-operator/id operator))]
+     (if operator
+       [:div.container
+        [:h2 (get-in app [:transport-operator :ote.db.transport-operator/name])]
+        [:a (merge {:href (str "#/new-route/")
+                    :id "new-route-button"
+                    :on-click #(do
+                                 (.preventDefault %)
+                                 (e! (route-list/->CreateNewRoute)))}
+                   (stylefy/use-style style-buttons/primary-button))
+         (tr [:buttons :add-new-route])]
+        [list-routes e! public-routes draft-routes]
+        (when schedule-services
+          [link-services-to-routes e! schedule-services services-with-route (not (empty? public-routes))])
+        (if (not (empty? public-routes))
+          (let [loc (.-location js/document)
+                url (str (.-protocol loc) "//" (.-host loc) (.-pathname loc)
+                         "export/gtfs/" (::t-operator/id operator))]
+            [:div
+             [:h4 (tr [:route-list-page :header-sea-route-interface])]
+             [:p (tr [:route-list-page :desc-sea-route-interface])]
+             [common/linkify url (op-util/gtfs-file-name operator)]])
           [:div
-           [:h4 (tr [:route-list-page :header-sea-route-interface])]
-           [:p (tr [:route-list-page :desc-sea-route-interface])]
-           [common/linkify url (op-util/gtfs-file-name operator)]])
-        [:div
-         [:h4 (tr [:route-list-page :header-download-gtfs])]
-         [:p (tr [:route-list-page :desc-download-gtfs])]])]]))
+           [:h4 (tr [:route-list-page :header-download-gtfs])]
+           [:p (tr [:route-list-page :desc-download-gtfs])]])]
+       [:div.container
+        [:p (tr [:route-list-page :add-operator-and-service])
+        [common/back-link-with-event :own-services (tr [:front-page :move-to-services-page])]]])]))
