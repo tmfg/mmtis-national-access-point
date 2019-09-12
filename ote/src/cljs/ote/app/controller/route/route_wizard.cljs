c135738c65cf096c0a10259b59e172f270768d7d
Tommi Kenakkala <tommi.kenakkala@solita.fi>
2019-09-12 11:33:34 +0300

    views: route-wizard: load route and stops failure message customized

--- ote/src/cljs/ote/app/controller/route/route_wizard.cljs
+++ ote/src/cljs/ote/app/controller/route/route_wizard.cljs
@@ -20,7 +20,7 @@
             [ote.localization :refer [selected-language]]
             [ote.ui.validation :as validation]
             [tuck.core :refer [define-event send-async! Event]]
-            [ote.app.controller.common :refer [->ServerError]]
+            [ote.app.controller.common :refer [->ServerErrorDetails]]
             [ote.localization :refer [tr] :as localization]))
 
 (declare ->LoadRoute)
@@ -277,7 +277,8 @@
   (process-event [_ app]
     (comm/get! "transit/stops.json"
                {:on-success (tuck/send-async! ->LoadStopsResponse)
-                :on-failure (tuck/send-async! ->ServerError)
+                :on-failure (tuck/send-async! ->ServerErrorDetails
+                                              {:desc (str "Merenkulun reitti- ja aikataulueditori tilapäisesti poissa käytöstä huoltotoimenpiteiden vuoksi")})
                 :response-format :json})
     app)
 
@@ -289,7 +290,8 @@
   (process-event [{id :id} app]
     (comm/get! (str "routes/" id)
                {:on-success (tuck/send-async! ->LoadRouteResponse)
-                :on-failure (send-async! ->ServerError)})
+                :on-failure (send-async! ->ServerErrorDetails
+                                         {:desc (str "Merenkulun reitti- ja aikataulueditori tilapäisesti poissa käytöstä huoltotoimenpiteiden vuoksi")})})
     (-> app
         (assoc-in [:route :loading?] true)))
 
