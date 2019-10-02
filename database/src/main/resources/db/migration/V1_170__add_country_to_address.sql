5ddb8a76986ffa7081ed3e71e0cc7a5de72895b6
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-10-02 11:11:46 +0300

    Update database to take countries in

--- database/src/main/resources/db/migration/V1_170__add_country_to_address.sql
+++ database/src/main/resources/db/migration/V1_170__add_country_to_address.sql
@@ -1,5 +1,5 @@
 ALTER TYPE address
-    ADD ATTRIBUTE "country-code" CHAR(2);
+    ADD ATTRIBUTE "country_code" CHAR(2);
 
 
 CREATE OR REPLACE FUNCTION convert_transport_service_address_countrycodes()
@@ -7,26 +7,28 @@ CREATE OR REPLACE FUNCTION convert_transport_service_address_countrycodes()
 BEGIN
     -- Update service address
     UPDATE "transport-service" as ts
-    SET ("contact-address"."country-code") = 'FI';
+    SET ("contact-address"."country_code") =
+            (SELECT 'FI'
+             FROM "transport-service" service
+             WHERE service.id = ts.id);
 
     -- update rental pick up addresses
     UPDATE "transport-service" as ts
     SET (rentals."pick-up-locations") =
             (SELECT array_agg(ROW(pa."pick-up-name",
-                pa."pick-up-type",
-                pa."service-hours",
-                pa."service-exceptions",
-                ((pa."pick-up-address").street,
-                 (pa."pick-up-address").postal_code,
-                 (pa."pick-up-address").post_office,
-                 (pa."pick-up-address").foreign_postal_code,
-                 'FI'
-                 )::address,
-                pa."service-hours-info")::pick_up_location)
-             FROM "transport-service" service
-                      join lateral unnest((service.rentals)."pick-up-locations") pa on true
-    WHERE service.type = 'rentals' AND ts.id = service.id
-                         GROUP BY service.id);
+                    pa."pick-up-type",
+                    pa."service-hours",
+                    pa."service-exceptions",
+                    ((pa."pick-up-address").street,
+                     (pa."pick-up-address").post_office,
+                     (pa."pick-up-address").postal_code,
+                     'FI'
+                    )::address,
+                    pa."service-hours-info")::pick_up_location)
+               FROM "transport-service" service,
+                    lateral unnest((service.rentals)."pick-up-locations") pa
+              WHERE service.type = 'rentals' AND ts.id = service.id
+              GROUP BY service.id);
 END;
 $$ LANGUAGE plpgsql;
 
@@ -37,10 +39,16 @@ CREATE OR REPLACE FUNCTION convert_transport_operator_address_countrycodes()
 AS $$
 BEGIN
     UPDATE "transport-operator" as "t-operator"
-    SET ("visiting-address"."country-code") = 'FI';
+    SET ("visiting-address"."country_code") =
+            (SELECT 'FI'
+             FROM "transport-operator" top
+             WHERE top.id = "t-operator".id);
 
     UPDATE "transport-operator" as "t-operator"
-    SET ("billing-address"."country-code") = 'FI';
+    SET ("billing-address"."country_code") =
+            (SELECT 'FI'
+             FROM "transport-operator" top
+             WHERE top.id = "t-operator".id);
 
 END;
 $$ LANGUAGE plpgsql;
