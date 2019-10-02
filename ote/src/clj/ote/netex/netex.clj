f526a8a034c94d26b8b2580ec82a81f74349c278
Tommi Kenakkala <tommi.kenakkala@solita.fi>
2019-10-02 13:16:58 +0300

    netex: gtfs: s3 uploading and operator name to netex output metadata

--- ote/src/clj/ote/netex/netex.clj
+++ ote/src/clj/ote/netex/netex.clj
@@ -6,26 +6,16 @@
     [clojure.test :refer [is]]
     [cheshire.core :as cheshire]
     [ote.db.netex :as netex]
-    [specql.core :as specql]))
+    [specql.core :as specql]
+    [clojure.java.shell :refer [sh with-sh-dir]]
+    [clojure.string :as str]
+    [amazonica.aws.s3 :as s3]))
 
-(defn set-conversion-status
-  "Resolves operation result based on input args and updates status to db.
-  Return: True on success, false on failure"
-  [filepath db {:keys [service-id external-interface-description-id]}]
-  (log/debug "set-netex-conversion-status: service-id = " service-id ", path = " filepath
-             ", external-interface-description-id = " external-interface-description-id)
-  (let [result (if (clojure.string/blank? filepath)
-                 :error
-                 :ok)]
-    (specql/upsert! db ::netex/netex-conversion
-                    #{::netex/transport-service-id ::netex/external-interface-description-id}
-                    {
-                     ::netex/transport-service-id service-id
-                     ::netex/external-interface-description-id external-interface-description-id
-                     ::netex/url (or filepath "")
-                     ::netex/modified (ote.time/sql-date (java.time.LocalDate/now)) ; TODO: db created and modfied use different timezone like this
-                     ::netex/status result})
-    (= :ok result)))
+(defn- path-allowed? [^String path]
+  ;; Sanity check to avoid operating in any system or app directory
+  (str/starts-with?
+    (.getAbsolutePath (clojure.java.io/file path))          ; TODO: should use .getCanonicalPath but osx
+    "/tmp/"))
 
 (defn delete-files-recursively! [f1]
   (when (.isDirectory (io/file f1))
@@ -34,12 +24,12 @@
   (io/delete-file f1 true))
 
 (defn cleanup-dir-recursive!
-  ;; TODO: replace using fileutils interop etc
-  ;; TODO; first create an in-mem file object and verity it's path instead of the path string arg
+  ;; TODO: replace using fileutils interop etc?
   "Takes an path argument and does a simple sanity check before removing it and child directories recursively"
   [path]
-  (when (< 1 (count path))
-    (delete-files-recursively! path)))
+  (if (path-allowed? path)
+    (delete-files-recursively! path)
+    (log/warn "Directory cleanup skipped, bad path = " path)))
 
 (defn- compose-chouette-import-gtfs-json [operator-name]
   (cheshire/generate-string
@@ -67,54 +57,112 @@
       :add_extension true}}
     {:pretty true}))
 
-(defn- upload-s3 [filepath]
-  (log/debug "upload-s3: path = " filepath)
-  (when-let [s3-path nil]))
-
-(defn- convert! [chouette-import-config-filename chouette-export-config-filename gtfs-filepath]
-  (let [netex-filepath nil]
-    netex-filepath))
+(defn- validate-chouette-output
+  "Takes choutette process exit info and output path and evaluates if conversion was a success or failure.
+  Return: On success string defining filesystem path to output file, on failure nil"
+  [{:keys [exit err] :as ex-info} output-filepath chouette-cmd]
+  (if (and (= 0 exit)
+           (str/blank? err)
+           (.exists (io/file output-filepath)))
+    output-filepath
+    (do (log/warn "chouette error = " ex-info ", tried = " chouette-cmd)
+        nil)))
 
 (defn- gtfs->netex!
-  "Return: String defining filesystem path to output file, or nil on failure"
-  [{:keys [conversion-work-path]} {:keys [gtfs-file gtfs-filename]}]
+  "Return: On success string defining filesystem path to output file, on failure nil"
+  [{:keys [gtfs-file gtfs-filename gtfs-basename operator-name]} {:keys [chouette-path conversion-work-path] :as config}]
   {:pre [(is (and (< 1 (count conversion-work-path))
                   (not (clojure.string/blank? conversion-work-path))))
          (is (not (clojure.string/blank? gtfs-filename)))
          (is (seq gtfs-file))]}                             ;`is` used to print the value of a failed precondition
-  (log/debug "gtfs->netex!: path = " conversion-work-path)
-  (let [chouette-import-config-filename (str conversion-work-path "importGtfs.json")
-        chouette-export-config-filename (str conversion-work-path "exportNetexjson")
-        gtfs-filepath (str conversion-work-path gtfs-filename)]
+  (let [import-config-filepath (str conversion-work-path "importGtfs.json")
+        export-config-filepath (str conversion-work-path "exportNetexjson")
+        gtfs-filepath (str conversion-work-path gtfs-filename)
+        gtfs-name-suffix "_gtfs"
+        netex-filepath (str conversion-work-path
+                            (if (str/ends-with? gtfs-basename gtfs-name-suffix)
+                              (subs gtfs-basename 0 (- (count gtfs-basename) (count gtfs-name-suffix)))
+                              gtfs-basename)
+                            "_netex.zip")
+        chouette-cmd ["./chouette.sh"                       ; Vector used to allow logging shell invocation on error
+                      (str "-i " import-config-filepath)
+                      (str "-o " export-config-filepath)
+                      (str "-f " netex-filepath)
+                      gtfs-filepath]]
+
+    (if (and (path-allowed? gtfs-filepath)
+             (path-allowed? conversion-work-path))
+      (do
+        ;; Setup input files for choutette command line tool conversion call.
+        ;; No return condition checks because calls throw and exception on failure
+        ;(cleanup-dir-recursive! conversion-work-path)
+        (.mkdirs (clojure.java.io/file conversion-work-path)) ; Returns true when created, false otherwise
+        (spit import-config-filepath (compose-chouette-import-gtfs-json operator-name)) ; Returns nil on success or throws an exception
+        (spit export-config-filepath (compose-chouette-export-netex-json operator-name))
+        (io/copy                                            ; Returns nil on success or throws an exception
+          (java.io.ByteArrayInputStream. gtfs-file)
+          (io/file gtfs-filepath))
+        ;; Do GTFS to NeTEx conversion, previous lines
+        (log/info (str "GTFS->NeTEx invokes: " chouette-cmd))
+        (->
+          (with-sh-dir chouette-path
+                       (apply sh chouette-cmd))             ; TODO: use a dedicated user with limited access rights
+          (validate-chouette-output netex-filepath chouette-cmd)))
+      (do
+        (log/error (str "Bad path argument(s) " config))
+        nil))))
+
+(defn- upload-s3
+  "Takes path to file and map with bucket name and puts file into bucket.
+  Returns: On success true,
+  on failure amazonica throws an exception, on other failures like missing argument this returns false"
+  [filepath {:keys [bucket]}]
+  (when (and bucket filepath)
+    (let [filename (.getName (io/file filepath))]
+      (log/debug "Putting to S3 bucket = " bucket ", file = " filepath ", filename = " filename)
+      ;; No need to set content-length?
+      ;; Access denied or nonexistent bucket will throw an exception
+      (s3/put-object bucket filename (io/file filepath))
+      filename)))
+
+(defn set-conversion-status!
+  "Resolves operation result based on input args and updates status to db.
+  Return: On successful conversion true, on failure false"
+  [{:keys [netex-filepath s3-filename]} db {:keys [service-id external-interface-description-id]}]
+  (let [result (if (clojure.string/blank? netex-filepath)
+                 :error
+                 :ok)]
+    (log/info (str "GTFS->NeTEx result to db: service-id = " service-id
+                   " result = " result
+                   ", external-interface-description-id = " external-interface-description-id
+                   ", s3-filename = " s3-filename))
+    (specql/upsert! db ::netex/netex-conversion
+                    #{::netex/transport-service-id ::netex/external-interface-description-id}
+                    {::netex/transport-service-id service-id
+                     ::netex/external-interface-description-id external-interface-description-id
+                     ::netex/filename (or s3-filename "")
+                     ::netex/modified (ote.time/sql-date (java.time.LocalDate/now)) ; TODO: db created and modified use different timezone like this
+                     ::netex/status result})
+    (= :ok result)))
+
+(defn gtfs->netex-and-set-status!
+  "Deletes `conversion-work-path` and runs gtfs->netex conversion to gtfs file in `conversion-meta`
+  Return: True on success, false on failure"
+  [db {:keys [conversion-work-path] :as config-netex} conversion-meta]
+  (cleanup-dir-recursive! conversion-work-path)
+  ;; If s3 bucket is not defined conversion result is set to db as success in order to reflect conversion tool
+  ;; invocation result. Even though created netex file is lost if s3 upload is skipped.
+  (let [meta (try
+               (as-> (assoc conversion-meta
+                       :netex-filepath (gtfs->netex! conversion-meta config-netex)) meta
+                     (assoc meta
+                       :s3-filename (upload-s3 (:netex-filepath meta) config-netex)))
+               (catch Exception e
+                 (log/warn (str "GTFS->NeTEx Conversion failed, service-id = " (:service-id conversion-meta)
+                                ", external-interface-description-id = " (:external-interface-description-id conversion-meta
+                                                                           )
+                                ", filename = " (:filename conversion-meta)
+                                ", Exception = \n" (pr-str e)))
+                 conversion-meta))]
     (cleanup-dir-recursive! conversion-work-path)
-    (.mkdirs (java.io.File. conversion-work-path))
-    (spit chouette-import-config-filename (compose-chouette-import-gtfs-json nil))
-    (spit chouette-export-config-filename (compose-chouette-export-netex-json nil))
-    (spit gtfs-filepath gtfs-file)
-    gtfs-filepath
-
-    (convert! chouette-import-config-filename chouette-export-config-filename gtfs-filepath)))
-
-;(defn convert-to-netex-and-store
-;  "Return: String defining s3 path to netex file, or nil on failure"
-;  [config data]
-;  (->
-;    (gtfs->netex! config data)
-;    (upload-s3)))
-
-(defn gtfs->netex-and-set-status
-  "Return: True on success, false on failure"
-  [db config-netex gtfs-and-meta]
-  (->
-    (try
-      (-> (gtfs->netex! config-netex gtfs-and-meta)
-          (upload-s3))
-      (catch Exception e
-        (log/warn (str "GTFS: Conversion failed, service-id = " (:service-id gtfs-and-meta)
-                       ", external-interface-description-id = " (:external-interface-description-id gtfs-and-meta
-                                                                  )
-                       ", file url = " (:filename gtfs-and-meta)
-                       ", Exception = \n" (pr-str e)))
-        (cleanup-dir-recursive! (:conversion-work-path config-netex))
-        false))
-    (set-conversion-status db gtfs-and-meta)))
+    (set-conversion-status! meta db conversion-meta)))
