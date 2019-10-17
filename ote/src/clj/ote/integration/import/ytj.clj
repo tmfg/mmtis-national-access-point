56664262d288616c03f8b51bceab1250c9484328
Tommi Kenakkala <tommi.kenakkala@solita.fi>
2019-10-17 20:49:15 +0300

    integration: ytj: code cleanup

--- ote/src/clj/ote/integration/import/ytj.clj
+++ ote/src/clj/ote/integration/import/ytj.clj
@@ -1,32 +1,23 @@
 (ns ote.integration.import.ytj
   "YTJ (Finnish company register) API client."
-  (:require [clj-http.client :as http-client]            
+  (:require [clj-http.client :as http-client]
             [taoensso.timbre :as log]
             [clojure.walk :as walk]
             [clj-time.format :as format]
             [clj-time.core :as t]
-            [clojure.string :as string]
             [ote.components.http :as http]
             [com.stuartsierra.component :as component]
             [compojure.core :refer [routes GET]]
-            [clojure.spec.alpha :as s]
             [ote.util.feature :as feature]))
-
-
-;; ui needs:
-;; - businessId
-;; - name
-;; - auxiliaryNames
-;; - addresses
 
 (defn parse-iso8601-date [s]
   (when (string? s)
     (format/parse (:date format/formatters) s)))
 
-(def http-get http-client/get) ;; redef'd to mock in test
+(def http-get http-client/get)                              ;; redef'd to mock in test
 
 (defn without-expired-items [ytj-map]
-  (let [now (t/now)        
+  (let [now (t/now)
         date-in-past? (fn [d]
                         (if (nil? d)
                           false
@@ -38,8 +29,8 @@
                               (date-in-past? d)))
         walk-fn (fn [x]
                   (if (and (vector? x) (map? (first x)))
-                     (filterv (complement end-date-expired?) x)
-                     x))]
+                    (filterv (complement end-date-expired?) x)
+                    x))]
     (walk/postwalk walk-fn ytj-map)))
 
 (defn- compose-result [response]
@@ -64,22 +55,21 @@
       ;(log/debug "ytj fetch: got response" (pr-str response))
       (if (or (-> response :status (not= 200))
               (-> response :body :totalResults str (not= "1")))
-        (log/debug "YTJ: all is not good, status=" (-> response :status (= 200)) " body=" (-> response :body pr-str))
-        )
-       (compose-result response))))
+        (log/debug "YTJ: all is not good, status=" (-> response :status (= 200)) " body=" (-> response :body pr-str)))
+      (compose-result response))))
 
 (defrecord YTJFetch [config]
   component/Lifecycle
-  (start [{db :db http :http :as this}]
+  (start [{http :http :as this}]
     (assoc this ::stop
-           (when (feature/feature-enabled? config :open-ytj-integration)
-             ;; require authentication because we don't want to be an open proxy for PRH API (and also there may be rate limits)
-             (http/publish! http {:authenticated? true}
-                            (routes                           
-                             (GET "/fetch/ytj" [company-id]
-                                  (let [r (fetch-by-company-id company-id)]
-                                    ;(println "YTJ: sending as transit:" (pr-str r))
-                                    (http/transit-response r))))))))
+                (when (feature/feature-enabled? config :open-ytj-integration)
+                  ;; require authentication because we don't want to be an open proxy for PRH API (and also there may be rate limits)
+                  (http/publish! http {:authenticated? true}
+                                 (routes
+                                   (GET "/fetch/ytj" [company-id]
+                                     (let [r (fetch-by-company-id company-id)]
+                                       ;(println "YTJ: sending as transit:" (pr-str r))
+                                       (http/transit-response r))))))))
   (stop [{stop ::stop :as this}]
     (when stop
       (stop))
