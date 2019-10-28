df53f6c34f02d96b1a0787e3f0ed657ed54ffe96
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-10-28 10:47:38 +0200

    Store netex download status to stats-service table

--- ote/src/clj/ote/integration/export/netex.clj
+++ ote/src/clj/ote/integration/export/netex.clj
@@ -7,24 +7,39 @@
             [com.stuartsierra.component :as component]
             [ote.components.http :as http]
             [compojure.core :refer [GET]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [specql.core :as specql]
+            [ote.db.common :as common]))
 
 (defn file-download-url [{{base-url :base-url} :environment} transport-service-id file-id]
   (format "%sexport/netex/%d/%d" base-url transport-service-id file-id))
 
-(defn fetch-netex-response [db {:keys [bucket]} file-id]
+(defn fetch-netex-response
+  "Return netex file as reponse.
+  `origin` parameter is not mandatody. If it is missing, it is handled as api call."
+  [db {:keys [bucket]} file-id service-id origin]
   (let [filename (:ote.db.netex/filename (netex/fetch-conversion db file-id))
         file (when (and bucket
                         (not (clojure.string/blank? filename)))
-               (s3/get-object bucket filename))]
-    (if file
-      {:status 200
-       :headers {"Content-Type" "application/zip"
-                 "Content-Disposition" (str "attachment; filename=" filename)}
-       :body (ring-io/piped-input-stream (fn [out]
-                                           (io/copy (:input-stream file) out)))}
-      {:status 404
-       :body "File not found"})))
+               (s3/get-object bucket filename))
+        stats-type (if (or (nil? origin) (= "api" origin))
+                     :download-route-netex-api
+                     :download-route-netex-ui)]
+    (do
+      ;; Store download statistics to database
+      (specql/insert! db ::common/stats-service
+                      {::common/transport-service-id service-id
+                       ::common/type stats-type
+                       ::common/created (java.sql.Timestamp. (System/currentTimeMillis))})
+      ;; Return file
+      (if file
+        {:status 200
+         :headers {"Content-Type" "application/zip"
+                   "Content-Disposition" (str "attachment; filename=" filename)}
+         :body (ring-io/piped-input-stream (fn [out]
+                                             (io/copy (:input-stream file) out)))}
+        {:status 404
+         :body "File not found"}))))
 
 (defrecord NeTExExport [config]
   component/Lifecycle
@@ -36,10 +51,13 @@
         (http/publish! http
                        {:authenticated? false}
                        (GET "/export/netex/:transport-service-id{[0-9]+}/:file-id{[0-9]+}"
-                            {{:keys [file-id]} :params}
+                            {{:keys [file-id transport-service-id]} :params
+                             {:strs [origin]} :query-params}
                          (fetch-netex-response db
                                                (:netex config)
-                                               (Long/parseLong file-id)))))))
+                                               (Long/parseLong file-id)
+                                               (Long/parseLong transport-service-id)
+                                               origin))))))
   (stop [{stop ::stop :as this}]
     (stop)
     (dissoc this ::stop)))
