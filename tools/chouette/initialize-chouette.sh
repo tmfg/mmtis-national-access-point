88fc6b57d8c59a1fad14091bb3dfdfae2f34d464
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-10-29 14:08:11 +0200

    Update initialize-chouette bash script to use files and folders from variables

--- tools/chouette/initialize-chouette.sh
+++ tools/chouette/initialize-chouette.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo "1. Create /opt/chouette/ folder"
-if [ ! -d "/opt/chouette/" ]; then
+# Current chouette version - change if version changes
+chouettezip=mobi.chouette.command-3.4.8.zip
+# Folder where unzipped content is moved
+chouettezipFolder=chouette-cmd_3.4.8
+# Folder where napote tries to use chouette. Configured in config.edn
+destinationdir=/opt/chouette
 
-  sudo mkdir -p /opt/chouette
+echo "1. Create $destinationdir folder"
+if [ ! -d "$destinationdir" ]; then
+
+  sudo mkdir -p "$destinationdir"
   response=$?
   if [ $response -eq 0 ]; then
       echo "OK"
@@ -12,30 +19,31 @@ if [ ! -d "/opt/chouette/" ]; then
   fi
 
 else
-  echo "Directory /opt/chouette/ exists."
+  echo "Directory $destinationdir exists."
 fi
 
-echo "2. Copy chouette.zip to /opt/chouette"
-sudo cp mobi.chouette.command-3.4.8.zip /opt/chouette/
+echo "2. Copy $chouettezip to $destinationdir"
+sudo cp $chouettezip $destinationdir
 if [ $? -eq 0 ]; then
     echo "Copy - OK"
 else
     echo "Copy - FAIL"
 fi
 
-echo "3. Unzip chouette.zip"
-sudo cd /opt/chouette
-sudo unzip /opt/chouette/mobi.chouette.command-3.4.8.zip -d /opt/chouette/
+echo "3. Unzip $chouettezip"
+sudo cd "$destinationdir"
+sudo unzip "$destinationdir"/$chouettezip -d "$destinationdir"
 
 echo "4. Give permissions "
+currentuser=$(who | awk 'NR==1{print $1}')
 sudo cd ..
-sudo chown -R markusva:staff /opt/chouette
-sudo chmod -R 777 /opt/chouette
+sudo chown -R $currentuser:staff "$destinationdir"
+sudo chmod -R 777 "$destinationdir"
 
 echo "5. Remove .zip"
-rm -rf /opt/chouette/mobi.chouette.command-3.4.8.zip
+rm -rf "$destinationdir"/$chouettezip
 
-echo "6. Copy files to /opt/chouette"
-sudo mv /opt/chouette/chouette-cmd_3.4.8/* /opt/chouette/
-sudo rm -rf /opt/chouette/chouette-cmd_3.4.8/
+echo "6. Copy files to $destinationdir"
+sudo mv "$destinationdir"/$chouettezipFolder/* "$destinationdir"/
+sudo rm -rf "$destinationdir"/$chouettezipFolder/
 echo "READY!"
