997d61cdb46e9b4335ac41fde1610ecf0f73cb3f
Markus Vähäkangas <markus.vahakangas@solita.fi>
2019-12-09 14:45:49 +0200

    Add backend routes for terms of service

--- ote/src/clj/ote/services/register.clj
+++ ote/src/clj/ote/services/register.clj
@@ -18,16 +18,17 @@
 
 (defn- valid-registration? [{:keys [name email password]}]
   (and (user/password-valid? password)
-    (user/email-valid? email)
-    (string? name) (not (str/blank? name))))
+       (user/email-valid? email)
+       (string? name) (not (str/blank? name))))
 
-(defn- register-user! [db auth-tkt-config {:keys [name email password token] :as form-data}]
+(defn- register-user! [db auth-tkt-config {:keys [name email password token acceped-tos?] :as form-data}]
   (if-not (valid-registration? form-data)
     ;; Check errors that should have been checked on the form
     {:success? false}
     (let [email-taken? (email-exists? db {:email email})
           group-info (when token
-                       (first (fetch-operator-info db {:token token})))]
+                       (first (fetch-operator-info db {:token token})))
+          _ (println "register " (pr-str form-data))]
       (if email-taken?
         ;; email taken, return errors to form
         {:success? false
@@ -36,38 +37,49 @@
         (do
           (let [user-id (str (UUID/randomUUID))
                 new-user (specql/insert! db ::user/user
-                           {::user/id user-id
-                            ::user/name user-id ;; Username not used anymore, use internal row id as placeholder just in case
-                            ::user/fullname name
-                            ::user/email email
-                            ::user/password (encrypt/buddy->passlib (encrypt/encrypt password))
-                            ::user/created (java.util.Date.)
-                            ::user/state "active"
-                            ::user/sysadmin false
-                            ::user/email-confirmed? false
-                            ::user/apikey (str (UUID/randomUUID))
-                            ::user/activity_streams_email_notifications false})]
+                                         {::user/id user-id
+                                          ::user/name user-id ;; Username not used anymore, use internal row id as placeholder just in case
+                                          ::user/fullname name
+                                          ::user/email email
+                                          ::user/password (encrypt/buddy->passlib (encrypt/encrypt password))
+                                          ::user/created (java.util.Date.)
+                                          ::user/state "active"
+                                          ::user/sysadmin false
+                                          ::user/email-confirmed? false
+                                          ::user/apikey (str (UUID/randomUUID))
+                                          ::user/activity_streams_email_notifications false
+                                          ::user/accepted-tos? acceped-tos?
+                                          ::user/seen-tos? acceped-tos?})]
             (when (and token group-info)                    ;; If the user doesn't have a token or group-info they can register, but aren't added to any group
               (transport-operator/create-member! db (::user/id new-user) (:ckan-group-id group-info))
               (specql/delete! db ::user/user-token
-                {::user/token token})
+                              {::user/token token})
               (log/info "New user (" email ") registered with token from " (:name group-info))))
           {:success? true})))))
 
 (defn- register-response [db email auth-tkt-config form-data]
   (with-transaction db
-    (feature/when-enabled :ote-register
-      (let [result (register-user! db auth-tkt-config form-data)
-            email-confirmation-token (str (UUID/randomUUID))
-            user-email (:email form-data)
-            language (:language form-data)]
-        (if (:success? result)
-          ;; User created, log in immediately with the user info
-          (do (user-service/create-confirmation-token! db (:email form-data) email-confirmation-token)
-              (user-service/send-email-verification email user-email language email-confirmation-token)
-              (http/transit-response result 201))
-          ;; registeration failed send errors
-          (http/transit-response result 400))))))
+                    (feature/when-enabled :ote-register
+                                          (let [result (register-user! db auth-tkt-config form-data)
+                                                email-confirmation-token (str (UUID/randomUUID))
+                                                user-email (:email form-data)
+                                                language (:language form-data)]
+                                            (if (:success? result)
+                                              ;; User created, log in immediately with the user info
+                                              (do (user-service/create-confirmation-token! db (:email form-data) email-confirmation-token)
+                                                  (user-service/send-email-verification email user-email language email-confirmation-token)
+                                                  (http/transit-response result 201))
+                                              ;; registeration failed send errors
+                                              (http/transit-response result 400))))))
+
+(defn- accept-tos [db user form-data]
+  (if (= (:email (:user user)) (:user-email form-data))
+    (do
+      (specql/update! db ::user/user
+                      {::user/seen-tos? true}
+                      {::user/id (:id (:user user))})
+      (http/transit-response "OK" 200))
+    (http/transit-response "ERROR!" 401)))
 
 (defn- register-routes
   "Unauthenticated routes"
@@ -82,7 +94,11 @@
           (http/transit-response {:success? false
                                   :message :already-logged-in} 400)
           (#'register-response db email auth-tkt-config
-            (http/transit-request form-data)))))))
+            (http/transit-request form-data))))
+
+      (POST "/register/tos" {form-data :body
+                             user :user}
+        (accept-tos db user (http/transit-request form-data))))))
 
 (defrecord Register [config]
   component/Lifecycle
