2fe4058e181a792cc327a83bd53ee12c0a112540
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-01-14 11:41:14 +0200

    Create database table for service company csv and add s3 bucket name to config files

---
+++ database/src/main/resources/db/migration/V1_181__create_service_csv_table.sql
@@ -0,0 +1,10 @@
+-- Create table to store s3 locations for transport-service company csv files
+CREATE TABLE "transport_service_company_csv"
+(
+    id                     SERIAL PRIMARY KEY,
+    "transport-service-id" INTEGER REFERENCES "transport-service" (id),
+    "csv-file-name"        TEXT,
+    "validation-warning"   TEXT,
+    created                timestamp with time zone DEFAULT NOW(),
+    "created-by"           TEXT REFERENCES "user" (id)
+);
