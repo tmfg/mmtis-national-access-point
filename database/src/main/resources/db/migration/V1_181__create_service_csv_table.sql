83fb0f584a1c3c27b9362436550cf34629bdfd7c
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-01-15 16:02:55 +0200

    Add on delete cascade to foreign key in service-csv-table

--- database/src/main/resources/db/migration/V1_181__create_service_csv_table.sql
+++ database/src/main/resources/db/migration/V1_181__create_service_csv_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "transport_service_company_csv"
 (
     id                     SERIAL PRIMARY KEY,
-    "transport-service-id" INTEGER REFERENCES "transport-service" (id),
+    "transport-service-id" INTEGER REFERENCES "transport-service" (id) ON DELETE CASCADE,
     "csv-file-name"        TEXT,
     "validation-warning"   TEXT,
     created                timestamp with time zone DEFAULT NOW(),
