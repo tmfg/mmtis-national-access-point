0464dec9999af661b24f5e764e48eab668125bb1
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-02-12 11:36:45 +0200

    import gtfs form url: Fix is first package issue when service has multiple interfaces

--- ote/src/clj/ote/integration/import/import_gtfs.sql
+++ ote/src/clj/ote/integration/import/import_gtfs.sql
@@ -1,2 +1,5 @@
 -- name: gtfs-set-package-geometry
 SELECT gtfs_set_package_geometry(:package-id::INTEGER);
+
+-- name: fetch-count-service-packages
+SELECT COUNT(p.id) as "package-count" FROM gtfs_package p WHERE p."transport-service-id" = :service-id;
