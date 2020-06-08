2f2b1ae12f68e5143c3b6c7eb28a7d35bf5f1b56
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-06-08 06:52:52 +0300

    Fix str to vector fn

--- ote/src/clj/ote/util/db.clj
+++ ote/src/clj/ote/util/db.clj
@@ -12,7 +12,10 @@
 
 (defn str-vec->str [v]
   "Mainly convert used-packages vector of strings to list of integers"
-  (let [val (str "{"
+  (let [v (if (string? v)
+              (conj [] v)
+              v)
+        val (str "{"
                  (clojure.string/join "," v)
                  "}")]
     val))
