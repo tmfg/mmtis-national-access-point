c80b698fe666f5a167269fe1cdbe2aa28c31ed9a
Esko Suomi <esko.suomi@solita.fi>
2021-11-18 17:23:58 +0200

    regenerated font import directive to include all variants

--- ote/src/clj/ote/services/index.clj
+++ ote/src/clj/ote/services/index.clj
@@ -17,7 +17,7 @@
                   {:href "css/nprogress.css"}
                   {:href "css/balloon.css"}
                   {:href "css/styles.css"}
-                  {:href "https://fonts.googleapis.com/css?family=Public%20Sans:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic%7Public%20Sans:200,300,300i,400,400i,500,500i,700,700i"
+                  {:href "https://fonts.googleapis.com/css?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900"
                    :rel "stylesheet"}
                   {:href "https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
                    :integrity "sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ=="}
