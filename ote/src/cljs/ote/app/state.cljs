f2e8080570d810dba56c6d660c7a2254d9e44be7
Esko Suomi <esko.suomi@solita.fi>
2022-02-23 11:08:12 +0200

    clean out unused references to storage-atom
    Previous removal was quick&dirty, this is actual cleanup.

--- ote/src/cljs/ote/app/state.cljs
+++ ote/src/cljs/ote/app/state.cljs
@@ -1,45 +1,35 @@
 (ns ote.app.state
-  "Contains the global frontend application `app` database. The database is stored into localhost and accessed as ratom
-   for interop. All frontend applications can access the shared global state. Everything that is in the current state of
-   the frontend is in the app atom."
-  (:require [alandipert.storage-atom :as store :refer [local-storage]]
-            [cognitect.transit :as transit]
-            [reagent.core :as r])
-  (:import [goog.date UtcDateTime]))
+  "Contains the frontend application `app` database.
+  Everything that is in the current state of the frontend is in the app atom."
+  (:require [reagent.core :as r]))
 
-; inject cljs-time/goog.date read+write support to storage-atom's transit serde
-; Yes, this is quite a hack right now, so TODO: less hacky plz
-(swap! store/transit-read-handlers assoc "m" (transit/read-handler (fn [s] (UtcDateTime.fromTimestamp s))))
-(swap! store/transit-write-handlers assoc UtcDateTime (transit/write-handler
-                                                  (constantly "m")
-                                                  (fn [v] (.getTime v))
-                                                  (fn [v] (str (.getTime v)))))
+(defonce app
+         (r/atom {;; current page
+                  ;; see ote.app.routes
+                  :page :front-page
+                  :params nil ; parameters from url route
+                  :query nil ; query parameters from url (like "?foo=bar")
 
-(defonce app (r/atom {;; current page
-                      ;; see ote.app.routes
-                      :page :front-page
-                      :params nil ; parameters from url route
-                      :query nil ; query parameters from url (like "?foo=bar")
+                  :user {} ;; No user data by default
 
-                       :user {} ;; No user data by default
+                  ;; Currently selected / edited transport operator (company basic info)
+                  :transport-operator nil
 
-                      ;; Currently selected / edited transport operator (company basic info)
-                      :transport-operator nil
-
-                      ;; Currently selected / edited transport service
-                      :transport-service {}}))
+                  ;; Currently selected / edited transport service
+                  :transport-service {}}))
 
 ;; Separate app state for viewer mode
 (defonce viewer (r/atom {:loading? true
                          :url nil
                          :geojson nil}))
 
+
 (defn windowresize-handler
   "When window size changes set width and height to app state"
   [event]
   (let [w (.-innerWidth js/window)
         h (.-innerHeight js/window)]
-      (swap! app assoc :width w :height h)))
+    (swap! app assoc :width w :height h)))
 
 (.addEventListener js/window "resize" windowresize-handler)
 
