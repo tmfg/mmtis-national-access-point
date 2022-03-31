2ea0c1d92b7c872fc762546a06b57ac02a7f0680
Esko Suomi <esko.suomi@solita.fi>
2022-03-31 07:59:46 +0300

    remove fully qualified part

--- database/src/main/resources/db/migration/V1_200__fix_package_report_constraint.sql
+++ database/src/main/resources/db/migration/V1_200__fix_package_report_constraint.sql
@@ -2,4 +2,4 @@
 ALTER TABLE gtfs_import_report
     DROP CONSTRAINT gtfs_import_report_package_id_fkey
     , ADD  CONSTRAINT gtfs_import_report_package_id_fkey
-    FOREIGN KEY (package_id) REFERENCES napote.public.gtfs_package (id) ON DELETE CASCADE;
+    FOREIGN KEY (package_id) REFERENCES gtfs_package (id) ON DELETE CASCADE;
