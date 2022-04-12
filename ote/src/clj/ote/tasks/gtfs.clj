c893d4608d9b2e2379a0ffb8301a56b19b252dcd
Esko Suomi <esko.suomi@solita.fi>
2022-04-12 11:15:05 +0300

    add filtering to emails so that only errors are reported
    This of course means that if there's only warnings, no email is sent.

--- ote/src/clj/ote/tasks/gtfs.clj
+++ ote/src/clj/ote/tasks/gtfs.clj
@@ -71,7 +71,8 @@
 (defn email-validation-results
   [db email service-id interface-id]
   ; disabled for now by request of customer
-  #_(let [report (report/latest-import-reports-for-service-interface db service-id interface-id)]
+  #_(let [report (->> (report/latest-import-reports-for-service-interface db service-id interface-id)
+                    (filter (fn [r] (= "error" (:gtfs-import-report/severity r)))))]
     (if (empty? report)
       (log/info (str "Empty report for service/interface " service-id "/" interface-id ", skipping email"))
       (let [service (some-> (specql/fetch db ::t-service/transport-service
