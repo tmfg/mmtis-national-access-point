77fbc47153c56a7d3edf28d9a1b54bd98527dc9b
Esko Suomi <esko.suomi@solita.fi>
2023-12-04 14:15:34 +0200

    make all logging info level to make the states visible in logs
    now that there's filtering for results this won't be hugely spammy

--- ote/src/clj/ote/tasks/tis.clj
+++ ote/src/clj/ote/tasks/tis.clj
@@ -22,7 +22,7 @@
       (mapv
         (fn [package]
           (let [[package-id entry-public-id] (select-keys package [:id :tis-entry-public-id])]
-            (log/debug (str "Polling package " package-id "/" entry-public-id " for results"))
+            (log/info (str "Polling package " package-id "/" entry-public-id " for results"))
             (let [entry     (tis-vaco/api-fetch-entry (:tis-vaco config) entry-public-id)
                   complete? (every? (fn [t] (not (some? (get t "completed")))) (get-in entry ["data" "tasks"]))
                   links     (get entry "links")
@@ -35,11 +35,11 @@
                                            :tis-result-links    (cheshire/generate-string result)}))
                 (if complete?
                   (do
-                    (log/debug (str "No results found for package " package-id "/" entry-public-id " but the entry is complete -> no result available"))
+                    (log/info (str "No results found for package " package-id "/" entry-public-id " but the entry is complete -> no result available"))
                     (update-tis-results! db {:tis-entry-public-id entry-public-id
                                              :tis-complete        true
                                              :tis-result-links    nil}))
-                  (log/debug (str "Package " package-id "/" entry-public-id " processing is not yet complete on TIS side."))))))
+                  (log/info (str "Package " package-id "/" entry-public-id " processing is not yet complete on TIS side."))))))
 
           )
         packages))))
