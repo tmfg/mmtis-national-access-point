3f70ac35b898cef9b57411b6acb6c893732a171f
Esko Suomi <esko.suomi@solita.fi>
2024-01-25 16:05:09 +0200

    replace TIS VACO scheduled integration with its own dedicated nightly job
    This transfers all feeds and also decouples it from NeTEX conversion/change detection which is great

--- ote/src/clj/ote/tasks/tis.sql
+++ ote/src/clj/ote/tasks/tis.sql
@@ -10,3 +10,19 @@ UPDATE "gtfs_package"
    SET "tis-complete" = :tis-complete,
        "tis-success" = :tis-success
 WHERE "tis-entry-public-id" = :tis-entry-public-id;
+
+-- name: fetch-count-service-packages
+SELECT COUNT(p.id) as "package-count" FROM gtfs_package p WHERE p."transport-service-id" = :service-id;
+
+-- name: list-all-external-interfaces
+SELECT tso.id                         AS "operator-id",
+       tso.name                       AS "operator-name",
+       tse.id                         AS "service-id",
+       eid.id                         AS "external-interface-description-id",
+       eid.license                    AS "license",
+       (eid."external-interface").url AS url
+  FROM "external-interface-description" eid
+           LEFT JOIN "transport-service" tse ON tse.id = eid."transport-service-id"
+           LEFT JOIN "transport-operator" tso ON tso.id = tse."transport-operator-id"
+ WHERE tse.published IS NOT NULL
+   AND ('GTFS' = ANY (eid.format) OR 'gtfs' = ANY (eid.format));
