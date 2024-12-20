f4f1863901592990ea75f7c1632f999b8f3c9909
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-12-20 07:46:24 +0200

    Fix transport-service compare test

--- ote/test/clj/ote/test_tools.clj
+++ ote/test/clj/ote/test_tools.clj
@@ -4,3 +4,13 @@
 (defn get-transport-opertor-by-name [operator-name]
   (first (sql-query
            (format "SELECT id, name, \"business-id\", email FROM \"transport-operator\" WHERE name = '%s'" operator-name))))
+
+(defn dissoc-in
+  "Like assoc-in but for dissoc"
+  [m [k & ks]]
+  (if ks
+    (let [sub-map (dissoc-in (get m k) ks)]
+      (if (empty? sub-map)
+        (dissoc m k)
+        (assoc m k sub-map)))
+    (dissoc m k)))
